a12983a7e31f0c294cb916c692d4e1da6eae2388
Marek Libra <marek.libra@gmail.com>
2020-06-24 10:35:20 +0200

    Add filters to ClustersPage

--- src/components/clusters/ClustersTable.tsx
+++ src/components/clusters/ClustersTable.tsx
@@ -17,6 +17,7 @@ import sortable from '../ui/table/sortable';
 import DeleteClusterModal from './DeleteClusterModal';
 import { getClusterTableStatusCell } from '../../selectors/clusters';
 import { CLUSTER_STATUS_LABELS } from '../../config/constants';
+import ClustersFilterToolbar, { ClusterFiltersType } from './ClustersFilterToolbar';
 
 const rowKey = ({ rowData }: ExtraParamsType) => rowData?.props.id;
 
@@ -41,11 +42,18 @@ const columns = [
   { title: 'Hosts', ...columnConfig },
 ];
 
+const getStatusCell = (row: IRow) => row.cells?.[3] as HumanizedSortable | undefined;
+
 const ClustersTable: React.FC<ClustersTableProps> = ({ rows, deleteCluster }) => {
   const [deleteClusterID, setDeleteClusterID] = React.useState<DeleteClusterID>();
   const [sortBy, setSortBy] = React.useState<ISortBy>({
     index: 0, // Name-column
     direction: SortByDirection.asc,
+  });
+
+  const [searchName, setSearchName] = React.useState('');
+  const [filters, setFilters] = React.useState<ClusterFiltersType>({
+    status: [],
   });
 
   const actionResolver: IActionsResolver = React.useCallback(
@@ -73,19 +81,41 @@ const ClustersTable: React.FC<ClustersTableProps> = ({ rows, deleteCluster }) =>
     [setSortBy],
   );
 
-  const sortedRows = React.useMemo(
-    () =>
-      rows.sort(
+  const rowFilter = React.useCallback(
+    (row: IRow) => {
+      if (searchName && !(row.props.name || '').includes(searchName)) {
+        return false;
+      }
+
+      const value = getStatusCell(row)?.sortableValue as string;
+      if (filters.status.length > 0 && value && !filters.status.includes(value)) {
+        return false;
+      }
+
+      return true;
+    },
+    [searchName, filters],
+  );
+
+  const sortedRows = React.useMemo(() => {
+    return rows
+      .filter(rowFilter)
+      .sort(
         rowSorter(
           sortBy,
           (row: IRow, index = 0) => row.cells?.[index] as string | HumanizedSortable,
         ),
-      ),
-    [rows, sortBy],
-  );
+      );
+  }, [rows, sortBy, rowFilter]);
 
   return (
     <>
+      <ClustersFilterToolbar
+        searchName={searchName}
+        setSearchName={setSearchName}
+        filters={filters}
+        setFilters={setFilters}
+      />
       <Table
         rows={sortedRows}
         cells={columns}
