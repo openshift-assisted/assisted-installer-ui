cdbab8eba135e2334b7b3639259c61deca0f283d
Jiri Tomasek <jtomasek@redhat.com>
2020-06-25 10:57:54 +0200

    Update versions API endpoint, export DetailList

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,7 @@ export * from './utils';
 export * from './formik';
 export * from './Toolbar';
 export * from './Alerts';
+export * from './DetailList';
 
 export { default as PageSection } from './PageSection';
 export { default as ExternalLink } from './ExternalLink';
