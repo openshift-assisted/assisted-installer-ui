0967ba08ab520f6d2a78166e542aa7d24b6c1823
Vitaliy Emporopulo <vemporop@redhat.com>
2020-09-13 22:55:47 +0300

    MGMT-1015 Remove time zone from dates following API fix

--- src/components/ui/utils.ts
+++ src/components/ui/utils.ts
@@ -2,13 +2,12 @@ import { DASH } from '../constants';
 
 export const getHumanizedDateTime = (dateTime?: string) => {
   if (!dateTime) return DASH;
-  // NOTE(jtomasek): server does not return ISO-8601 formatted date, therefore we need to adjust it
-  const date = new Date(`${dateTime}Z`);
+  const date = new Date(`${dateTime}`);
   return date.toLocaleString();
 };
 
 export const getHumanizedTime = (dateTime?: string) => {
   if (!dateTime) return DASH;
-  const date = new Date(`${dateTime}Z`);
+  const date = new Date(`${dateTime}`);
   return date.toLocaleTimeString();
 };
