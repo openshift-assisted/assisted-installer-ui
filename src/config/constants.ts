b781aa4e5944f43b81197af77b609997b35f0b7a
andybraren <abraren@redhat.com>
2020-09-13 20:17:00 -0400

    Change pull secret link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -19,7 +19,7 @@ export const DEFAULT_OPENSHIFT_VERSION: OpenshiftVersionOptionType['value'] =
   OPENSHIFT_VERSION_OPTIONS[0].value;
 
 export const CLUSTER_MANAGER_SITE_LINK =
-  'https://cloud.redhat.com/openshift/install/metal/user-provisioned';
+  'https://cloud.redhat.com/openshift/install/pull-secret';
 
 export const FEEDBACK_FORM_LINK =
   'https://docs.google.com/forms/d/e/1FAIpQLSfg9M8wRW4m_HkWeAl6KpB5dTcMu8iI3iJ29GlLfZpF2hnjng/viewform';
