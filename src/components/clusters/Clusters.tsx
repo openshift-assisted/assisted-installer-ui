bce41270a3e49f236432c75803ae29f47c824927
Marek Libra <marek.libra@gmail.com>
2020-10-20 14:10:00 +0200

    Bredcrumb navigates to OCM list

--- src/components/clusters/Clusters.tsx
+++ src/components/clusters/Clusters.tsx
@@ -24,7 +24,7 @@ import { deleteCluster as ApiDeleteCluster } from '../../api/clusters';
 import AlertsSection from '../ui/AlertsSection';
 import { handleApiError, getErrorMessage } from '../../api/utils';
 import { AlertsContext, AlertsContextProvider } from '../AlertsContextProvider';
-import DeveloperPreview from '../ui/DeveloperPreview';
+import ClusterBreadcrumbs from './ClusterBreadcrumbs';
 
 type ClustersProps = RouteComponentProps;
 
@@ -96,8 +96,8 @@ const Clusters: React.FC<ClustersProps> = ({ history }) => {
     default:
       return (
         <>
+          <ClusterBreadcrumbs />
           <PageSection variant={PageSectionVariants.light}>
-            <DeveloperPreview />
             <TextContent>
               <Text component="h1">Assisted Bare Metal Clusters</Text>
             </TextContent>
