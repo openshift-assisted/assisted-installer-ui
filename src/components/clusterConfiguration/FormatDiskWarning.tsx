aa8822ec53e4eb0bfbb71d525a5cfb880f1da599
Marek Libra <marek.libra@gmail.com>
2020-11-03 13:24:24 +0100

    MGMT-2583: Add warning about wiping disks

---
+++ src/components/clusterConfiguration/FormatDiskWarning.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from '@patternfly/react-core';
+import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+import { global_warning_color_100 as warningColor } from '@patternfly/react-tokens';
+
+const FormatDiskWarning = () => (
+  <Text component="p">
+    <ExclamationTriangleIcon className="status-icon" color={warningColor.value} size="sm" />
+    &nbsp;Note that all disks will be formatted when the installation starts.
+  </Text>
+);
+
+export default FormatDiskWarning;
