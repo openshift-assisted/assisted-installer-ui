d7a7605f0452622131380925aa0c99b8f0254797
Jiri Tomasek <jtomasek@redhat.com>
2020-12-10 15:02:01 +0100

    Remove usage of AlertsSection and replace it with Alerts component

--- src/components/clusters/NewClusterPage.tsx
+++ src/components/clusters/NewClusterPage.tsx
@@ -17,7 +17,7 @@ import {
 import PageSection from '../ui/PageSection';
 import { ToolbarButton } from '../ui/Toolbar';
 import ClusterToolbar from './ClusterToolbar';
-import AlertsSection from '../ui/AlertsSection';
+import Alerts from '../ui/Alerts';
 import { handleApiError, getErrorMessage } from '../../api/utils';
 import { AlertsContext, AlertsContextProvider } from '../AlertsContextProvider';
 import ClusterBreadcrumbs from './ClusterBreadcrumbs';
@@ -133,6 +133,7 @@ const NewClusterForm: React.FC<NewClusterFormProps> = ({ pullSecret = '', versio
                 </Stack>
               </GridItem>
             </Grid>
+            <Alerts />
             <ClusterToolbar>
               <ToolbarButton
                 name="save"
@@ -182,7 +183,6 @@ const NewCluster: React.FC = () => {
         ) : (
           <NewClusterForm pullSecret={pullSecret} versions={versions} />
         )}
-        <AlertsSection />
       </PageSection>
     </>
   );
