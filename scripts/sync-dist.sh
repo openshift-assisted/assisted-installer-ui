65e6150960a0bbdf52cacf08610cfe0cb80d2da1
Marek Libra <marek.libra@gmail.com>
2020-12-10 14:09:59 +0100

    Changes after repository ownership transfer
    Former upstream location:
      https://github.com/marek-libra/facet-lib
    
    New location:
      https://github.com/openshift-assisted/assisted-ui-lib

--- scripts/sync-dist.sh
+++ scripts/sync-dist.sh
@@ -8,21 +8,21 @@ UHC_PORTAL=${UHC_PORTAL:-../uhc-portal}
 
 function sync {
   if [ -d "${FACET_ROOT}" ]; then
-    rsync -avz --delete ${BUILD_OUTPUT} ${FACET_ROOT}/node_modules/facet-lib
+    rsync -avz --delete ${BUILD_OUTPUT} ${FACET_ROOT}/node_modules/assisted-ui-lib
   else
-    echo Sync to ${FACET_ROOT}/node_modules/facet-lib skipped
+    echo Sync to ${FACET_ROOT}/node_modules/openshift-assisted-ui-lib skipped
   fi
 
   if [ -d "${UHC_PORTAL}" ]; then
-    rsync -avz --delete ${BUILD_OUTPUT} ${UHC_PORTAL}/node_modules/facet-lib
+    rsync -avz --delete ${BUILD_OUTPUT} ${UHC_PORTAL}/node_modules/assisted-ui
   else
-    echo Sync to ${UHC_PORTAL}/node_modules/facet-lib skipped
+    echo Sync to ${UHC_PORTAL}/node_modules/openshift-assisted-ui-lib skipped
   fi
 }
 
 sync
-cp ./package.json ${FACET_ROOT}/node_modules/facet-lib/package.json || true
-cp ./package.json ${UHC_PORTAL}/node_modules/facet-lib/package.json || true
+cp ./package.json ${FACET_ROOT}/node_modules/assisted-ui-lib/package.json || true
+cp ./package.json ${UHC_PORTAL}/node_modules/assisted-ui-lib/package.json || true
 
 while inotifywait -r -e modify,create,delete,move ./dist ; do
   sleep 2 # give JS bundler time to write everything
