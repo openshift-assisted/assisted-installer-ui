062de091e4af8c67b284eeb67412a5ca34b0b7cf
Marek Libra <marek.libra@gmail.com>
2020-12-15 14:21:08 +0100

    MGMT-3266: Rephrase error states when creating Day2 cluster

--- src/components/AddBareMetalHosts/BareMetalHostsClusterDetailTab.tsx
+++ src/components/AddBareMetalHosts/BareMetalHostsClusterDetailTab.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, ButtonVariant } from '@patternfly/react-core';
+import React, { ReactNode } from 'react';
+import { Button, ButtonVariant, EmptyStateVariant } from '@patternfly/react-core';
 import { normalizeClusterVersion } from '../../config';
 import { OcmClusterType } from './types';
 import { AddHostsClusterCreateParams, Cluster, getCluster, handleApiError } from '../../api';
@@ -12,20 +12,61 @@ import { POLLING_INTERVAL } from '../../config';
 import AddBareMetalHosts from './AddBareMetalHosts';
 import { AddBareMetalHostsContextProvider } from './AddBareMetalHostsContext';
 
+type OpenModalType = (modalName: string, cluster?: OcmClusterType) => void;
+
 export const BareMetalHostsClusterDetailTab: React.FC<{
   cluster?: OcmClusterType;
   isVisible: boolean;
-}> = ({ cluster, isVisible }) => {
-  const [error, setError] = React.useState<string>();
+  openModal?: OpenModalType;
+}> = ({ cluster, isVisible, openModal }) => {
+  const [error, setError] = React.useState<ReactNode>();
   const [day2Cluster, setDay2Cluster] = React.useState<Cluster | null>();
   const pullSecret = usePullSecretFetch();
+
+  const TryAgain = React.useCallback(
+    () => (
+      <Button
+        onClick={() => {
+          setError(undefined);
+          setDay2Cluster(undefined);
+        }}
+        variant={ButtonVariant.link}
+        isInline
+      >
+        try again
+      </Button>
+    ),
+    [setError, setDay2Cluster],
+  );
+
+  const MissingApiUrl = React.useCallback(() => {
+    return (
+      <>
+        Neither API nor Console URL has been reported by the cluster yet.
+        {openModal && (
+          <>
+            {' '}
+            <br />
+            Please hold on and {<TryAgain />} later or{' '}
+            <Button
+              variant={ButtonVariant.link}
+              isInline
+              onClick={() => openModal('edit-console-url', cluster)}
+            >
+              add console URL
+            </Button>{' '}
+            manually.
+          </>
+        )}
+      </>
+    );
+  }, [openModal, cluster]);
 
   React.useEffect(() => {
     if (!isVisible && day2Cluster) {
       // the tab is not visible, stop polling
       setDay2Cluster(undefined);
     }
-
     const openshiftClusterId = getOpenshiftClusterId(cluster);
 
     if (isVisible && day2Cluster === undefined && cluster && openshiftClusterId && pullSecret) {
@@ -35,7 +76,13 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
       // validate input
       const openshiftVersion = normalizeClusterVersion(cluster.openshift_version);
       if (!openshiftVersion) {
-        setError(`Unsupported OpenShift cluster version: ${cluster.openshift_version}`);
+        setError(
+          <>
+            Unsupported OpenShift cluster version: ${cluster.openshift_version}.
+            <br />
+            Check your connection and <TryAgain />.
+          </>,
+        );
         return;
       }
 
@@ -46,7 +93,11 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
           const apiVipUrl = new URL(cluster.api.url);
           apiVipDnsname = apiVipUrl.hostname; // just domain is needed
         } catch {
-          setError('Cluster API URL is not valid.');
+          setError(
+            <>
+              Cluster API URL is not valid (${cluster.api.url}), you can <TryAgain />.
+            </>,
+          );
           return;
         }
       } else if (cluster.console?.url) {
@@ -59,13 +110,17 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
           apiVipDnsname =
             'api.' + consoleUrlHostname.substring(consoleUrlHostname.indexOf(APPS) + APPS.length);
         } catch {
-          setError('Cluster Console URL is not valid.');
+          setError(
+            <>
+              Cluster Console URL is not valid (${cluster.console?.url}), you can <TryAgain />.
+            </>,
+          );
           return;
         }
       }
 
       if (!apiVipDnsname) {
-        setError('Missing API URL');
+        setError(<MissingApiUrl />);
         return;
       }
 
@@ -82,7 +137,13 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
         } catch (e) {
           if (e.response?.status !== 404) {
             handleApiError(e);
-            setError('Failed to read cluster details.');
+            setError(
+              <>
+                Failed to read cluster details.
+                <br />
+                Check your connection and <TryAgain />.
+              </>,
+            );
             return;
           }
         }
@@ -100,14 +161,20 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
             setDay2Cluster(data);
           } catch (e) {
             handleApiError<AddHostsClusterCreateParams>(e);
-            setError('Failed to create wrapping cluster for adding new hosts.');
+            setError(
+              <>
+                Failed to create wrapping cluster for adding new hosts.
+                <br />
+                Check your connection and <TryAgain />.
+              </>,
+            );
           }
         }
       };
 
       doItAsync();
     }
-  }, [cluster, pullSecret, day2Cluster, isVisible]);
+  }, [cluster, openModal, pullSecret, day2Cluster, isVisible]);
 
   React.useEffect(() => {
     if (day2Cluster) {
@@ -118,7 +185,13 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
             setDay2Cluster(data);
           } catch (e) {
             handleApiError<AddHostsClusterCreateParams>(e);
-            setError('Failed to reload cluster data.');
+            setError(
+              <>
+                Failed to reload cluster data.
+                <br />
+                Check your connection and <TryAgain />.
+              </>,
+            );
           }
         };
         doItAsync();
@@ -130,24 +203,8 @@ export const BareMetalHostsClusterDetailTab: React.FC<{
   if (error) {
     return (
       <ErrorState
-        content={
-          <>
-            {error}
-            <br />
-            Check your connection and{' '}
-            <Button
-              onClick={() => {
-                setError(undefined);
-                setDay2Cluster(undefined);
-              }}
-              variant={ButtonVariant.link}
-              isInline
-            >
-              try again
-            </Button>
-            .
-          </>
-        }
+        variant={EmptyStateVariant.large}
+        content={error}
         title="Failed to prepare the cluster for adding hosts."
       />
     );
