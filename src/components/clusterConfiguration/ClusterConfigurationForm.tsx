abfbf2bceadf40c4081153529ec29d9413f70037
Jiri Tomasek <jtomasek@redhat.com>
2021-01-20 10:28:00 +0100

    MGMT-3233 Introduce ClusterDetails wizard step
    - Adjusted wizard steps
    - Moved DNS domain configuration from Networking

--- src/components/clusterConfiguration/ClusterConfigurationForm.tsx
+++ src/components/clusterConfiguration/ClusterConfigurationForm.tsx
@@ -31,9 +31,7 @@ const validationSchema = Yup.object<BareMetalDiscoveryValues>().shape({
   name: nameValidationSchema,
 });
 
-const ClusterConfigurationForm: React.FC<{
-  cluster: Cluster;
-}> = ({ cluster }) => {
+const ClusterConfigurationForm: React.FC<{ cluster: Cluster }> = ({ cluster }) => {
   const { alerts, addAlert, clearAlerts } = React.useContext(AlertsContext);
   const { setCurrentStepId } = React.useContext(ClusterWizardContext);
   const dispatch = useDispatch();
@@ -155,7 +153,7 @@ const ClusterConfigurationForm: React.FC<{
                 isSubmitting={isSubmitting}
                 isNextDisabled={!canNextClusterConfiguration({ isValid, isSubmitting, cluster })}
                 onNext={submitForm}
-                onBack={undefined /* No transition back from the first step */}
+                onBack={() => setCurrentStepId('cluster-details')}
               />
             </StackItem>
           </Stack>
