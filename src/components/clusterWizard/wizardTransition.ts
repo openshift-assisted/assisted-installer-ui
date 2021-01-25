630498c7d6dc6d431db341e1cb570bf1397a5804
Marek Libra <marek.libra@gmail.com>
2021-01-25 13:08:15 +0100

    Use TypeScript instead of constants to guard wizard transitions

---
+++ src/components/clusterWizard/wizardTransition.ts
@@ -0,0 +1,41 @@
+import { Cluster } from '../../api';
+
+export type ClusterWizardStepsType = 'cluster-configuration' | 'networking';
+export const CLUSTER_WIZARD_FIRST_STEP: ClusterWizardStepsType = 'cluster-configuration';
+
+// We are collocating all these canNext* functions for easier maintenance.
+// However they should be independent on each other anyway.
+type TransitionBackendProps = { cluster: Cluster };
+type TransitionProps = TransitionBackendProps & { isValid?: boolean; isSubmitting?: boolean };
+
+export const canNextClusterConfigurationBackend = ({ cluster }: TransitionBackendProps) => {
+  // TODO(mlibra): check backend validations
+  return !!cluster;
+};
+
+export const canNextClusterConfiguration = ({
+  isValid,
+  isSubmitting,
+  cluster,
+}: TransitionProps) => {
+  let uiValidation = true;
+  if (isValid !== undefined) {
+    uiValidation = isValid && !isSubmitting;
+  }
+
+  return uiValidation && canNextClusterConfigurationBackend({ cluster });
+};
+
+export const canNextNetworkBackend = ({ cluster }: TransitionBackendProps) => {
+  // TODO(mlibra): check backend validations
+  return !!cluster;
+};
+
+export const canNextNetwork = ({ isValid, isSubmitting, cluster }: TransitionProps) => {
+  let uiValidation = true;
+  if (isValid !== undefined) {
+    uiValidation = isValid && !isSubmitting;
+  }
+
+  return uiValidation && canNextNetworkBackend({ cluster });
+};
