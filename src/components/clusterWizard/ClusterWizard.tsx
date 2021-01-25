69495cd27b30870f10099e58754441916f319ff3
Marek Libra <marek.libra@gmail.com>
2021-01-25 13:08:15 +0100

    Use TypeScript instead of constants to guard wizard transitions

--- src/components/clusterWizard/ClusterWizard.tsx
+++ src/components/clusterWizard/ClusterWizard.tsx
@@ -4,27 +4,25 @@ import { Cluster } from '../../api/types';
 import ClusterConfigurationForm from '../clusterConfiguration/ClusterConfigurationForm';
 import NetworkConfigurationStep from '../clusterConfiguration/NetworkConfigurationStep';
 import ClusterWizardContext from './ClusterWizardContext';
-import {
-  CLUSTER_STEP_CONFIGURATION,
-  // CLUSTER_STEP_DETAIL,
-  CLUSTER_STEP_NETWORKING,
-} from './constants';
+import { ClusterWizardStepsType, CLUSTER_WIZARD_FIRST_STEP } from './wizardTransition';
 
 type ClusterWizardProps = {
   cluster: Cluster;
 };
 
 const ClusterWizard: React.FC<ClusterWizardProps> = ({ cluster }) => {
-  const [currentStepId, setCurrentStepId] = React.useState('cluster-configuration');
+  const [currentStepId, setCurrentStepId] = React.useState<ClusterWizardStepsType>(
+    CLUSTER_WIZARD_FIRST_STEP,
+  );
 
   const renderCurrentStep = React.useCallback(() => {
     switch (currentStepId) {
       //case CLUSTER_STEP_DETAIL:
       // TODO(mlibra): use different component!!
       //  return <BaremetalInventory cluster={cluster} />;
-      case CLUSTER_STEP_NETWORKING:
+      case 'networking':
         return <NetworkConfigurationStep cluster={cluster} />;
-      case CLUSTER_STEP_CONFIGURATION:
+      case 'cluster-configuration':
       default:
         return <ClusterConfigurationForm cluster={cluster} />;
     }
