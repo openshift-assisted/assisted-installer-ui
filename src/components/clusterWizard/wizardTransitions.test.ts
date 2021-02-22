372788bf648590dc427f1fc68fb3ffced2a17bdd
Jiri Tomasek <jtomasek@redhat.com>
2021-02-22 16:22:09 +0100

    Add 2 wizard transition tests

--- src/components/clusterWizard/wizardTransitions.test.ts
+++ src/components/clusterWizard/wizardTransitions.test.ts
@@ -1,4 +1,9 @@
-import { checkHostValidationGroups, checkHostValidations } from './wizardTransition';
+import {
+  checkClusterValidationGroups,
+  checkClusterValidations,
+  checkHostValidationGroups,
+  checkHostValidations,
+} from './wizardTransition';
 
 describe('wizardTransitions module tests', () => {
   describe('checkHostValidationGroups', () => {
@@ -9,6 +14,16 @@ describe('wizardTransitions module tests', () => {
   describe('checkHostValidations', () => {
     it('should return false when validationsInfo parameter is empty object', () => {
       expect(checkHostValidations({}, ['connected'])).toBeFalsy();
+    });
+  });
+  describe('checkClusterValidationGroups', () => {
+    it('should return false when validationsInfo parameter is empty object', () => {
+      expect(checkClusterValidationGroups({}, ['hostsData'])).toBeFalsy();
+    });
+  });
+  describe('checkClusterValidations', () => {
+    it('should return false when validationsInfo parameter is empty object', () => {
+      expect(checkClusterValidations({}, ['machine-cidr-defined'])).toBeFalsy();
     });
   });
 });
