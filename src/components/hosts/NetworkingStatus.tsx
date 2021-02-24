d32108adc30abf74eb77b9a54b681537e2e8f75e
Marek Libra <marek.libra@gmail.com>
2021-02-24 07:40:34 +0100

    MGMT-4202: Introduce soft validations

--- src/components/hosts/NetworkingStatus.tsx
+++ src/components/hosts/NetworkingStatus.tsx
@@ -3,6 +3,7 @@ import { Cluster, Host } from '../../api/types';
 import { ValidationsInfo } from '../../types/hosts';
 import HostStatus from './HostStatus';
 import {
+  getFailingClusterWizardSoftValidationIds,
   getWizardStepHostStatus,
   getWizardStepHostValidationsInfo,
 } from '../clusterWizard/wizardTransition';
@@ -16,8 +17,17 @@ type NetworkingStatusProps = {
 const NetworkingStatus: React.FC<NetworkingStatusProps> = (props) => {
   const networkingStatus = getWizardStepHostStatus(props.host, 'networking');
   const validationsInfo = getWizardStepHostValidationsInfo(props.validationsInfo, 'networking');
+  const sublabel = getFailingClusterWizardSoftValidationIds(validationsInfo, 'networking').length
+    ? 'Some validations failed'
+    : undefined;
+
   return (
-    <HostStatus {...props} statusOverride={networkingStatus} validationsInfo={validationsInfo} />
+    <HostStatus
+      {...props}
+      statusOverride={networkingStatus}
+      validationsInfo={validationsInfo}
+      sublabel={sublabel}
+    />
   );
 };
 
