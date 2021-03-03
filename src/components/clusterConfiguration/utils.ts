eb0555698bf1f58771012695ead2f3ecf741b85e
Jonathan Kilzi <jkilzi@redhat.com>
2021-03-03 16:21:49 +0200

    refactor: Replaces hard-coded logic with dynamic API data

--- src/components/clusterConfiguration/utils.ts
+++ src/components/clusterConfiguration/utils.ts
@@ -1,14 +1,10 @@
 import { HostSubnets, ClusterConfigurationValues } from '../../types/clusters';
 import { Cluster, Inventory, ListOperators, ManagedDomain } from '../../api/types';
 import { stringToJSON } from '../../api/utils';
-import {
-  CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV4,
-  CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV6,
-} from '../../config/constants';
 import { Address4, Address6 } from 'ip-address';
 import { getHostname } from '../hosts/utils';
-import { ClusterNetworkDefaultSettings } from './types';
 import { NO_SUBNET_SET } from '../../config/constants';
+import { ClusterNetworkDefaultSettings } from './types';
 
 export const getSubnet = (cidr: string): Address6 | Address4 | null => {
   if (Address4.isValid(cidr)) {
@@ -61,40 +57,19 @@ export const getSubnetFromMachineNetworkCidr = (machineNetworkCidr?: string) => 
   return getHumanizedSubnet(subnet);
 };
 
-const getDefaultNetworkSettings = (clusterNetworkCidr: string): ClusterNetworkDefaultSettings => {
-  const isIPv4 = Address4.isValid(clusterNetworkCidr);
-  return {
-    clusterNetworkCidr: isIPv4
-      ? CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV4.clusterNetworkCidr
-      : CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV6.clusterNetworkCidr,
-    serviceNetworkCidr: isIPv4
-      ? CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV4.serviceNetworkCidr
-      : CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV6.serviceNetworkCidr,
-    clusterNetworkHostPrefix: isIPv4
-      ? CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV4.clusterNetworkHostPrefix
-      : CLUSTER_DEFAULT_NETWORK_SETTINGS_IPV6.clusterNetworkHostPrefix,
-  };
-};
-
-// TODO: Replace this by API call to retrieve the defaults from the backend
-export const getClusterDefaultSettings = (clusterNetworkCidr: string) => ({
-  ...getDefaultNetworkSettings(clusterNetworkCidr),
-});
-
-export const isAdvConf = (cluster: Cluster) => {
-  const defaultNetworkSettings = getDefaultNetworkSettings(cluster.clusterNetworkCidr as string);
-  return (
-    cluster.clusterNetworkCidr !== defaultNetworkSettings.clusterNetworkCidr ||
-    cluster.clusterNetworkHostPrefix !== defaultNetworkSettings.clusterNetworkHostPrefix ||
-    cluster.serviceNetworkCidr !== defaultNetworkSettings.serviceNetworkCidr
-  );
-};
+export const isAdvConf = (
+  cluster: Cluster,
+  defaultNetworkSettings: Partial<ClusterNetworkDefaultSettings>,
+) =>
+  cluster.clusterNetworkCidr !== defaultNetworkSettings.clusterNetworkCidr ||
+  cluster.clusterNetworkHostPrefix !== defaultNetworkSettings.clusterNetworkHostPrefix ||
+  cluster.serviceNetworkCidr !== defaultNetworkSettings.serviceNetworkCidr;
 
 export const getInitialValues = (
   cluster: Cluster,
   managedDomains: ManagedDomain[],
+  defaultNetworkSettings: Partial<ClusterNetworkDefaultSettings>,
 ): ClusterConfigurationValues => {
-  const defaultNetworkSettings = getDefaultNetworkSettings(cluster.clusterNetworkCidr as string);
   const operators = stringToJSON<ListOperators>(cluster.operators);
 
   return {
