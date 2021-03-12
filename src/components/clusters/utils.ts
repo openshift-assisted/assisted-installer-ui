e8c26d3a8ddd94c578d318fad08088adeff811c2
Jonathan Kilzi <jkilzi@redhat.com>
2021-03-12 11:12:01 +0200

    refactor: Updates ResetClusterModal

--- src/components/clusters/utils.ts
+++ src/components/clusters/utils.ts
@@ -1,3 +1,14 @@
-import { Cluster } from '../../api/types';
+import { Cluster, LogsState } from '../../api/types';
 
 export const isSingleNodeCluster = (cluster: Cluster) => cluster.highAvailabilityMode === 'None';
+
+export const filterHostsByLogStates = (cluster: Cluster, logStates: LogsState[]) =>
+  cluster.hosts?.filter(({ logsInfo }) => logsInfo && logStates.includes(logsInfo)) || [];
+
+export const calculateCollectedLogsCount = (cluster: Cluster) => {
+  const collectedLogStates: LogsState[] = ['completed', 'timeout'];
+  const clusterHasCollectedLogs = cluster.logsInfo && collectedLogStates.includes(cluster.logsInfo);
+  const hostsWithCollectedLogsCount = filterHostsByLogStates(cluster, collectedLogStates).length;
+
+  return (clusterHasCollectedLogs ? 1 : 0) + hostsWithCollectedLogsCount;
+};
