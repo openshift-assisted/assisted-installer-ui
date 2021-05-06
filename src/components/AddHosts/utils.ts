2a7511c3a86dcf241ff1e404eb057dd31e50e548
Marek Libra <marek.libra@gmail.com>
2021-05-06 13:53:44 +0200

    BZ1957156: Fix OCM cluster data model for AI-originated subscriptions
    The AI-cluster's Subscription is recently created by the AI-Service.
    It's content is slightly different.

--- src/components/AddHosts/utils.ts
+++ src/components/AddHosts/utils.ts
@@ -3,7 +3,20 @@ import { OcmClusterType } from './types';
 export const getOpenshiftClusterId = (ocmCluster?: OcmClusterType) =>
   ocmCluster && ocmCluster.external_id;
 
-export const canAddHost = ({ cluster }: { cluster: OcmClusterType }) =>
-  cluster.state === 'ready' &&
-  !!getOpenshiftClusterId(cluster) &&
-  cluster.cloud_provider?.id === 'baremetal';
+export const canAddHost = ({ cluster }: { cluster: OcmClusterType }) => {
+  if (getOpenshiftClusterId(cluster)) {
+    if (cluster.product?.id === 'OCP-AssistedInstall' && cluster.state === 'installed') {
+      // TODO(mlibra): Review that "installed" state once we start getting cluster state from metrics
+      return true;
+    }
+    if (
+      cluster.product?.id !== 'OCP-AssistedInstall' &&
+      cluster.cloud_provider?.id === 'baremetal' &&
+      cluster.state === 'ready'
+    ) {
+      // Backward compatibility for old AI clusters (when the subscription was created by the telemetry instead of by the AI-Service)
+      return true;
+    }
+  }
+  return false;
+};
