292e0d6e8528e5d3da125b8d9e7860696b2095dd
Marek Libra <marek.libra@gmail.com>
2021-05-17 14:31:38 +0200

    Refactor DiscoveryImageSummary to  DownloadIsoModal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
 export * from './constants';
 export * from './clusterDetail';
+export * from './clusterConfiguration';
 export * from './clusters';
 export * from './hosts';
 export * from './ui';
 export * from './AlertsContextProvider';
 export * from './Router';
 export * from './AddHosts';
-// TODO(mlibra): reexport all from clusterConfiguration
