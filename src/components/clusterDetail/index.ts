e5c183a9bc7cf14033f836678266847289e4e8e7
Jiri Tomasek <jtomasek@redhat.com>
2021-05-20 10:59:52 +0200

    Re-export ClusterProgress component

--- src/components/clusterDetail/index.ts
+++ src/components/clusterDetail/index.ts
@@ -2,3 +2,4 @@ export * from './utils';
 export { default as ClusterDetail } from './ClusterDetail';
 export { default as ClusterInstallationProgressCard } from './ClusterInstallationProgressCard';
 export { default as AssistedInstallerDetailCard } from './AssistedInstallerDetailCard';
+export { default as ClusterProgress } from './ClusterProgress';
