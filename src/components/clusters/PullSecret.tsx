89bc5665be11c3888cc878d87b759a9c39cb1ccb
Rastislav Wagner <rawagner@redhat.com>
2021-06-07 15:27:27 +0200

    Export PullSecret field

--- src/components/clusters/PullSecret.tsx
+++ src/components/clusters/PullSecret.tsx
@@ -1,79 +1,17 @@
 import React from 'react';
 import { Checkbox } from '@patternfly/react-core';
-import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import { ClusterCreateParams, ocmClient } from '../../api';
-import { CLUSTER_MANAGER_SITE_LINK, PULL_SECRET_INFO_LINK } from '../../config';
-import { TextAreaField } from '../ui';
 import { useFormikContext } from 'formik';
-import PopoverIcon from '../ui/PopoverIcon';
+import PullSecretField, { PullSecretInfo } from '../ui/formik/PullSecretField';
 
 export type PullSecretProps = {
   pullSecret?: string;
 };
 
-const ClusterManagerSiteLink = () => (
-  <a href={CLUSTER_MANAGER_SITE_LINK} target="_blank" rel="noopener noreferrer">
-    OpenShift Cluster Manager <ExternalLinkAltIcon />
-  </a>
-);
-
-const PullSecretInfoLink = () => (
-  <a href={PULL_SECRET_INFO_LINK} target="_blank" rel="noopener noreferrer">
-    Learn more about pull secrets <ExternalLinkAltIcon />.
-  </a>
-);
-
-const pullSecretHelperText = ocmClient ? (
-  <>
-    Your Red Hat account's pull secret is used by default.&nbsp;
-    <PullSecretInfoLink />
-  </>
-) : (
-  <>
-    A Red Hat account's pull secret can be found in &nbsp;
-    <ClusterManagerSiteLink />
-  </>
-);
-
-const PullSecretInfo = () => (
-  <PopoverIcon
-    bodyContent={
-      <>
-        Pull secrets are used to download OpenShift Container Platform components and connect
-        clusters to a Red Hat account.&nbsp;
-        {ocmClient ? (
-          <PullSecretInfoLink />
-        ) : (
-          <>
-            Pull secrets can be found in&nbsp;
-            <ClusterManagerSiteLink />
-          </>
-        )}
-      </>
-    }
-  />
-);
-
 const PullSecret: React.FC<PullSecretProps> = ({ pullSecret }) => {
   // Fetched pull secret will never change - see LoadingState in NewCluster
   const [isExpanded, setExpanded] = React.useState(!pullSecret);
   const { setFieldValue } = useFormikContext<ClusterCreateParams>();
-
-  const textArea = (
-    <TextAreaField
-      name="pullSecret"
-      label="Pull Secret"
-      labelIcon={ocmClient ? undefined : <PullSecretInfo />}
-      getErrorText={(error) => (
-        <>
-          {error} {pullSecretHelperText}
-        </>
-      )}
-      helperText={pullSecretHelperText}
-      rows={10}
-      isRequired
-    />
-  );
 
   if (ocmClient) {
     const onCheckboxChange = () => {
@@ -98,12 +36,12 @@ const PullSecret: React.FC<PullSecretProps> = ({ pullSecret }) => {
             </>
           }
         />
-        {isExpanded && textArea}
+        {isExpanded && <PullSecretField />}
       </>
     );
   }
 
-  return textArea;
+  return <PullSecretField />;
 };
 
 export default PullSecret;
