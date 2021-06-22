b0d850256c059767235ebe4bf3e9226a47b275f0
Jonathan Kilzi <jkilzi@redhat.com>
2021-06-22 23:26:05 +0300

    BZ-1963928 Dont show "add host" tab for SNO clusters
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/components/AddHosts/utils.ts
+++ src/components/AddHosts/utils.ts
@@ -4,24 +4,26 @@ export const getOpenshiftClusterId = (ocmCluster?: OcmClusterType) =>
   ocmCluster && ocmCluster.external_id;
 
 export const canAddHost = ({ cluster }: { cluster: OcmClusterType }) => {
+  let isAllowed = false;
+
   if (getOpenshiftClusterId(cluster)) {
     // Collision of "ready" states for AI and Telemetry
     // Show the tab only if telemetry reports "ready", not the AI
     if (
-      cluster.product?.id === 'OCP-AssistedInstall' &&
-      cluster.state === 'ready' &&
-      (!cluster.aiCluster || cluster.aiCluster.status === 'installed')
+      (cluster.state === 'ready' &&
+        cluster.product?.id === 'OCP-AssistedInstall' &&
+        (!cluster.aiCluster || cluster.aiCluster.status === 'installed')) ||
+      // Backward compatibility for old AI clusters (when the subscription was created by
+      // the telemetry instead of by the AI-Service)
+      (cluster.product?.id !== 'OCP-AssistedInstall' && cluster.cloud_provider?.id === 'baremetal')
     ) {
-      return true;
+      isAllowed = true;
     }
-    if (
-      cluster.product?.id !== 'OCP-AssistedInstall' &&
-      cluster.cloud_provider?.id === 'baremetal' &&
-      cluster.state === 'ready'
-    ) {
-      // Backward compatibility for old AI clusters (when the subscription was created by the telemetry instead of by the AI-Service)
-      return true;
+
+    if (cluster.aiCluster && cluster.aiCluster['high_availability_mode'] !== 'None') {
+      isAllowed = false;
     }
   }
-  return false;
+
+  return isAllowed;
 };
