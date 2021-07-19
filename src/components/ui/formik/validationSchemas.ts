9fa5d673f2c7bbb2809d809b2b0ad46a56ad4307
jgyselov <jgyselov@redhat.com>
2021-07-19 11:04:39 +0200

    Name+domain validation when 'useRedHatDnsService' is set

--- src/components/ui/formik/validationSchemas.ts
+++ src/components/ui/formik/validationSchemas.ts
@@ -26,7 +26,14 @@ export const nameValidationSchema = (usedClusterNames: string[], baseDnsDomain =
       excludeEmptyString: true,
     })
     .max(54, 'Cannot be longer than 54 characters.')
-    .required('Required');
+    .required('Required')
+    .when('useRedHatDnsService', {
+      is: true,
+      then: Yup.string().test('is-name-unique', 'The name is already taken.', (value: string) => {
+        const clusterFullName = `${value}.${baseDnsDomain}`;
+        return !value || !usedClusterNames.includes(clusterFullName);
+      }),
+    });
 
 export const sshPublicKeyValidationSchema = Yup.string().test(
   'ssh-public-key',
