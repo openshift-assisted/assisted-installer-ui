70b70cec1059772dc91890fa1b1743b824ac12cf
Julie Gyselová <jgyselov@redhat.com>
2021-07-26 18:41:43 +0200

    Remove React Fragment completely
    Co-authored-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/common/components/ui/PrismCode.tsx
+++ src/common/components/ui/PrismCode.tsx
@@ -17,27 +17,25 @@ const PrismCode: React.FC<PrismCodeProps> = ({
 }) => (
   <Highlight {...defaultProps} code={code} language={language} theme={theme}>
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <>
-        <Text component={TextVariants.pre} className={className} style={style}>
-          {copiable && (
-            <ClipboardCopy
-              isReadOnly
-              variant={'inline-compact'}
-              onCopy={(e) => clipboardCopyFunc(e, code)}
-              style={{ float: 'right', background: 'inherit' }}
-            >
-              {}
-            </ClipboardCopy>
-          )}
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </Text>
-      </>
+      <Text component={TextVariants.pre} className={className} style={style}>
+        {copiable && (
+          <ClipboardCopy
+            isReadOnly
+            variant={'inline-compact'}
+            onCopy={(e) => clipboardCopyFunc(e, code)}
+            style={{ float: 'right', background: 'inherit' }}
+          >
+            {}
+          </ClipboardCopy>
+        )}
+        {tokens.map((line, i) => (
+          <div key={i} {...getLineProps({ line, key: i })}>
+            {line.map((token, key) => (
+              <span key={key} {...getTokenProps({ token, key })} />
+            ))}
+          </div>
+        ))}
+      </Text>
     )}
   </Highlight>
 );
