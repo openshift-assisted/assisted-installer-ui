4d92cf98bc5d7022cfe1df250db576d2b338038a
Rastislav Wagner <rawagner@redhat.com>
2021-08-23 10:18:45 +0200

    Get logs and events via k8s proxy

--- src/cim/components/ClusterDeployment/ClusterDeploymentProgress.tsx
+++ src/cim/components/ClusterDeployment/ClusterDeploymentProgress.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ClusterProgress from '../../../common/components/clusterDetail/ClusterProgress';
-import { EventListFetchProps } from '../../../common/types/events';
 import { AgentK8sResource } from '../../types/k8s/agent';
 import { AgentClusterInstallK8sResource } from '../../types/k8s/agent-cluster-install';
 import { ClusterDeploymentK8sResource } from '../../types/k8s/cluster-deployment';
+import { k8sProxyURL } from '../helpers/proxy';
 import { getAICluster } from '../helpers/toAssisted';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -48,18 +48,27 @@ const getAgentsProgressPercent = (agents: AgentK8sResource[] = []) => {
   return totalSteps ? Math.round((completedSteps / totalSteps) * 100) : 100;
 };
 
+const getEventsURL = (agentClusterInstall?: AgentClusterInstallK8sResource) => {
+  if (agentClusterInstall?.status?.debugInfo?.eventsURL) {
+    const eventsURL = new URL(agentClusterInstall.status?.debugInfo?.eventsURL);
+    return `${k8sProxyURL}${eventsURL.pathname}${eventsURL.search}`;
+  }
+  return null;
+};
+
 type ClusterDeploymentProgressProps = {
   clusterDeployment: ClusterDeploymentK8sResource;
   agentClusterInstall: AgentClusterInstallK8sResource;
   agents: AgentK8sResource[];
-  onFetchEvents: EventListFetchProps['onFetchEvents'];
+  // eslint-disable-next-line
+  fetchEvents: (url: string) => Promise<any>;
 };
 
 const ClusterDeploymentProgress = ({
   clusterDeployment,
   agentClusterInstall,
   agents,
-  onFetchEvents,
+  fetchEvents,
 }: ClusterDeploymentProgressProps) => {
   const agentsProgressPercent = React.useMemo(() => getAgentsProgressPercent(agents), [agents]);
 
@@ -68,7 +77,19 @@ const ClusterDeploymentProgress = ({
     <ClusterProgress
       totalPercentage={agentsProgressPercent}
       cluster={cluster}
-      onFetchEvents={onFetchEvents}
+      onFetchEvents={async (params, onSuccess, onError) => {
+        const eventsURL = getEventsURL(agentClusterInstall);
+        if (!eventsURL) {
+          onError('Cannot determine events URL');
+          return;
+        }
+        try {
+          const result = await fetchEvents(eventsURL);
+          onSuccess(result);
+        } catch (e) {
+          onError(e.message);
+        }
+      }}
     />
   );
 };
