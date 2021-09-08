1dccb775d2438b0965481764e4b290f2756806f4
Marek Libra <marek.libra@gmail.com>
2021-09-08 10:20:55 +0200

    Rename RESERVED_AGENT_LABEL_KEY

--- src/cim/components/common/constants.ts
+++ src/cim/components/common/constants.ts
@@ -9,7 +9,6 @@ export const AGENT_NOLOCATION_VALUE = 'NOLOCATION';
 
 // We need that in addition to the AgentClusterInstall's clusterDeploymentRefName
 // since the agentSelector in clusterDeployment expects exact match on the hosts included in the installation
-export const RESERVED_AGENT_LABEL_KEY =
-  'agentclusterinstalls.extensions.hive.openshift.io/cluster-reserved';
+export const RESERVED_AGENT_LABEL_KEY = 'agentclusterinstalls.extensions.hive.openshift.io/cluster';
 
 export const INFRAENV_AGENTINSTALL_LABEL_KEY = 'infraenvs.agent-install.openshift.io';
