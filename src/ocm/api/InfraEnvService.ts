12a292790fd000a42cfbcd2d3e57abd21b7dfd07
Jonathan Kilzl <jkilzi@redhat.com>
2021-09-20 01:42:58 +0300

    refactor: Moves create-cluster-related APIs to v2
    - /v2/default-config
    - /v2/domains/
    - /v2/openshift-versions

---
+++ src/ocm/api/InfraEnvService.ts
@@ -0,0 +1,11 @@
+import { AxiosPromise } from 'axios';
+import { InfraEnv, InfraEnvCreateParams, InfraEnvList } from '../../common/api/types';
+import { client } from './axiosClient';
+
+export const registerInfraEnv = (params: InfraEnvCreateParams): AxiosPromise<InfraEnv> =>
+  client.post('/v2/infra-envs', params);
+
+export const deregisterInfraEnv = (infraEnvId: string): AxiosPromise<void> =>
+  client.delete(`/v2/infra-envs/${infraEnvId}`);
+
+export const listInfraEnvs = (): AxiosPromise<InfraEnvList> => client.get('/v2/infra-envs/');
