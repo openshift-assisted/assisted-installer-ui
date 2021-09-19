c61fc623e51fe44903e257de1b1375ca96cbc6cd
Jonathan Kilzl <jkilzi@redhat.com>
2021-09-20 01:44:50 +0300

    refactor: Implements RegisterCluster flow w/cache mechanism

---
+++ src/ocm/adapters/LocalStorageBackedCache.ts
@@ -0,0 +1,61 @@
+const CACHE_KEY = 'ai-lib-cache';
+
+const update = (cache: Record<string, string>): void => {
+  localStorage.setItem(CACHE_KEY, JSON.stringify(cache));
+};
+
+const read = (): Record<string, string> => {
+  const EMPTY_CACHE = '{}';
+  const cache = localStorage.getItem(CACHE_KEY) || EMPTY_CACHE;
+
+  try {
+    return JSON.parse(cache) as Record<string, string>;
+  } catch {
+    return {};
+  }
+};
+
+const LocalStorageBackedCache: Storage = {
+  key(index: number): string | null {
+    return localStorage.key(index);
+  },
+
+  get length(): number {
+    return localStorage.length;
+  },
+
+  clear(): void {
+    localStorage.removeItem(CACHE_KEY);
+  },
+
+  getItem(key: string): string | null {
+    if (!key) {
+      return null;
+    }
+
+    const cache = read();
+    return cache[key] || null;
+  },
+
+  removeItem(key: string): void {
+    if (!key) {
+      return;
+    }
+
+    const cache = read();
+    delete cache[key];
+    update(cache);
+  },
+
+  setItem(key: string, value: string): void {
+    if (!key || !value) {
+      return;
+    }
+
+    const cache = read();
+    cache[key] = value;
+    update(cache);
+  },
+};
+
+export default LocalStorageBackedCache;
