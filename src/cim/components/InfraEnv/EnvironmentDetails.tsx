baf9eff474c9e1f44dd5dbc58b59db43629c08f0
Rastislav Wagner <rawagner@redhat.com>
2021-09-22 14:27:10 +0200

    Replace icon if ssh/pull secret is missing

--- src/cim/components/InfraEnv/EnvironmentDetails.tsx
+++ src/cim/components/InfraEnv/EnvironmentDetails.tsx
@@ -9,7 +9,7 @@ import {
   Title,
   TitleSizes,
 } from '@patternfly/react-core';
-import { CheckCircleIcon, CrossIcon } from '@patternfly/react-icons';
+import { CheckCircleIcon, ExclamationTriangleIcon } from '@patternfly/react-icons';
 import { global_palette_green_500 as okColor } from '@patternfly/react-tokens/dist/js/global_palette_green_500';
 import { global_warning_color_100 as warningColor } from '@patternfly/react-tokens/dist/js/global_warning_color_100';
 
@@ -90,7 +90,7 @@ const EnvironmentDetails: React.FC<EnvironmentDetailsProps> = ({ infraEnv, aiCon
                 {infraEnv.spec?.pullSecretRef ? (
                   <CheckCircleIcon color={okColor.value} />
                 ) : (
-                  <CrossIcon color={warningColor.value} />
+                  <ExclamationTriangleIcon color={warningColor.value} />
                 )}
                 &nbsp;Pull Secret
               </div>
@@ -98,7 +98,7 @@ const EnvironmentDetails: React.FC<EnvironmentDetailsProps> = ({ infraEnv, aiCon
                 {infraEnv.spec?.sshAuthorizedKey ? (
                   <CheckCircleIcon color={okColor.value} />
                 ) : (
-                  <CrossIcon color={warningColor.value} />
+                  <ExclamationTriangleIcon color={warningColor.value} />
                 )}
                 &nbsp;Public SSH Key
               </div>
