5634f8279388101f2beef0c603b43d5a8a0af1b2
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-04 12:08:46 +0300

    MGMT-7722: Clusters list page with v2 APIs (WIP) (#813)
    * refactor: WIP moving APIs to v2
    
    Migrated:
    - GET /v2/clusters
    - DELETE /v2/clusters/{cluster_id}
    
    * chore: enable node.js support in .idea

--- src/ocm/api/versions.ts
+++ src/ocm/api/versions.ts
@@ -2,7 +2,7 @@ import { AxiosPromise } from 'axios';
 import { client } from './axiosClient';
 import { ListVersions, OpenshiftVersions } from '../../common';
 
-export const getVersions = (): AxiosPromise<ListVersions> => client.get('/v1/component_versions');
+export const getVersions = (): AxiosPromise<ListVersions> => client.get('/v2/component-versions');
 
 export const getOpenshiftVersions = (): AxiosPromise<OpenshiftVersions> =>
   client.get('/v2/openshift-versions');
