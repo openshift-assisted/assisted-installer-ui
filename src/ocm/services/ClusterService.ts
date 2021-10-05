7115d509920926248eec96bb941006ef730cd9c5
jkilzi <jkilzi@redhat.com>
2021-10-05 16:32:12 +0300

    refactor: Implements delete cluster

---
+++ src/ocm/services/ClusterService.ts
@@ -0,0 +1,15 @@
+import type { AxiosPromise } from 'axios';
+import { Cluster } from '../../common';
+import { client } from '../api/axiosClient';
+
+const ClusterService = {
+  list(): AxiosPromise<Cluster[]> {
+    return client.get('/v2/clusters');
+  },
+
+  deregister(id: string): AxiosPromise<void> {
+    return client.delete(`/v2/clusters/${id}`);
+  },
+};
+
+export default ClusterService;
