9cd4618ce6b51e0d2429715ef94275181f2baac5
jkilzi <jkilzi@redhat.com>
2021-10-11 22:36:53 +0300

    test: removes common/hacks.ts Logging the lib version is AssistedUILibVersion.tsx duty

--- src/common/hacks.ts
+++
@@ -1,3 +0,0 @@
-import { getAssistedUiLibVersion } from './config';
-
-console.log('openshift-assisted-ui-lib', `v${getAssistedUiLibVersion()}`);
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,5 +1,4 @@
 import './styles.css';
-import './hacks';
 
 export * from './components';
 export * from './api';
