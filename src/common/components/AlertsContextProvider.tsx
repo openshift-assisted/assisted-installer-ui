f397fbe24a1f6db6434722dce0155ee8eac56aa3
jkilzi <jkilzi@redhat.com>
2021-10-12 20:07:05 +0300

    refactor: exposes AlertsContext only through useAlerts hook
    Signed-off-by: jkilzi <jkilzi@redhat.com>

--- src/common/components/AlertsContextProvider.tsx
+++ src/common/components/AlertsContextProvider.tsx
@@ -15,28 +15,22 @@ export type AlertsContextType = {
   clearAlerts: () => void;
 };
 
-export const AlertsContext = React.createContext<AlertsContextType>({
-  alerts: [],
-  addAlert: () => null,
-  removeAlert: () => null,
-  clearAlerts: () => null,
-});
+const AlertsContext = React.createContext<AlertsContextType | null>(null);
 
 export const AlertsContextProvider: React.FC = ({ children }) => {
-  const [alerts, dispatchAlertsAction] = React.useReducer(alertsReducer, []);
-  const addAlert = (alert: AlertPayload) => dispatchAlertsAction(addAlertAction(alert));
-  const removeAlert = (alertKey: string) => dispatchAlertsAction(removeAlertAction(alertKey));
-  const clearAlerts = () => dispatchAlertsAction(clearAlertsAction());
-  return (
-    <AlertsContext.Provider value={{ alerts, addAlert, removeAlert, clearAlerts }}>
-      {children}
-    </AlertsContext.Provider>
-  );
+  const [alerts, dispatch] = React.useReducer(alertsReducer, []);
+  const context = {
+    alerts,
+    addAlert: React.useCallback((alert: AlertPayload) => dispatch(addAlertAction(alert)), []),
+    removeAlert: React.useCallback((alertKey: string) => dispatch(removeAlertAction(alertKey)), []),
+    clearAlerts: React.useCallback(() => dispatch(clearAlertsAction()), []),
+  };
+  return <AlertsContext.Provider value={context}>{children}</AlertsContext.Provider>;
 };
 
 export const useAlerts = () => {
   const context = React.useContext(AlertsContext);
-  if (context === undefined) {
+  if (!context) {
     throw new Error('useAlerts must be used within AlertsContextProvider');
   }
   return context;
