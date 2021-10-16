64cfb0f2b2dc8e2c0fc4dd95ef03434d19356391
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-16 05:06:42 +0300

    fix: export ocm.services.apis namespace
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/services/index.ts
+++ src/ocm/services/index.ts
@@ -2,3 +2,4 @@ export { default as ClustersService } from './ClustersService';
 export { default as ClusterDetailsService } from './ClusterDetailsService';
 export { default as HostsService } from './HostsService';
 export { default as InfraEnvsService } from './InfraEnvsService';
+export * as APIs from './apis';
