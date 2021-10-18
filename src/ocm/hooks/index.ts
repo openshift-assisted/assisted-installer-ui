6d6985fc6cc1f51b23f60f12fa7ca6077dfe11a6
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-18 14:54:42 +0300

    refactor: adds useInfraEnvId hook
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/hooks/index.ts
+++ src/ocm/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useOpenshiftVersions } from './useOpenshiftVersions';
 export { default as useClusterSupportedPlatforms } from './useClusterSupportedPlatforms';
 export { default as useManagedDomains } from './useManagedDomains';
 export { default as useUsedClusterNames } from './useUsedClusterNames';
+export { default as useInfraEnvId } from './useInfraEnvId';
