a2798d7a2e0cbe660a052748f75d40f4718c8dc0
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-18 04:19:38 +0300

    refactor: updates ocm/services/apis/index.ts
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/services/apis/index.ts
+++ src/ocm/services/apis/index.ts
@@ -1,5 +1,6 @@
-export { default as VersionsAPI } from './VersionsAPI';
 export { default as ManagedDomainsAPI } from './ManagedDomainsAPI';
+export { default as SupportedOpenshiftVersionsAPI } from './SupportedOpenshiftVersionsAPI';
+export { default as ComponentVersionsAPI } from './ComponentVersionsAPI';
 export { default as ClustersAPI } from './ClustersAPI';
 export { default as HostsAPI } from './HostsAPI';
 export { default as InfraEnvsAPI } from './InfraEnvsAPI';
