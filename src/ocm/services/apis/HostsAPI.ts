4a5c8e9bb708a11effc8a6c3ed11720c18adcfc9
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-18 04:17:56 +0300

    refactor: updates HostsAPI.ts
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/services/apis/HostsAPI.ts
+++ src/ocm/services/apis/HostsAPI.ts
@@ -1,29 +1,30 @@
 import { Host, HostUpdateParams, InfraEnv } from '../../../common';
 import { client } from '../../api';
 import { AxiosResponse } from 'axios';
+import InfraEnvsAPI from './InfraEnvsAPI';
 
 const HostsAPI = {
-  getBaseURI(infraEnvId: InfraEnv['id'], hostId?: Host['id']) {
-    return `/v2/infra-envs/${infraEnvId}/hosts${hostId ? `/${hostId}` : ''}`;
+  makeBaseURI(infraEnvId: InfraEnv['id'], hostId?: Host['id']) {
+    return `${InfraEnvsAPI.makeBaseURI(infraEnvId)}/hosts${hostId ? hostId : ''}`;
   },
 
   list(infraEnvId: InfraEnv['id']) {
-    return client.get<Host[]>(`${HostsAPI.getBaseURI(infraEnvId)}`);
+    return client.get<Host[]>(`${HostsAPI.makeBaseURI(infraEnvId)}`);
   },
 
   get(infraEnvId: InfraEnv['id'], hostId: Host['id']) {
-    return client.get<Host>(`${HostsAPI.getBaseURI(infraEnvId, hostId)}`);
+    return client.get<Host>(`${HostsAPI.makeBaseURI(infraEnvId, hostId)}`);
   },
 
   update(infraEnvId: InfraEnv['id'], hostId: Host['id'], params: HostUpdateParams) {
     return client.patch<HostUpdateParams, AxiosResponse<Host>>(
-      `${HostsAPI.getBaseURI(infraEnvId, hostId)}`,
+      `${HostsAPI.makeBaseURI(infraEnvId, hostId)}`,
       params,
     );
   },
 
   deregister(infraEnvId: InfraEnv['id'], hostId: Host['id']) {
-    return client.delete<void>(`${HostsAPI.getBaseURI(infraEnvId, hostId)}`);
+    return client.delete<void>(`${HostsAPI.makeBaseURI(infraEnvId, hostId)}`);
   },
 };
 
