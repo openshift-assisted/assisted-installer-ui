f78dbcbe16878cba76be165afecc893f54e07bdb
jgyselov <jgyselov@redhat.com>
2021-10-18 17:12:21 +0200

    Added back api/versions.ts

--- src/ocm/api/index.ts
+++ src/ocm/api/index.ts
@@ -1,7 +1,8 @@
 export * from './axiosClient';
 export * from './utils';
-
+export * from './versions';
 export * from './clusters';
+// export * from './domains';
 export * from './addHostsClusters';
 
 export { default as useApi } from './useApi';
