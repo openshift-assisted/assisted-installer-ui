dcc3f70365b1155e99c2f9b2e0a53d8707db97a5
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-20 12:54:49 +0300

    test: updates ocm/integration/utils/types.ts - adds route options to render
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/integration/utils/types.ts
+++ src/ocm/integration/utils/types.ts
@@ -2,7 +2,9 @@ import { RenderOptions } from '@testing-library/react';
 import { ConfigureStoreOptions } from '@reduxjs/toolkit';
 import { AssistedInstallerUILibRootStore } from '../../store';
 
-export type IntegrationTestsRenderOptions = Omit<RenderOptions, 'wrapper'> & {
+export type IntegrationTestsRenderOptions<C = never> = Omit<RenderOptions, 'wrapper'> & {
   preloadedState?: ConfigureStoreOptions['preloadedState'];
   store?: AssistedInstallerUILibRootStore;
+  route?: string;
+  routeContext?: C;
 };
