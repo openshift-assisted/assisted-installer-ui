4bf828f40ca3cd780873b5042ff35d42ec5f0c26
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-20 15:15:10 +0300

    refactor: adds DiscoveryImageFormService.ts
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/services/index.ts
+++ src/ocm/services/index.ts
@@ -4,4 +4,5 @@ export { default as HostsService } from './HostsService';
 export { default as HostDiscoveryService } from './HostDiscoveryService';
 export { default as InfraEnvsService } from './InfraEnvsService';
 export { default as APIVersionService } from './APIVersionService';
+export { default as DiscoveryImageFormService } from './DiscoveryImageFormService';
 export * as APIs from './apis';
