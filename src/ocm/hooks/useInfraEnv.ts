d23a013380b271f4a3698ad2320683bc2e461de6
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-21 19:09:54 +0300

    refactor: define the async effects outside the effect callback

--- src/ocm/hooks/useInfraEnv.ts
+++ src/ocm/hooks/useInfraEnv.ts
@@ -7,28 +7,28 @@ import { InfraEnvsAPI } from '../services/apis';
 export default function useInfraEnv(clusterId: Cluster['id']) {
   const [infraEnv, setInfraEnv] = React.useState<InfraEnv>();
   const [error, setError] = React.useState<APIErrorMixin & Error>();
-
   const { infraEnvId, error: infraEnvIdError } = useInfraEnvId(clusterId);
+
+  const getInfraEnv = React.useCallback(async () => {
+    try {
+      if (infraEnvId) {
+        const { data: infraEnv } = await InfraEnvsAPI.get(infraEnvId);
+        setInfraEnv(infraEnv);
+      }
+    } catch (e) {
+      setError(e);
+    }
+  }, [infraEnvId]);
 
   React.useEffect(() => {
     if (infraEnvIdError) {
       setError(infraEnvIdError);
     } else {
-      const getInfraEnv = async () => {
-        try {
-          if (infraEnvId) {
-            const { data: infraEnv } = await InfraEnvsAPI.get(infraEnvId);
-            setInfraEnv(infraEnv);
-          }
-        } catch (e) {
-          setError(e);
-        }
-      };
       if (!infraEnv) {
         void getInfraEnv();
       }
     }
-  }, [infraEnv, infraEnvId, infraEnvIdError]);
+  }, [getInfraEnv, infraEnv, infraEnvId, infraEnvIdError]);
 
   return { infraEnv, error, isLoading: !infraEnv && !error };
 }
