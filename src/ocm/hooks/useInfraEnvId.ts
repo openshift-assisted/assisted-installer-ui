95ff39203d3ae6b6cf6a4774c606ebf2ceeec1d1
Jonathan Kilzi <jkilzi@redhat.com>
2021-10-21 19:09:54 +0300

    refactor: define the async effects outside the effect callback

--- src/ocm/hooks/useInfraEnvId.ts
+++ src/ocm/hooks/useInfraEnvId.ts
@@ -6,19 +6,21 @@ import { InfraEnvsService } from '../services';
 export default function useInfraEnvId(clusterId: Cluster['id']) {
   const [infraEnvId, setInfraEnv] = React.useState<InfraEnv['id']>();
   const [error, setError] = React.useState<APIErrorMixin & Error>();
+
+  const findInfraEnvId = React.useCallback(async () => {
+    try {
+      const infraEnvId = await InfraEnvsService.getInfraEnvId(clusterId);
+      setInfraEnv(infraEnvId);
+    } catch (e) {
+      setError(e);
+    }
+  }, [clusterId]);
+
   React.useEffect(() => {
-    const findInfraEnvId = async () => {
-      try {
-        const infraEnvId = await InfraEnvsService.getInfraEnvId(clusterId);
-        setInfraEnv(infraEnvId);
-      } catch (e) {
-        setError(e);
-      }
-    };
     if (!infraEnvId) {
       void findInfraEnvId();
     }
-  }, [clusterId, infraEnvId]);
+  }, [clusterId, findInfraEnvId, infraEnvId]);
 
   return { infraEnvId, error };
 }
--- src/ocm/hooks/useUsedClusterNames.ts
+++ src/ocm/hooks/useUsedClusterNames.ts
@@ -3,33 +3,32 @@ import { getErrorMessage, handleApiError } from '../api';
 import { ClustersAPI } from '../services/apis';
 import { Cluster, useAlerts } from '../../common';
 
-export default function useUsedClusterNames(
-  clusterId: Cluster['id'],
-  hookDeps?: React.DependencyList,
-) {
-  const { addAlert } = useAlerts();
+export default function useUsedClusterNames(clusterId: Cluster['id']) {
   const [usedClusterNames, setUsedClusterNames] = React.useState<string[]>();
+  const { addAlert } = useAlerts();
+
+  const fetcher = React.useCallback(async () => {
+    try {
+      const { data: clusters } = await ClustersAPI.list();
+      const names = clusters
+        .filter((c) => c.id !== clusterId)
+        .map((c) => `${c.name}.${c.baseDnsDomain}`);
+      setUsedClusterNames(names);
+    } catch (e) {
+      setUsedClusterNames([]);
+      handleApiError(e, () =>
+        addAlert({
+          title: 'Failed to retrieve names of existing clusters.',
+          message: getErrorMessage(e),
+        }),
+      );
+    }
+  }, [addAlert, clusterId]);
+
   React.useEffect(() => {
-    const fetcher = async () => {
-      try {
-        const { data: clusters } = await ClustersAPI.list();
-        const names = clusters
-          .filter((c) => c.id !== clusterId)
-          .map((c) => `${c.name}.${c.baseDnsDomain}`);
-        setUsedClusterNames(names);
-      } catch (e) {
-        setUsedClusterNames([]);
-        handleApiError(e, () =>
-          addAlert({
-            title: 'Failed to retrieve names of existing clusters.',
-            message: getErrorMessage(e),
-          }),
-        );
-      }
-    };
     void fetcher();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, hookDeps);
+  }, []);
 
   return { usedClusterNames };
 }
