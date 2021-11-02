960d938c92a027e806e97a963074195f2b079550
Jiri Tomasek <jtomasek@redhat.com>
2021-11-02 09:43:00 +0100

    Replace microbundle-crl with esbuild

---
+++ scripts/esbuild/common-config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { dtsPlugin } = require('esbuild-plugin-d.ts');
+const pkg = require('../../package.json');
+
+module.exports = {
+  entryPoints: ['src/index.ts'],
+  // don't bundle dependences and peerDependencies https://github.com/evanw/esbuild/issues/727#issuecomment-771743582
+  external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies || {})],
+  bundle: true,
+  sourcemap: true,
+  outdir: 'dist',
+  format: 'esm',
+  target: ['esnext'],
+  plugins: [dtsPlugin()], // requires "noEmit": false in tsconfig.json to successfully emit types
+};
