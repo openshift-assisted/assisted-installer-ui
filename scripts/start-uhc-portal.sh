b54c90efe3315eaf1fa0850f8176005ab483146b
Jonathan Kilzi <jkilzi@redhat.com>
2021-11-17 15:17:00 +0200

    chore: updates start-uhc-portal.sh
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- scripts/start-uhc-portal.sh
+++ scripts/start-uhc-portal.sh
@@ -3,4 +3,4 @@
 node_modules/.bin/concurrently \
   -n "start-lib,sync-dist,start-uhc" \
   -c "magenta,cyan,yellow" \
-  "yarn start" "yarn sync-to-ui" "cd ../uhc-portal; yarn dev-server --env noproxy api-env=staging"
+  "yarn start" "yarn sync-dist" "yarn --cwd ../uhc-portal start"
