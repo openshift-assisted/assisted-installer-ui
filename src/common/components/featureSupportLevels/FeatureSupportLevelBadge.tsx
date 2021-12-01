03ac69b3de1b0a620d6d6dbceeef11ef9cceb16f
Bat Zion Rotman <brotman@redhat.com>
2021-12-01 16:56:38 +0200

    preiew badge improvements

--- src/common/components/featureSupportLevels/FeatureSupportLevelBadge.tsx
+++ src/common/components/featureSupportLevels/FeatureSupportLevelBadge.tsx
@@ -1,37 +1,18 @@
-import { Popover } from '@patternfly/react-core';
 import React from 'react';
-import { TECH_SUPPORT_LEVEL_LINK } from '../../config/constants';
 import { FeatureId, SupportLevel, isPreviewSupportLevel } from '../../types';
-import ExternalLink from '../ui/ExternalLink';
-import {
-  TechnologyPreview,
-  DeveloperPreview,
-  PreviewBadgePosition,
-  DeveloperPreviewProps,
-} from '../ui/PreviewBadge';
+import { TechnologyPreview, DeveloperPreview } from '../ui/PreviewBadge';
 import FeatureSupportLevelContext from './FeatureSupportLevelContext';
 
 export type SupportLevelBadgeProps = {
   featureId: FeatureId;
   openshiftVersion: string | undefined;
-} & DeveloperPreviewProps;
-
-const popoverTexts = {
-  'tech-preview': `Technology preview features provide early access to upcoming product innovations, 
-  enabling you to test functionality and provide feedback during the development process.`,
-  'dev-preview': `Developer Preview features provide early access to upcoming innovations and contain 
-  a functional set of features that Red Hat is encouraging customers to use and provide feedback on.`,
 };
 
 const FeatureSupportLevelBadge: React.FC<SupportLevelBadgeProps> = ({
   featureId,
   openshiftVersion,
-  className = 'pf-u-ml-md',
-  position = PreviewBadgePosition.inline,
-  ...props
 }) => {
   const featureSupportLevelData = React.useContext(FeatureSupportLevelContext);
-
   const supportLevel: SupportLevel | undefined = React.useMemo(() => {
     if (!openshiftVersion) {
       return undefined;
@@ -41,25 +22,7 @@ const FeatureSupportLevelBadge: React.FC<SupportLevelBadgeProps> = ({
   if (!isPreviewSupportLevel(supportLevel)) {
     return null;
   }
-  const Component = supportLevel === 'tech-preview' ? TechnologyPreview : DeveloperPreview;
-  const bodyContent = (
-    <>
-      <p>
-        {popoverTexts[supportLevel]}
-        {supportLevel === 'tech-preview' && (
-          <>
-            <br />
-            <ExternalLink href={TECH_SUPPORT_LEVEL_LINK}>Learn more</ExternalLink>
-          </>
-        )}
-      </p>
-    </>
-  );
-  return (
-    <Popover bodyContent={bodyContent}>
-      <Component position={position} className={className} {...props} />
-    </Popover>
-  );
+  return supportLevel === 'tech-preview' ? <TechnologyPreview /> : <DeveloperPreview />;
 };
 
 export default FeatureSupportLevelBadge;
--- src/common/components/ui/PreviewBadge.tsx
+++ src/common/components/ui/PreviewBadge.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Label, LabelProps } from '@patternfly/react-core';
+import { Label, Popover } from '@patternfly/react-core';
+import { InfoCircleIcon } from '@patternfly/react-icons';
+import { TECH_SUPPORT_LEVEL_LINK } from '../../config/constants';
+import ExternalLink from './ExternalLink';
 
 export enum PreviewBadgePosition {
   default,
@@ -7,15 +10,21 @@ export enum PreviewBadgePosition {
   inlineRight,
 }
 
-export type DeveloperPreviewProps = {
+type PreviewBadgeProps = {
   position?: PreviewBadgePosition;
   className?: string;
+  text: string;
+  externalLink?: string;
+  popoverText: string;
 };
-type TechnologyPreviewProps = DeveloperPreviewProps;
 
-const PreviewBadge: React.FC<
-  DeveloperPreviewProps & { text: string; color?: LabelProps['color'] }
-> = ({ position = PreviewBadgePosition.inlineRight, className = '', color = 'orange', text }) => {
+const PreviewBadge: React.FC<PreviewBadgeProps> = ({
+  position = PreviewBadgePosition.inline,
+  className = 'pf-u-ml-md',
+  text,
+  popoverText,
+  externalLink,
+}) => {
   let clsName = className;
   switch (position) {
     case PreviewBadgePosition.inlineRight:
@@ -25,22 +34,52 @@ const PreviewBadge: React.FC<
       clsName += ' pf-u-display-inline';
       break;
   }
+  const bodyContent = (
+    <>
+      <div style={{ marginBottom: 'var(--pf-global--spacer--sm)' }}>{popoverText}</div>
+      {externalLink && (
+        <>
+          <ExternalLink href={externalLink}>Learn more</ExternalLink>
+        </>
+      )}
+    </>
+  );
 
   return (
-    <Label
-      className={clsName}
-      color={color}
-      onClick={(e) => e.preventDefault()}
-      style={{ cursor: 'pointer' }}
-    >
-      {text}
-    </Label>
+    <Popover bodyContent={bodyContent} position="top">
+      <Label
+        style={{ cursor: 'pointer' }}
+        color="orange"
+        onClick={(e) => e.preventDefault()}
+        icon={<InfoCircleIcon color="var(--pf-c-label__content--Color)" />}
+        className={clsName}
+      >
+        {text}
+      </Label>
+    </Popover>
   );
 };
 
+export type DeveloperPreviewProps = Pick<PreviewBadgeProps, 'className' | 'position'>;
+
+export type TechnologyPreviewProps = DeveloperPreviewProps;
+
+const popoverTexts = {
+  'tech-preview': `Technology preview features provide early access to upcoming product innovations, 
+  enabling you to test functionality and provide feedback during the development process.`,
+  'dev-preview': `Developer Preview features provide early access to upcoming innovations and contain 
+  a functional set of features that Red Hat is encouraging customers to use and provide feedback on.`,
+};
+
 export const DeveloperPreview: React.FC<DeveloperPreviewProps> = (props) => (
-  <PreviewBadge {...props} text="Developer Preview" />
+  <PreviewBadge text="Developer Preview" popoverText={popoverTexts['dev-preview']} {...props} />
 );
+
 export const TechnologyPreview: React.FC<TechnologyPreviewProps> = (props) => (
-  <PreviewBadge {...props} text="Technology Preview" />
+  <PreviewBadge
+    text="Technology Preview"
+    popoverText={popoverTexts['tech-preview']}
+    externalLink={TECH_SUPPORT_LEVEL_LINK}
+    {...props}
+  />
 );
