e3268693e58eaa41dc4199d6eedc29a180c91fba
Jonathan Kilzi <jkilzi@redhat.com>
2021-12-02 12:10:05 +0200

    refactor: moves FeatureSupportLevelsAPI.ts to services/apis
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/services/apis/index.ts
+++ src/ocm/services/apis/index.ts
@@ -5,3 +5,4 @@ export { default as ClustersAPI } from './ClustersAPI';
 export { default as HostsAPI } from './HostsAPI';
 export { default as InfraEnvsAPI } from './InfraEnvsAPI';
 export { default as EventsAPI } from './EventsAPI';
+export { default as FeatureSupportLevelsAPI } from './FeatureSupportLevelsAPI';
