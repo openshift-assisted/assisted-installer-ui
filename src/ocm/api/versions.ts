5e193e8480e4865e7ff662af05205ccc1ca744ac
Jonathan Kilzi <jkilzi@redhat.com>
2021-12-02 21:54:57 +0200

    refactor: leftovers post v2 migration
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/api/versions.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosPromise } from 'axios';
-import { client } from './axiosClient';
-import { ListVersions, OpenshiftVersions } from '../../common';
-
-export const getVersions = (): AxiosPromise<ListVersions> => client.get('/v2/component_versions');
-
-export const getOpenshiftVersions = (): AxiosPromise<OpenshiftVersions> =>
-  client.get('/v2/openshift_versions');
