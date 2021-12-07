1c7fef43dca953bba04a1d49c1d5ed18f80056e6
jgyselov <jgyselov@redhat.com>
2021-12-07 10:38:33 +0100

    Add firewall config helper link

--- src/common/config/constants.ts
+++ src/common/config/constants.ts
@@ -21,6 +21,9 @@ export const FEEDBACK_FORM_LINK =
 
 export const TECH_SUPPORT_LEVEL_LINK = 'https://access.redhat.com/support/offerings/techpreview';
 
+export const FIREWALL_DOCUMENTATION_LINK =
+  'https://docs.openshift.com/container-platform/4.8/installing/installing_bare_metal_ipi/ipi-install-installation-workflow.html#ipi-install-setting-proxy-settings-within-install-config_ipi-install-configuration-files';
+
 export const getOcpConsoleNodesPage = (ocpConsoleUrl: string) =>
   `${ocpConsoleUrl}/k8s/cluster/nodes`;
 
