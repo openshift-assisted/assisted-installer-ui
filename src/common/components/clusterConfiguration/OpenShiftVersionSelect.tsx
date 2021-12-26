3c43b4b77690d3dde4c260b53e8ec258be13d37f
Jonathan Kilzi <jkilzi@redhat.com>
2021-12-26 15:17:20 +0200

    Revert "Swich SNO checkbox and OCP version select positions (#1037)" (#1053)
    This reverts commit 7c2b96b2e8f3c28a8b4143724c8f68c2d587b7d6.

--- src/common/components/clusterConfiguration/OpenShiftVersionSelect.tsx
+++ src/common/components/clusterConfiguration/OpenShiftVersionSelect.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useFormikContext } from 'formik';
 import { ExclamationTriangleIcon, ExternalLinkAltIcon } from '@patternfly/react-icons';
 import { global_warning_color_100 as warningColor } from '@patternfly/react-tokens';
 import { OPENSHIFT_LIFE_CYCLE_DATES_LINK } from '../../config';
+import { ClusterCreateParams } from '../../api';
 import { OpenshiftVersionOptionType } from '../../types';
-import { SelectField } from '../ui';
+import { isSNOSupportedVersion, SelectField } from '../ui';
 
 const OpenShiftLifeCycleDatesLink = () => (
   <a href={OPENSHIFT_LIFE_CYCLE_DATES_LINK} target="_blank" rel="noopener noreferrer">
@@ -31,15 +33,32 @@ type OpenShiftVersionSelectProps = {
   versions: OpenshiftVersionOptionType[];
 };
 const OpenShiftVersionSelect: React.FC<OpenShiftVersionSelectProps> = ({ versions }) => {
+  const {
+    values: { highAvailabilityMode, openshiftVersion },
+    setFieldValue,
+  } = useFormikContext<ClusterCreateParams>();
+
+  const selectedVersion = versions.find((v) => v.value === openshiftVersion) || versions[0];
+  React.useEffect(() => {
+    if (highAvailabilityMode === 'None' && !isSNOSupportedVersion(selectedVersion)) {
+      const firstSupportedVersionValue = versions.find(isSNOSupportedVersion)?.value;
+      setFieldValue('openshiftVersion', firstSupportedVersionValue);
+    }
+  }, [highAvailabilityMode, selectedVersion, setFieldValue, versions]);
+
   const selectOptions = React.useMemo(
     () =>
       versions
-        .filter((version) => version.supportLevel !== 'maintenance')
+        .filter(
+          (version) =>
+            version.supportLevel !== 'maintenance' &&
+            (highAvailabilityMode !== 'None' || isSNOSupportedVersion(version)),
+        )
         .map((version) => ({
           label: version.label,
           value: version.value,
         })),
-    [versions],
+    [versions, highAvailabilityMode],
   );
 
   return (
