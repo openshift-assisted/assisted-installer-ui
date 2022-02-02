24485e8afc4e111918b3421e4e4fbee150e02eda
Rastislav Wagner <rawagner@redhat.com>
2022-02-02 11:19:01 +0100

    Update approval popover

--- src/cim/components/Agent/AgentStatus.tsx
+++ src/cim/components/Agent/AgentStatus.tsx
@@ -1,4 +1,4 @@
-import { Button, Popover } from '@patternfly/react-core';
+import { Button, Popover, Stack, StackItem } from '@patternfly/react-core';
 import * as React from 'react';
 import { getHostname, HostStatus } from '../../../common';
 import { AgentK8sResource } from '../../types';
@@ -19,7 +19,6 @@ const AgentStatus: React.FC<AgentStatusProps> = ({ agent, onApprove, onEditHostn
   const editHostname = onEditHostname ? () => onEditHostname(agent) : undefined;
   const pendingApproval = !agent.spec.approved;
 
-  const macAddress = agent.status?.inventory?.interfaces?.[0]?.macAddress;
   const hostname = getHostname(host, agent.status?.inventory || {});
 
   const [status, , validationsInfo] = getAgentStatus(agent);
@@ -38,10 +37,12 @@ const AgentStatus: React.FC<AgentStatusProps> = ({ agent, onApprove, onEditHostn
           maxWidth="50rem"
           headerContent={<div>Approve host to join infrastructure environment</div>}
           bodyContent={
-            <>
-              {hostname && <div>Hostname: {hostname}</div>}
-              {macAddress && <div>MAC address: {macAddress}</div>}
-            </>
+            <Stack hasGutter>
+              <StackItem>
+                Make sure that you expect and recognize the host before approving.
+              </StackItem>
+              <StackItem>{hostname && <>Hostname: {hostname}</>}</StackItem>
+            </Stack>
           }
           footerContent={
             <Button variant="link" onClick={() => onApprove(agent)} isInline>
