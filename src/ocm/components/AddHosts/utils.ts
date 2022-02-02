107e23fd2266c6fc5e7edc013ef5c900863010b7
jgyselov <jgyselov@redhat.com>
2022-02-02 13:04:27 +0100

    Rework canAddHost condition

--- src/ocm/components/AddHosts/utils.ts
+++ src/ocm/components/AddHosts/utils.ts
@@ -1,27 +1,22 @@
+import { isSNO } from '../../selectors';
 import Day2ClusterService from '../../services/Day2ClusterService';
 import { OcmClusterType } from './types';
 
 export const canAddHost = ({ cluster }: { cluster: OcmClusterType }) => {
-  let isAllowed = false;
-
   if (Day2ClusterService.getOpenshiftClusterId(cluster)) {
-    // Collision of "ready" states for AI and Telemetry
-    // Show the tab only if telemetry reports "ready", not the AI
     if (
-      (cluster.state === 'ready' &&
-        cluster.product?.id === 'OCP-AssistedInstall' &&
-        (!cluster.aiCluster || cluster.aiCluster.status === 'installed')) ||
-      // Backward compatibility for old AI clusters (when the subscription was created by
-      // the telemetry instead of by the AI-Service)
-      (cluster.product?.id !== 'OCP-AssistedInstall' && cluster.cloud_provider?.id === 'baremetal')
+      cluster.aiCluster &&
+      cluster.aiCluster.status === 'installed' &&
+      !isSNO(cluster.aiCluster)
     ) {
-      isAllowed = true;
-    }
-
-    if (cluster.aiCluster && cluster.aiCluster['high_availability_mode'] === 'None') {
-      isAllowed = false;
+      return true;
+    } else if (
+      (cluster.state === 'ready' && cluster.product?.id == 'OCP-AssistedInstaller') ||
+      cluster.cloud_provider?.id == 'baremetal'
+    ) {
+      return true;
     }
   }
 
-  return isAllowed;
+  return false;
 };
