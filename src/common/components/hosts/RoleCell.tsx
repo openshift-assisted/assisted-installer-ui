667a4a8d7dab611010f46eb981d4e45eda6b3c7b
Jonathan Kilzi <jkilzi@redhat.com>
2022-02-03 14:09:08 +0200

    Revert "MGMT-9067: Implement the UI for auto-select control plane nodes (#1097)"
    This reverts commit 397a7d136646ed2ee205f624d558d07a904bb8fa.

--- src/common/components/hosts/RoleCell.tsx
+++ src/common/components/hosts/RoleCell.tsx
@@ -1,4 +1,3 @@
-import { Tooltip } from '@patternfly/react-core';
 import React from 'react';
 import { Host, HostUpdateParams } from '../../api';
 import RoleDropdown from './RoleDropdown';
@@ -9,24 +8,13 @@ export type RoleCellProps = {
   readonly?: boolean;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onEditRole?: (role: HostUpdateParams['hostRole']) => Promise<any>;
-  displayTooltip?: boolean;
 };
 
-const RoleCell: React.FC<RoleCellProps> = ({
-  host,
-  role,
-  readonly = false,
-  onEditRole,
-  displayTooltip,
-}) =>
+const RoleCell: React.FC<RoleCellProps> = ({ host, role, readonly = false, onEditRole }) =>
   !readonly && onEditRole ? (
     <RoleDropdown host={host} onEditRole={onEditRole} current={role} />
-  ) : displayTooltip ? (
-    <Tooltip content={'To make the role editable, 4 or more hosts need to be discovered.'}>
-      <span>{role}</span>
-    </Tooltip>
   ) : (
-    <span>{role}</span>
+    <>{role}</>
   );
 
 export default RoleCell;
