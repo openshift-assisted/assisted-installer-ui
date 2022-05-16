a0c609e2ff2839a03ca70706bbdee5b560f499da
Jiri Tomasek <jtomasek@redhat.com>
2022-05-16 13:03:56 +0200

    Publish with yalc after build in dev environment
    - removes obsolete sync-dist.sh script

--- scripts/esbuild/dev.js
+++ scripts/esbuild/dev.js
@@ -1,6 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require('esbuild');
 const commonConfig = require('./common-config');
+const { publishPackage } = require('yalc');
+const { join } = require('path');
+
+function publishWithYalc() {
+  try {
+    publishPackage({
+      workingDir: join(process.cwd() || ''),
+      push: true,
+      replace: true,
+      changed: true,
+    });
+  } catch (e) {
+    console.log('Failed to publish the package with Yalc:', e);
+  }
+}
 
 esbuild
   .build({
@@ -8,8 +23,14 @@ esbuild
     watch: {
       onRebuild(error, result) {
         if (error) console.error('Watch build failed:', error);
-        else console.log('Watch build succeeded:', result);
+        else {
+          console.log('Watch build succeeded:', result);
+          publishWithYalc();
+        }
       },
     },
   })
-  .then((result) => console.log('Build succeeded and started watching', result));
+  .then((result) => {
+    console.log('Build succeeded and started watching', result);
+    publishWithYalc();
+  });
