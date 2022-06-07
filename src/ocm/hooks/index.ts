bd4fdb85792feaedd5520932d7fcfb12c1ff7ac9
Jonathan Kilzi <jkilzi@redhat.com>
2022-06-07 03:01:05 +0300

    Move useClusterPreflightRequirements to its own file

--- src/ocm/hooks/index.ts
+++ src/ocm/hooks/index.ts
@@ -4,3 +4,4 @@ export { default as useManagedDomains } from './useManagedDomains';
 export { default as useUsedClusterNames } from './useUsedClusterNames';
 export { default as useInfraEnvId } from './useInfraEnvId';
 export { default as usePullSecret } from './usePullSecret';
+export { default as useClusterPreflightRequirements } from './useClusterPreflightRequirements';
