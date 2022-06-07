7cb6ce5dda5c57188ac41fa07cce8acf7a121082
Jonathan Kilzi <jkilzi@redhat.com>
2022-06-07 08:49:59 +0300

    adds HostRequirementsContent.tsx
    removes unused vars from HostRequirementsContent.tsx

--- src/ocm/components/hosts/HostRequirementsContent.tsx
+++ src/ocm/components/hosts/HostRequirementsContent.tsx
@@ -1,62 +1,23 @@
 import React from 'react';
 import { List, ListItem, Text, TextContent } from '@patternfly/react-core';
+import { Cluster, ErrorState, LoadingState, fileSize } from '../../../common';
+import { useClusterPreflightRequirements } from '../../hooks';
+import ExternalLink from '../../../common/components/ui/ExternalLink';
 
-import {
-  Cluster,
-  PreflightHardwareRequirements,
-  OPERATOR_NAME_CNV,
-  ErrorState,
-  LoadingState,
-  fileSize,
-  RenderIf,
-  alertsSlice,
-  HostRequirements,
-} from '../../../common';
-import { getErrorMessage, handleApiError } from '../../api';
-import { ClustersAPI } from '../../services/apis';
+const DISK_WRITE_SPEED_LINK = 'https://access.redhat.com/solutions/4885641';
+const parseRAM = (ramMib: number) => fileSize(ramMib * 1024 * 1024, 2, 'iec');
 
-const { addAlert } = alertsSlice.actions;
-
-export type PreflightHWRequirementsContentComponent = React.FC<{
+export type PreflightRequirementsContentProps = {
   clusterId: Cluster['id'];
   isSingleNode?: boolean;
-}>;
-
-const useClusterPreflightRequirements = (clusterId: Cluster['id']) => {
-  const [preflightRequirements, setPreflightRequirements] =
-    React.useState<PreflightHardwareRequirements>();
-  const [error, setError] = React.useState();
-
-  React.useEffect(() => {
-    const fetchFunc = async () => {
-      try {
-        const { data } = await ClustersAPI.getPreflightRequirements(clusterId);
-        setPreflightRequirements(data);
-      } catch (e) {
-        setError(e);
-        // report error systematically at one place show defaults instead
-        handleApiError(e, () =>
-          addAlert({
-            title: 'Failed to retrieve preflight cluster requirements',
-            message: getErrorMessage(e),
-          }),
-        );
-      }
-    };
-    fetchFunc();
-  }, [setPreflightRequirements, setError, clusterId]);
-
-  return {
-    preflightRequirements,
-    error,
-    isLoading: !error && !preflightRequirements,
-  };
+  isAddingHosts?: boolean;
 };
 
-// Day-2 flow
-export const AddHostRequirementsContent: PreflightHWRequirementsContentComponent = ({
+const HostRequirementsContent = ({
   clusterId,
-}) => {
+  isSingleNode,
+  isAddingHosts,
+}: PreflightRequirementsContentProps) => {
   const { preflightRequirements, error, isLoading } = useClusterPreflightRequirements(clusterId);
 
   if (isLoading) {
@@ -66,111 +27,51 @@ export const AddHostRequirementsContent: PreflightHWRequirementsContentComponent
     return <ErrorState />;
   }
 
-  const worker = preflightRequirements?.ocp?.worker?.quantitative;
-  const workerRam = fileSize((worker?.ramMib || 8 * 1024) * 1024 * 1024, 2, 'iec');
+  const masterQuantitativeData = preflightRequirements?.ocp?.master?.quantitative;
+  const masterCpuCores = masterQuantitativeData?.cpuCores || 4;
+  const masterRam = parseRAM(masterQuantitativeData?.ramMib || 16 * 1024);
+  const masterDiskSizeGb = masterQuantitativeData?.diskSizeGb || 100;
+  const contentForControlPlaneNodes = `${masterCpuCores} CPU cores, ${masterRam} RAM, and ${masterDiskSizeGb} GB disk size.`;
 
-  return (
-    <TextContent>
-      <Text component="p">
-        Worker hosts must have at least {worker?.cpuCores || 2} CPU cores, {workerRam} of RAM, and{' '}
-        {worker?.diskSizeGb || 120} GB of disk size storage.
-      </Text>
-    </TextContent>
-  );
-};
+  const workerQuantitativeData = preflightRequirements?.ocp?.worker?.quantitative;
+  const workerCpuCores = workerQuantitativeData?.cpuCores || 2;
+  const workerRam = parseRAM(workerQuantitativeData?.ramMib || 16 * 1024);
+  const workerDiskSizeGb = workerQuantitativeData?.diskSizeGb || 100;
+  const contentForWorkerNodes = `${workerCpuCores} CPU cores, ${workerRam} RAM, and ${workerDiskSizeGb} GB disk size.`;
 
-// Regular multi-node deployment
-export const HostRequirementsContent: PreflightHWRequirementsContentComponent = ({ clusterId }) => {
-  const { preflightRequirements, error, isLoading } = useClusterPreflightRequirements(clusterId);
-
-  if (isLoading) {
-    return <LoadingState content="Loading hardware requirements ..." />;
-  }
-  if (error) {
-    return <ErrorState />;
+  if (isAddingHosts) {
+    return (
+      <TextContent>
+        <Text component={'p'}>{`Worker hosts must have at least ${contentForWorkerNodes}`}</Text>
+      </TextContent>
+    );
   }
 
-  const master = preflightRequirements?.ocp?.master?.quantitative;
-  const worker = preflightRequirements?.ocp?.worker?.quantitative;
-
-  return <HostRequirements master={master} worker={worker} />;
-};
-
-// For Single Node Operator
-export const SingleHostRequirementsContent: PreflightHWRequirementsContentComponent = ({
-  clusterId,
-}) => {
-  const { preflightRequirements, error, isLoading } = useClusterPreflightRequirements(clusterId);
-
-  if (isLoading) {
-    return <LoadingState content="Loading hardware requirements ..." />;
-  }
-  if (error) {
-    return <ErrorState />;
-  }
-
-  const master = preflightRequirements?.ocp?.master?.quantitative;
-  const masterRam = fileSize((master?.ramMib || 16 * 1024) * 1024 * 1024, 2, 'iec');
-
-  return (
-    <TextContent>
-      <Text component="p">
-        One host is required with at least {master?.cpuCores || 4} CPU cores, {masterRam} of RAM,
-        and {master?.diskSizeGb || 120} GB of disk size storage.
-      </Text>
-    </TextContent>
-  );
-};
-
-export const CNVHostRequirementsContent: PreflightHWRequirementsContentComponent = ({
-  clusterId,
-  isSingleNode = false,
-}) => {
-  const { preflightRequirements, error, isLoading } = useClusterPreflightRequirements(clusterId);
-
-  if (isLoading) {
-    return <LoadingState content="Loading hardware requirements ..." />;
-  }
-  if (error) {
-    return <ErrorState />;
-  }
-
-  const cnvRequirements = preflightRequirements?.operators?.find(
-    (operatorRequirements) => operatorRequirements.operatorName === OPERATOR_NAME_CNV,
-  );
-
-  const workerRequirements = cnvRequirements?.requirements?.worker?.quantitative;
-  const masterRequirements = cnvRequirements?.requirements?.master?.quantitative;
-
-  return (
-    <>
+  let content: JSX.Element;
+  if (isSingleNode) {
+    content = (
+      <TextContent>
+        <Text component="p">
+          {`One host is required with at least ${contentForControlPlaneNodes}`}.
+        </Text>
+      </TextContent>
+    );
+  } else {
+    content = (
       <List>
+        <ListItem>{`Control plane nodes: At least ${contentForControlPlaneNodes}`}</ListItem>
+        <ListItem>{`Workers: At least ${contentForWorkerNodes}`}</ListItem>
         <ListItem>
-          Enabled CPU virtualization support in BIOS (Intel-VT / AMD-V) on all nodes
-        </ListItem>
-        <RenderIf condition={!isSingleNode}>
-          <ListItem>
-            Each worker node requires an additional {workerRequirements?.ramMib || 360} MiB of
-            memory {workerRequirements?.diskSizeGb ? ',' : ' and'}{' '}
-            {workerRequirements?.cpuCores || 2} CPUs
-            {workerRequirements?.diskSizeGb
-              ? ` and ${workerRequirements?.diskSizeGb} storage space`
-              : ''}
-          </ListItem>
-        </RenderIf>
-        <ListItem>
-          Each control plane node requires an additional {masterRequirements?.ramMib || 150} MiB of
-          memory {masterRequirements?.diskSizeGb ? ',' : ' and'} {masterRequirements?.cpuCores || 4}{' '}
-          CPUs
-          {masterRequirements?.diskSizeGb
-            ? ` and ${masterRequirements?.diskSizeGb} storage space`
-            : ''}
-        </ListItem>
-        <ListItem>
-          OpenShift Data Foundation (recommended for full functionality) or another persistent
-          storage service
+          Also note that each host's disk write speed should meet the minimum requirements to run
+          OpenShift.
+          <br />
+          <ExternalLink href={DISK_WRITE_SPEED_LINK}>Learn more</ExternalLink>
         </ListItem>
       </List>
-    </>
-  );
+    );
+  }
+
+  return content;
 };
+
+export default HostRequirementsContent;
