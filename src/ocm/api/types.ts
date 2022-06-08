6f7dce1370d6341415630d6122240cf184f85c5c
Celia Amador <camadorg@redhat.com>
2022-06-08 15:43:51 +0200

    NO_ISSUE: Avoid using functionality from Axios > v0.19.x

--- src/ocm/api/types.ts
+++ src/ocm/api/types.ts
@@ -1,10 +1,16 @@
-import Axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import { Error as APIError, InfraError } from '../../common/api/types';
 
 export type APIErrorMixin = InfraError & APIError;
 
+// Implementation from Axios.isAxiosError v0.29.2, which is not available in OCM's version 0.17.x
+const isAxiosError = (error: unknown): error is AxiosError<APIErrorMixin, APIErrorMixin> => {
+  const axiosError = error as AxiosError<APIErrorMixin, APIErrorMixin>;
+  return axiosError !== null && typeof axiosError === 'object' && axiosError.isAxiosError === true;
+};
+
 export const isApiError = (error: unknown): error is AxiosError<APIErrorMixin, APIErrorMixin> => {
-  if (!Axios.isAxiosError(error)) {
+  if (!isAxiosError(error)) {
     return false;
   }
   return typeof error.response?.data === 'object';
