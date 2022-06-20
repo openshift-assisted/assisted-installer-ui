48f9346c58b254df92c4f13b564c768db1bebccf
Montse Ortega <mortegag@redhat.com>
2022-06-20 07:35:33 +0200

    Bug 2080711: Change the question mark icons to be gray and not blue

--- src/ocm/components/clusterConfiguration/ODFCheckbox.tsx
+++ src/ocm/components/clusterConfiguration/ODFCheckbox.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { CheckboxField, getFieldId, PopoverIcon, ODF_REQUIREMENTS_LINK } from '../../../common';
 import { useFeatureSupportLevel } from '../../../common/components/featureSupportLevels';
 import { FormGroup, Tooltip } from '@patternfly/react-core';
-import { ExternalLinkAltIcon, HelpIcon } from '@patternfly/react-icons';
+import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 const ODFLabel: React.FC = () => (
   <>
     Install OpenShift Data Foundation{' '}
     <PopoverIcon
       component={'a'}
-      variant={'plain'}
-      IconComponent={HelpIcon}
       headerContent="Additional Requirements"
       bodyContent={
         <a href={ODF_REQUIREMENTS_LINK} target="_blank" rel="noopener noreferrer">
