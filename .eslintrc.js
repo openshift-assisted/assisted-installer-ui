60093b2a4c85cb1724f78d79dcd62df41d6889ee
Celia Amador <camadorg@redhat.com>
2022-06-20 17:00:35 +0200

    NO_ISSUE: Generate errors for eslint - unused vars (#1453)

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,6 +76,7 @@ module.exports = {
         '@typescript-eslint/no-unsafe-assignment': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
         '@typescript-eslint/no-unsafe-return': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/require-await': 'warn',
         '@typescript-eslint/restrict-plus-operands': 'warn',
         '@typescript-eslint/restrict-template-expressions': 'warn',
