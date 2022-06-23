948dcf7d96c7b6ef33ad285c9f1eb020b1922bca
Celia Amador <camadorg@redhat.com>
2022-06-23 09:31:14 +0200

    MGMT-10951 Improve Sentry logging for AI errors

--- src/ocm/sentry.ts
+++ src/ocm/sentry.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/browser';
+import { getAssistedUiLibVersion } from '../common/config/constants';
 import { ocmClient } from './api/axiosClient';
 
 export const captureException = (
@@ -8,8 +9,15 @@ export const captureException = (
   severity: Sentry.Severity = Sentry.Severity.Error,
 ) => {
   if (ocmClient) {
-    message && Sentry.captureMessage(message, severity);
-    Sentry.captureException(error);
+    const body = {
+      level: severity,
+      tags: {
+        app_section: 'assisted-ui-lib',
+        section_version: getAssistedUiLibVersion(),
+      },
+    };
+    message && Sentry.captureMessage(message, body);
+    Sentry.captureException(error, body);
   } else {
     severity === Sentry.Severity.Error
       ? console.error(message, error)
