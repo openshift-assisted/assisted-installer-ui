78c068318ea95efdc925d256cf5c7629a5bd3d58
Jonathan Kilzi <jkilzi@redhat.com>
2022-06-26 22:28:13 +0300

    NO-ISSUE: moves new type-guards to typescriptExtensions.ts

--- src/ocm/api/axiosExtensions.ts
+++ src/ocm/api/axiosExtensions.ts
@@ -1,17 +1,5 @@
 import { AxiosError } from 'axios';
-
-function isNonNullObject(val: unknown): val is object {
-  return typeof val === 'object' && val !== null;
-}
-
-// Still, as of June 18, 2022, Typescript cannot use the 'in' keyword or 'hasOwnProperty' method as a type guard
-// See: https://github.com/microsoft/TypeScript/issues/21732
-function hasProp<T extends object, K extends PropertyKey>(
-  obj: T,
-  prop: K,
-): obj is T & Record<K, unknown> {
-  return prop in obj;
-}
+import { hasProp, isNonNullObject } from '../../common/types';
 
 // Implementation from Axios.isAxiosError v0.29.2, which is not available in OCM's version 0.17.x
 export function isAxiosError<T = unknown, D = unknown>(
