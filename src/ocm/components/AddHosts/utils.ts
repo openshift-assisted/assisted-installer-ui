49c76d9de1e1bc639c49f0f8052bdf0d1240ccc4
Celia Amador <camadorg@redhat.com>
2022-07-06 08:47:08 +0200

    MGMT-11009 Use always Day2 cluster

--- src/ocm/components/AddHosts/utils.ts
+++ src/ocm/components/AddHosts/utils.ts
@@ -1,4 +1,3 @@
-import { isSNO } from '../../../common';
 import Day2ClusterService from '../../services/Day2ClusterService';
 import { getFeatureSupported } from '../featureSupportLevels/FeatureSupportLevelProvider';
 import { OcmClusterType } from './types';
@@ -16,16 +15,10 @@ export const canAddHost = ({ cluster }: { cluster: OcmClusterType }) => {
     return false;
   }
 
-  const { aiCluster } = cluster;
-  if (aiCluster) {
-    const isMultiNode = !isSNO(aiCluster);
-    return aiCluster.status === 'installed' && (isMultiNode || isSNOExpansionAllowed(cluster));
-  } else {
-    const isMultiNode = (cluster.metrics?.nodes?.total || 0) > 1;
-    return (
-      cluster.state === 'ready' &&
-      cluster.product?.id === 'OCP-AssistedInstall' &&
-      (isMultiNode || isSNOExpansionAllowed(cluster))
-    );
-  }
+  const isMultiNode = (cluster.metrics?.nodes?.total || 0) > 1;
+  return (
+    cluster.state === 'ready' &&
+    cluster.product?.id === 'OCP-AssistedInstall' &&
+    (isMultiNode || isSNOExpansionAllowed(cluster))
+  );
 };
