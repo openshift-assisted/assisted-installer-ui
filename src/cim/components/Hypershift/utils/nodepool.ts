8bb579a125bb3fd1f91fca0cde0d1c5f1d52aa3e
Rastislav Wagner <rawagner@redhat.com>
2022-07-20 14:53:11 +0200

    Improve agent -> agentMachine -> nodepool  detection (#1518)

--- src/cim/components/Hypershift/utils/nodepool.ts
+++ src/cim/components/Hypershift/utils/nodepool.ts
@@ -1,6 +1,9 @@
 import { AgentK8sResource } from '../../../types';
 import { AgentMachineK8sResource, HostedClusterK8sResource, NodePoolK8sResource } from '../types';
 
+const CLUSTER_NAME_LABEL = 'cluster.x-k8s.io/cluster-name';
+const NODEPOOL_NAME_ANNOTATION = 'cluster.x-k8s.io/cloned-from-name';
+
 export const getNodepoolAgents = (
   nodePool: NodePoolK8sResource,
   agents: AgentK8sResource[],
@@ -10,11 +13,10 @@ export const getNodepoolAgents = (
   const nodePoolAgentMachines = agentMachines
     .filter(
       (am) =>
-        am.metadata?.labels?.[
-          `${hostedCluster.metadata?.name || ''}-${hostedCluster.metadata?.namespace || ''}-${
-            nodePool.metadata?.name || ''
-          }`
-        ] &&
+        am.metadata?.namespace ===
+          `${hostedCluster.metadata?.namespace || ''}-${hostedCluster.metadata?.name || ''}` &&
+        am.metadata?.labels?.[CLUSTER_NAME_LABEL] === hostedCluster.metadata?.name &&
+        am.metadata?.annotations?.[NODEPOOL_NAME_ANNOTATION] === nodePool.metadata?.name &&
         am.status?.agentRef?.name &&
         am.status?.agentRef?.namespace,
     )
