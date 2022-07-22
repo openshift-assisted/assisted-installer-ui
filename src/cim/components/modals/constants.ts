71eaf1b7b067811b77d1b748417e01e9406da4e9
Montse Ortega <mortegag@redhat.com>
2022-07-22 13:52:29 +0200

    Changes in template of upload hosts usings BMCs in mass

--- src/cim/components/modals/constants.ts
+++ src/cim/components/modals/constants.ts
@@ -1,13 +1,13 @@
 export const credentials = {
   apiVersion: 'v1',
   data: {
+    username: '<<base64 of the username>>',
     password: '<<base64 of the password>>',
-    username: '<<base64 of the password>>',
   },
   kind: 'Secret',
   metadata: {
     name: '<<name of credentials. Should be unique>>',
-    namespace: '<<name of namespace>>',
+    namespace: '<<name of namespace - usually the same as the name of the infra env>>',
   },
   type: 'Opaque',
 };
@@ -17,21 +17,21 @@ export const host1 = {
   kind: 'BareMetalHost',
   metadata: {
     annotations: {
-      'bmac.agent-install.openshift.io/hostname': '<<the name of hostname>>',
+      'bmac.agent-install.openshift.io/hostname': '<<hostname of the host>>',
       'inspect.metal3.io': 'disabled',
     },
     labels: {
       'infraenvs.agent-install.openshift.io':
         '<<the name of the infra env in which this host will be>>',
     },
-    name: '<<name of bare metal host>>',
-    namespace: '<<name of namespace>>',
+    name: '<<the name of the BareMetalHost CR>>',
+    namespace: '<<name of namespace - usually the same as the name of the infra env>>',
   },
   spec: {
     automatedCleaningMode: 'disabled',
     bmc: {
       address: '<<Baseboard Management Controller Address>>',
-      credentialsName: '<<name of credentials indicate in 7th line>>',
+      credentialsName: '<<the name of the secret containing the credentials (defined in line 7)>>',
       disableCertificateVerification: true,
     },
     bootMACAddress:
@@ -45,21 +45,21 @@ export const host2 = {
   kind: 'BareMetalHost',
   metadata: {
     annotations: {
-      'bmac.agent-install.openshift.io/hostname': '<<the name of hostname>>',
+      'bmac.agent-install.openshift.io/hostname': '<<hostname of the host>>',
       'inspect.metal3.io': 'disabled',
     },
     labels: {
       'infraenvs.agent-install.openshift.io':
         '<<the name of the infra env in which this host will be>>',
     },
-    name: '<<name of bare metal host>>',
-    namespace: '<<name of namespace>>',
+    name: '<<the name of the BareMetalHost CR>>',
+    namespace: '<<name of namespace - usually the same as the name of the infra env>>',
   },
   spec: {
     automatedCleaningMode: 'disabled',
     bmc: {
       address: '<<Baseboard Management Controller Address>>',
-      credentialsName: '<<name of credentials indicate in 7th line>>',
+      credentialsName: '<<the name of the secret containing the credentials (defined in line 7)>>',
       disableCertificateVerification: true,
     },
     bootMACAddress:
