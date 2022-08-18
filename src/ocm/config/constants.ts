4ed256ae546b6aa6a40ff86aaef2e102a7039360
Celia Amador <camadorg@redhat.com>
2022-08-18 09:43:14 +0200

    MGMT-10110 Do not show Add Hosts for Day2 if user cannot edit

--- src/ocm/config/constants.ts
+++ src/ocm/config/constants.ts
@@ -8,7 +8,7 @@ export const isSingleClusterMode = () => {
   return process.env.REACT_APP_BUILD_MODE === 'single-cluster';
 };
 
-/* Used from e2e tests so we can mock the permissions */
+/* Used from Integration tests so we can mock the permissions */
 export type ExtendedCluster = Cluster & {
   permissions: AssistedInstallerOCMPermissionTypesListType;
 };
