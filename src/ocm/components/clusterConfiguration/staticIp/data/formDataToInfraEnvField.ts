e957c3a90353259975e4e64aeef882b65c44f43e
Montse Ortega <mortegag@redhat.com>
2022-09-16 11:02:54 +0200

    Vlan ID must be a number

--- src/ocm/components/clusterConfiguration/staticIp/data/formDataToInfraEnvField.ts
+++ src/ocm/components/clusterConfiguration/staticIp/data/formDataToInfraEnvField.ts
@@ -183,5 +183,7 @@ export const networkWideToInfraEnvField = (
   }
   const currentFormData = formDataFromInfraEnvField(staticNetworkConfig);
   fixHostIps(networkWide, currentFormData.hosts);
+  //Transform vlanId to number
+  networkWide.vlanId !== '' ? (networkWide.vlanId = Number(networkWide.vlanId)) : '';
   return formDataToInfraEnvField({ networkWide, hosts: currentFormData.hosts });
 };
