47335fd30f59efe97d7e9904ef45b229451166d4
Celia Amador <camadorg@redhat.com>
2022-10-05 13:01:29 +0200

    MGMT-11294 Use networkType from Backend in cluster creation

--- src/ocm/components/clusterConfiguration/networkConfiguration/StackTypeControl.tsx
+++ src/ocm/components/clusterConfiguration/networkConfiguration/StackTypeControl.tsx
@@ -8,14 +8,16 @@ import {
   HostSubnets,
   NetworkConfigurationValues,
   getFieldId,
-  getDefaultNetworkType,
   DUAL_STACK,
   IPV4_STACK,
+  NETWORK_TYPE_OVN,
+  NETWORK_TYPE_SDN,
   NO_SUBNET_SET,
   Cluster,
   ClusterNetwork,
   MachineNetwork,
   ServiceNetwork,
+  canSelectNetworkTypeSDN,
 } from '../../../../common';
 import { ConfirmationModal, PopoverIcon } from '../../../../common/components/ui';
 import { useDefaultConfiguration } from '../ClusterDefaultConfigurationContext';
@@ -66,7 +68,6 @@ export const StackTypeControlGroup = ({
 
   const setSingleStack = React.useCallback(() => {
     setFieldValue('stackType', IPV4_STACK);
-    setFieldValue('networkType', getDefaultNetworkType(isSNO, false));
 
     if (values.machineNetworks && values.machineNetworks?.length >= 2) {
       setFieldValue('machineNetworks', values.machineNetworks.slice(0, 1));
@@ -80,7 +81,6 @@ export const StackTypeControlGroup = ({
 
     void validateForm();
   }, [
-    isSNO,
     setFieldValue,
     validateForm,
     values.clusterNetworks,
@@ -90,8 +90,11 @@ export const StackTypeControlGroup = ({
 
   const setDualStack = () => {
     setFieldValue('stackType', DUAL_STACK);
-    setFieldValue('networkType', getDefaultNetworkType(isSNO, true));
     setFieldValue('vipDhcpAllocation', false);
+
+    if (values.networkType === NETWORK_TYPE_SDN && !canSelectNetworkTypeSDN(isSNO, true)) {
+      setFieldValue('networkType', NETWORK_TYPE_OVN);
+    }
 
     if (values.machineNetworks && values.machineNetworks?.length < 2) {
       setFieldValue(
