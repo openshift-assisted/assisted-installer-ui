60c44df119f016d78773e120865dbcb084127781
Jonathan Kilzi <jkilzi@redhat.com>
2022-10-10 17:04:26 +0300

    Removes rootReducer.ts
    All the store initialization is located now inside store/index.ts

--- src/ocm/store/index.ts
+++ src/ocm/store/index.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
+import clustersReducer from '../reducers/clusters/clustersSlice';
+import currentClusterReducer from '../reducers/clusters/currentClusterSlice';
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    clusters: clustersReducer,
+    currentCluster: currentClusterReducer,
+  },
   preloadedState: {},
 });
+export type RootState = ReturnType<typeof store.getState>;
