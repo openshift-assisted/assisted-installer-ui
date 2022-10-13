426cb2ce8584a7dca8f6cb89762a1daaf349cb14
Montse Ortega <mortegag@redhat.com>
2022-10-13 13:41:07 +0200

    Add Korean, Japanese and simplified Chinese translation files
    Change validatei18n_files.sh to take into account only english files
    
    We only generate automatically the english translation files

--- scripts/validatei18n_files.sh
+++ scripts/validatei18n_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 yarn i18n
-GIT_STATUS="$(git status --short --untracked-files=no -- locales dist/locales)"
+GIT_STATUS="$(git status --short --untracked-files=no -- locales/en dist/locales/en)"
 if [ -n "$GIT_STATUS" ]; then
   echo "i18n files are not up to date. Run 'yarn i18n' and then commit changes."
   git diff
   exit 1
 fi
-result=$(grep -v -r '^  "ai:' ./locales | grep -v "{" | grep -v "}") 
+result=$(grep -v -r '^  "ai:' ./locales/en | grep -v "{" | grep -v "}") 
 if [ -n "$result" ]; then
    echo "i18n keys that not contains 'ai:' prefix are found. Please, review it and run 'yarn i18n' and then commit changes:"
    echo $result
