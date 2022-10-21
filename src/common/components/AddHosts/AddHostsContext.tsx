1cf200ee4728b69633b5b3a5d71aaedc6de95005
Celia Amador <camadorg@redhat.com>
2022-10-21 15:01:35 +0200

    Retrieve Day1 cluter cpu architecture

--- src/common/components/AddHosts/AddHostsContext.tsx
+++ src/common/components/AddHosts/AddHostsContext.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Cluster } from '../../api';
+import { CpuArchitecture } from '../../types';
 
 export type AddHostsContextType = {
   cluster?: Cluster;
   resetCluster?: () => Promise<void>;
   ocpConsoleUrl?: string;
+  day1CpuArchitecture?: CpuArchitecture;
 };
 
 export const AddHostsContext = React.createContext<AddHostsContextType>({});
@@ -13,9 +15,10 @@ export const AddHostsContextProvider: React.FC<AddHostsContextType> = ({
   cluster,
   resetCluster,
   ocpConsoleUrl,
+  day1CpuArchitecture,
   children,
 }) => (
-  <AddHostsContext.Provider value={{ cluster, resetCluster, ocpConsoleUrl }}>
+  <AddHostsContext.Provider value={{ cluster, resetCluster, day1CpuArchitecture, ocpConsoleUrl }}>
     {children}
   </AddHostsContext.Provider>
 );
