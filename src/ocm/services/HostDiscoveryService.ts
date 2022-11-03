d65f70af6aea292bb15e6a9c8f0b2abd79f799a9
Montse Ortega <mortegag@redhat.com>
2022-11-03 11:38:08 +0100

    Fix wrong patch request when platform integration is disabled

--- src/ocm/services/HostDiscoveryService.ts
+++ src/ocm/services/HostDiscoveryService.ts
@@ -10,8 +10,12 @@ const HostDiscoveryService = {
   setPlatform(
     params: V2ClusterUpdateParams,
     platformToIntegrate: SupportedPlatformType | undefined,
+    userManagedNetworking: boolean | undefined,
   ): void {
-    const type = platformToIntegrate === undefined ? 'none' : platformToIntegrate;
+    //the UI client sends cluster.platform.type=none if UMN is selected.
+    //the UI client sends cluster.platform.type=baremetal if UMN is not selected.
+    const resetPlatform = userManagedNetworking ? 'none' : 'baremetal';
+    const type = platformToIntegrate === undefined ? resetPlatform : platformToIntegrate;
     params.platform = {
       type,
     };
