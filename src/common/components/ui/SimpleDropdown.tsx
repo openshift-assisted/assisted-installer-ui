28a1011b28f260fead3732ce40ae96c539f69429
Julie Gyselová <jgyselov@redhat.com>
2022-11-25 09:07:14 +0100

    Role column alignment issues (#1765)

--- src/common/components/ui/SimpleDropdown.tsx
+++ src/common/components/ui/SimpleDropdown.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { DropdownItem, DropdownToggle, Dropdown, DropdownProps } from '@patternfly/react-core';
 import { CaretDownIcon } from '@patternfly/react-icons';
 import { HostRole } from '../../../common/types/hosts';
+import './SimpleDropdown.css';
 
 type SimpleDropdownProps = {
   current?: string;
@@ -46,7 +47,7 @@ export const SimpleDropdown = ({
         toggleIndicator={CaretDownIcon}
         isDisabled={isDisabled}
         id={idPrefix ? `${idPrefix}-dropdown-toggle-items` : undefined}
-        className="pf-m-text" // TODO(jtomasek): replace this with 'isText' prop once we update the PF
+        className="role-dropdown"
       >
         {current || defaultValue}
       </DropdownToggle>
