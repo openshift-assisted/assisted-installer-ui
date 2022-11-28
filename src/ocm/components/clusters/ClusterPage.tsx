88162a3a05bd76229062b53bc5e6858f77466d32
Montse Ortega <ammont82@users.noreply.github.com>
2022-11-28 16:04:52 +0100

    MGMT-12075: Improved error-handling in the UI when the API is unavailable (#1763)
    * MGMT-12075: Block UI if cluster polling fails to update cluster
    
    * MGMT-12075 Adding modal
    
    * MGMT-12075 Ignore errors of purposedly aborted requests
    
    * MGMT-12075 Implement for OCM's Ai Card
    
    * Changing the clusterPollingError modal
    
    * Remove close button from modal
    
    * Change the text of error modal
    
    Co-authored-by: Celia Amador <camadorg@redhat.com>

--- src/ocm/components/clusters/ClusterPage.tsx
+++ src/ocm/components/clusters/ClusterPage.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { Link, Redirect, RouteComponentProps } from 'react-router-dom';
-import {
-  Button,
-  ButtonVariant,
-  PageSection,
-  PageSectionVariants,
-  Text,
-  TextContent,
-} from '@patternfly/react-core';
+import { Redirect, RouteComponentProps } from 'react-router-dom';
+import { PageSection, PageSectionVariants, Text, TextContent } from '@patternfly/react-core';
 import {
   AddHostsContextProvider,
   AlertsContextProvider,
@@ -15,7 +8,6 @@ import {
   CpuArchitecture,
   ErrorState,
   InfraEnv,
-  LoadingState,
   ResourceUIState,
 } from '../../../common';
 import ClusterDetail from '../clusterDetail/ClusterDetail';
@@ -28,12 +20,15 @@ import ClusterWizard from '../clusterWizard/ClusterWizard';
 import { ModalDialogsContextProvider } from '../hosts/ModalDialogsContext';
 import { useClusterPolling, useFetchCluster } from './clusterPolling';
 import { DiscoveryImageModal } from '../clusterConfiguration/DiscoveryImageModal';
-import { isSingleClusterMode, routeBasePath } from '../../config';
+import { routeBasePath } from '../../config';
 import { FeatureSupportLevelProvider } from '../featureSupportLevels';
 import ClusterWizardContextProvider from '../clusterWizard/ClusterWizardContextProvider';
 import useInfraEnv from '../../hooks/useInfraEnv';
 import { SentryErrorMonitorContextProvider } from '../SentryErrorMonitorContextProvider';
 import { forceReload } from '../../reducers/clusters';
+import { getErrorStateActions, ClusterUiError } from './ClusterPageErrors';
+import ClusterLoading from './ClusterLoading';
+import ClusterPollingErrorModal from '../clusterDetail/ClusterPollingErrorModal';
 
 type MatchParams = {
   clusterId: string;
@@ -49,18 +44,7 @@ const ClusterPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
     error: infraEnvError,
     updateInfraEnv,
   } = useInfraEnv(clusterId, CpuArchitecture.USE_DAY1_ARCHITECTURE);
-  const errorStateActions = [];
-  if (!isSingleClusterMode()) {
-    errorStateActions.push(
-      <Button
-        key="cancel"
-        variant={ButtonVariant.secondary}
-        component={(props) => <Link to={`${routeBasePath}/clusters`} {...props} />}
-      >
-        Back
-      </Button>,
-    );
-  }
+
   const getContent = (cluster: Cluster, infraEnv: InfraEnv) => {
     if (cluster.status === 'adding-hosts') {
       return (
@@ -117,21 +101,14 @@ const ClusterPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
     }
   };
 
-  const loadingUI = (
-    <PageSection variant={PageSectionVariants.light} isFilled>
-      <LoadingState />
-    </PageSection>
-  );
-
   if (uiState === ResourceUIState.LOADING || infraEnvLoading) {
-    return loadingUI;
+    return <ClusterLoading />;
   }
 
-  if (uiState === ResourceUIState.ERROR) {
+  if (uiState === ResourceUIState.ERROR && !cluster) {
     if (Number(errorDetail?.code) === 404) {
       return <Redirect to={`${routeBasePath}/clusters`} />;
     }
-
     return (
       <PageSection variant={PageSectionVariants.light} isFilled>
         <ErrorState
@@ -139,7 +116,7 @@ const ClusterPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
           fetchData={
             Number(errorDetail?.code) === 401 ? () => window.location.reload() : fetchCluster
           }
-          actions={errorStateActions}
+          actions={getErrorStateActions()}
         />
       </PageSection>
     );
@@ -150,7 +127,7 @@ const ClusterPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
       <PageSection variant={PageSectionVariants.light} isFilled>
         <ErrorState
           title="Cluster not found"
-          actions={errorStateActions}
+          actions={getErrorStateActions()}
           content={
             'Check to make sure the cluster-ID is valid. Otherwise, the cluster may have been deleted.'
           }
@@ -159,23 +136,19 @@ const ClusterPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
     );
   }
 
-  const errorUI = (
-    <PageSection variant={PageSectionVariants.light} isFilled>
-      <ErrorState
-        title="Failed to retrieve the default configuration"
-        actions={errorStateActions}
-      />
-    </PageSection>
-  );
-
   if (cluster && infraEnv) {
+    const isOutdatedClusterData = uiState === ResourceUIState.ERROR;
     return (
       <AlertsContextProvider>
         <SentryErrorMonitorContextProvider>
           <ModalDialogsContextProvider>
-            <ClusterDefaultConfigurationProvider loadingUI={loadingUI} errorUI={errorUI}>
-              <FeatureSupportLevelProvider loadingUi={loadingUI} cluster={cluster}>
+            <ClusterDefaultConfigurationProvider
+              loadingUI={<ClusterLoading />}
+              errorUI={<ClusterUiError />}
+            >
+              <FeatureSupportLevelProvider loadingUi={<ClusterLoading />} cluster={cluster}>
                 {getContent(cluster, infraEnv)}
+                {isOutdatedClusterData && <ClusterPollingErrorModal />}
                 <CancelInstallationModal />
                 <ResetClusterModal />
                 <DiscoveryImageModal />
