8ef5f4e421af127253cfb9cb299d7869d31c3c79
Celia Amador <camadorg@redhat.com>
2022-11-28 12:24:52 +0100

    MGMT-11041: Enhance Add-hosts tab features

--- src/ocm/components/HostsClusterDetailTab/utils.ts
+++ src/ocm/components/HostsClusterDetailTab/utils.ts
@@ -10,15 +10,48 @@ const isSNOExpansionAllowed = (cluster: OcmClusterType) => {
   );
 };
 
-export const canAddHost = ({ cluster }: { cluster: OcmClusterType }) => {
-  if (!cluster.canEdit || !Day2ClusterService.getOpenshiftClusterId(cluster)) {
-    return false;
+export const getAddHostTabDetails = ({ cluster }: { cluster: OcmClusterType }) => {
+  const isAiDay2Ready = cluster.state === 'ready' && cluster.product?.id === 'OCP-AssistedInstall';
+
+  // TODO camador make the tab visible for !canEdit with read-only content for Day2 in MGMT-11768
+  const isHiddenTab = !cluster.canEdit || !isAiDay2Ready;
+  if (isHiddenTab) {
+    return {
+      showTab: false,
+      isDisabled: false,
+      tabTooltip: '',
+    };
   }
 
-  const isMultiNode = (cluster.metrics?.nodes?.total || 0) > 1;
-  return (
-    cluster.state === 'ready' &&
-    cluster.product?.id === 'OCP-AssistedInstall' &&
-    (isMultiNode || isSNOExpansionAllowed(cluster))
-  );
+  // If it's a Day2 AI cluster, it can happen that the cluster hasn't reported required information yet.
+  // In that case the Tab will be disabled and the content would be a message explaining what the problem is
+  const day2ClusterHostCount = cluster.metrics?.nodes?.total || 0;
+  if (day2ClusterHostCount === 0 || !Day2ClusterService.getOpenshiftClusterId(cluster)) {
+    return {
+      showTab: true,
+      isDisabled: true,
+      tabTooltip:
+        day2ClusterHostCount === 0
+          ? // TODO use real copy messages
+            'The cluster did not report metrics yet'
+          : 'Explanation of why the cluster cannot be scaled at this moment',
+    };
+  }
+
+  if (day2ClusterHostCount === 1 && !isSNOExpansionAllowed(cluster)) {
+    // It's an SNO with an OpenshiftVersion that doesn't support Day2 flow
+    return {
+      showTab: true,
+      isDisabled: true,
+      // TODO use real copy messages
+      tabTooltip: 'The OpenShift version does not support adding hosts',
+    };
+  }
+
+  // This is a multi-node cluster that supports the AI Day2 flow
+  return {
+    showTab: true,
+    isDisabled: false,
+    tabTooltip: '',
+  };
 };
