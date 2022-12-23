9c75545c0ee75f564a6d1c04f67b826a9861e95d
jgyselov <jgyselov@redhat.com>
2022-12-23 12:21:29 +0100

    Rework the Operator types

--- src/ocm/components/clusterConfiguration/review/ReviewOperatorsTable.tsx
+++ src/ocm/components/clusterConfiguration/review/ReviewOperatorsTable.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { Table, TableVariant, TableBody, IRow } from '@patternfly/react-table';
 import {
   Cluster,
+  ExposedOperatorNames,
   hasEnabledOperators,
   operatorLabels,
-  OPERATOR_NAME_CNV,
-  OPERATOR_NAME_LVM,
-  OPERATOR_NAME_ODF,
 } from '../../../../common';
 import { useTranslation } from '../../../../common/hooks/use-translation-wrapper';
 
@@ -14,24 +12,19 @@ export const ReviewOperatorsTable = ({ cluster }: { cluster: Cluster }) => {
   const { t } = useTranslation();
   const operatorNames = operatorLabels(t);
 
-  const operators = React.useMemo(
-    () => [OPERATOR_NAME_CNV, OPERATOR_NAME_ODF, OPERATOR_NAME_LVM],
-    [],
-  );
-
   const rows = React.useMemo(() => {
-    return operators
-      .filter((operator) => hasEnabledOperators(cluster.monitoredOperators, operator))
-      .map((operator) => ({
-        cells: [
-          operatorNames[operator],
-          {
-            title: 'Enabled',
-            props: { 'data-testid': `operator-${operator}` },
-          },
-        ],
-      }));
-  }, [cluster.monitoredOperators, operatorNames, operators]);
+    return ExposedOperatorNames.filter((operator) =>
+      hasEnabledOperators(cluster.monitoredOperators, operator),
+    ).map((operator) => ({
+      cells: [
+        operatorNames[operator],
+        {
+          title: 'Enabled',
+          props: { 'data-testid': `operator-${operator}` },
+        },
+      ],
+    }));
+  }, [cluster.monitoredOperators, operatorNames]);
 
   return (
     <Table
