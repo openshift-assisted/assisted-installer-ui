172701391073bec6366353b898ce6d270a79225f
Jonathan Kilzi <jkilzi@redhat.com>
2023-01-11 10:54:42 +0200

    Deprecates single-cluster mode (#1829)
    * Deprecates single-cluster mode
    * Reusable BackButton component
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- src/ocm/components/clusters/ClusterPageErrors.tsx
+++ src/ocm/components/clusters/ClusterPageErrors.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { Button, ButtonVariant, PageSection, PageSectionVariants } from '@patternfly/react-core';
-import { Link } from 'react-router-dom';
+import { PageSection, PageSectionVariants } from '@patternfly/react-core';
 import { ErrorState } from '../../../common';
-import { isSingleClusterMode, routeBasePath } from '../../config';
-
-export const getErrorStateActions = () => {
-  if (isSingleClusterMode()) {
-    return [];
-  }
-
-  return [
-    <Button
-      key="cancel"
-      variant={ButtonVariant.secondary}
-      component={(props) => <Link to={`${routeBasePath}/clusters`} {...props} />}
-    >
-      Back
-    </Button>,
-  ];
-};
+import { routeBasePath } from '../../config';
+import { BackButton } from '../ui/Buttons/BackButton';
 
 export const ClusterUiError = () => (
   <PageSection variant={PageSectionVariants.light} isFilled>
     <ErrorState
       title="Failed to retrieve the default configuration"
-      actions={getErrorStateActions()}
+      actions={[<BackButton key={'cancel'} to={`${routeBasePath}/clusters`} />]}
     />
   </PageSection>
 );
