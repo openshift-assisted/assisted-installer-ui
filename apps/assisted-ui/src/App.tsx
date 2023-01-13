163fda01173cedb39c717337b9c669bbb816b8f7
Jonathan Kilzi <jkilzi@redhat.com>
2023-01-13 15:57:23 +0200

    Uses node16 module resolution

--- apps/assisted-ui/src/App.tsx
+++ apps/assisted-ui/src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import { Page } from '@patternfly/react-core';
-import { Config, Features, Router as LibRouter, Store } from 'openshift-assisted-ui-lib/ocm';
-import Header from './Header';
-import BackgroundImage from './BackgroundImage';
+import { Features, Router as LibRouter, Store } from 'openshift-assisted-ui-lib/ocm';
+import Header from './Header.jsx';
+import BackgroundImage from './BackgroundImage.jsx';
 import './i18n.js';
 
 const { store } = Store;
