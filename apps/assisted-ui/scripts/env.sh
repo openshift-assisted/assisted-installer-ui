0b5a29568e9580f860db1aea7d201e845ff28cc1
Jonathan Kilzi <jkilzi@redhat.com>
2023-01-13 15:56:43 +0200

    Updates env.sh
    Disables shellcheck rule #SC2155

--- apps/assisted-ui/scripts/env.sh
+++ apps/assisted-ui/scripts/env.sh
@@ -1,4 +1,6 @@
 export VITE_APP_API_ROOT="/api/assisted-install"
+# shellcheck disable=SC2155
 export VITE_APP_GIT_SHA="$(git rev-parse --short HEAD)"
 export VITE_APP_IMAGE_REPO="quay.io/edge-infrastructure/assisted-installer-ui"
+# shellcheck disable=SC2155
 export VITE_APP_VERSION="$(jq -r '.version' ../../libs/openshift-assisted-ui-lib/package.json)"
