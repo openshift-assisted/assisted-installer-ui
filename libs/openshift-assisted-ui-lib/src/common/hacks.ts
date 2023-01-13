eef519775b814cd02407defc9fd174fa30865d7d
Jonathan Kilzi <jkilzi@redhat.com>
2023-01-13 08:45:00 +0200

    Removes src/common/hacks.ts
    squashme

--- libs/openshift-assisted-ui-lib/src/common/hacks.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// TODO(jtomasek): Stop using this once https://github.com/developit/microbundle/issues/564#issuecomment-593146626
-// is fixed, use `microbundle --jsxFragment React.Fragment` instead.
-// Workaround for TS17016
-window['Fragment'] = React.Fragment;
--- libs/openshift-assisted-ui-lib/src/common/index.ts
+++ libs/openshift-assisted-ui-lib/src/common/index.ts
@@ -1,5 +1,4 @@
 import './styles.css';
-import './hacks';
 import './utils';
 
 export * from './components';
