e1b62c60572854eebd427bfdd8b5d549fd3af3e4
Jonathan Kilzi <jkilzi@redhat.com>
2023-01-14 09:09:40 +0200

    Do not use jq for parsing .json files

--- apps/assisted-ui/scripts/env.sh
+++ apps/assisted-ui/scripts/env.sh
@@ -3,4 +3,4 @@ export VITE_APP_API_ROOT="/api/assisted-install"
 export VITE_APP_GIT_SHA="$(git rev-parse --short HEAD)"
 export VITE_APP_IMAGE_REPO="quay.io/edge-infrastructure/assisted-installer-ui"
 # shellcheck disable=SC2155
-export VITE_APP_VERSION="$(jq -r '.version' ../../libs/openshift-assisted-ui-lib/package.json)"
+export VITE_APP_VERSION="$(node -e 'require('\''../../libs/openshift-assisted-ui-lib/package.json'\'').version')"
