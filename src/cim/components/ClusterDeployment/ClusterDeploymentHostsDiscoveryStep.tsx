e2ab46a5c8fb2a4097b48480b4f298a17ddaeaf6
rawagner <rawagner@redhat.com>
2023-03-14 16:44:26 +0100

    Propagate errors from infra env to add host modal

--- src/cim/components/ClusterDeployment/ClusterDeploymentHostsDiscoveryStep.tsx
+++ src/cim/components/ClusterDeployment/ClusterDeploymentHostsDiscoveryStep.tsx
@@ -17,21 +17,6 @@ const ClusterDeploymentHostsDiscoveryStep: React.FC<ClusterDeploymentHostsDiscov
   agentClusterInstall,
   agents: allAgents,
   infraEnv,
-  infraNMStates,
-  clusterDeployment,
-  bareMetalHosts,
-  onCreateBMH,
-  onSaveAgent,
-  onEditRole,
-  onSetInstallationDiskId,
-  onSaveBMH,
-  onSaveISOParams,
-  fetchSecret,
-  onChangeBMHHostname,
-  onApproveAgent,
-  onDeleteHost,
-  getClusterDeploymentLink,
-  isBMPlatform,
   onSaveHostsDiscovery,
   onClose,
   ...rest
@@ -152,22 +137,7 @@ const ClusterDeploymentHostsDiscoveryStep: React.FC<ClusterDeploymentHostsDiscov
             agentClusterInstall={agentClusterInstall}
             agents={infraEnvAgents}
             infraEnv={infraEnv}
-            infraNMStates={infraNMStates}
-            bareMetalHosts={bareMetalHosts}
             usedHostnames={usedHostnames}
-            clusterDeployment={clusterDeployment}
-            onCreateBMH={onCreateBMH}
-            onSaveAgent={onSaveAgent}
-            onEditRole={onEditRole}
-            onSetInstallationDiskId={onSetInstallationDiskId}
-            onSaveBMH={onSaveBMH}
-            onSaveISOParams={onSaveISOParams}
-            fetchSecret={fetchSecret}
-            onChangeBMHHostname={onChangeBMHHostname}
-            onApproveAgent={onApproveAgent}
-            onDeleteHost={onDeleteHost}
-            getClusterDeploymentLink={getClusterDeploymentLink}
-            isBMPlatform={isBMPlatform}
             {...rest}
           />
         </GridItem>
--- src/cim/components/modals/AddHostModal.tsx
+++ src/cim/components/modals/AddHostModal.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
 import { FormikHelpers } from 'formik';
-import { Modal, ModalVariant } from '@patternfly/react-core';
+import {
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+  Modal,
+  ModalVariant,
+  Spinner,
+} from '@patternfly/react-core';
 import { DownloadIso, DiscoveryImageConfigForm, DiscoveryImageFormValues } from '../../../common';
-import BMCForm from '../Agent/BMCForm';
 import { AddHostModalProps } from './types';
 import { useTranslation } from '../../../common/hooks/use-translation-wrapper';
+import { EnvironmentErrors } from '../InfraEnv/EnvironmentErrors';
+import { InfraEnvK8sResource } from '../../types';
 
-type AddHostModalStepType = 'bmc' | 'iso-config' | 'iso-download';
+type AddHostModalStepType = 'iso-config' | 'iso-download';
 
 const AddHostModal: React.FC<AddHostModalProps> = ({
   isOpen,
   onClose,
   infraEnv,
   agentClusterInstall,
-  onCreateBMH,
   onSaveISOParams,
-  usedHostnames,
+  docVersion,
 }) => {
   const hasDHCP = infraEnv.metadata?.labels?.networkType !== 'static';
   const sshPublicKey = infraEnv.spec?.sshAuthorizedKey || agentClusterInstall?.spec?.sshPublicKey;
@@ -52,37 +59,57 @@ const AddHostModal: React.FC<AddHostModalProps> = ({
       hasNoBodyWrapper
       id="add-host-modal"
     >
-      {dialogType === 'iso-config' && (
-        <DiscoveryImageConfigForm
-          onCancel={onClose}
-          handleSubmit={handleIsoConfigSubmit}
-          hideDiscoveryImageType={true} // So far configured by env variable on backend
-          imageType="full-iso" // So far the only option for CIM
-          sshPublicKey={sshPublicKey}
-          httpProxy={httpProxy}
-          httpsProxy={httpsProxy}
-          noProxy={noProxy}
-          hasDHCP={hasDHCP}
-        />
-      )}
-      {dialogType === 'iso-download' && (
-        <DownloadIso
-          onClose={onClose}
-          downloadUrl={infraEnv?.status?.isoDownloadURL}
-          onReset={() => setDialogType('iso-config')}
-          hasDHCP={hasDHCP}
-        />
-      )}
-      {dialogType === 'bmc' && (
-        <BMCForm
-          onCreateBMH={onCreateBMH}
-          onClose={onClose}
-          hasDHCP={hasDHCP}
-          infraEnv={infraEnv}
-          usedHostnames={usedHostnames}
-        />
-      )}
+      <EnvironmentErrors infraEnv={infraEnv} docVersion={docVersion} inModal>
+        {dialogType === 'iso-config' && (
+          <DiscoveryImageConfigForm
+            onCancel={onClose}
+            handleSubmit={handleIsoConfigSubmit}
+            hideDiscoveryImageType={true} // So far configured by env variable on backend
+            imageType="full-iso" // So far the only option for CIM
+            sshPublicKey={sshPublicKey}
+            httpProxy={httpProxy}
+            httpsProxy={httpsProxy}
+            noProxy={noProxy}
+            hasDHCP={hasDHCP}
+          />
+        )}
+        {dialogType === 'iso-download' && (
+          <GeneratingIsoDownload
+            onClose={onClose}
+            infraEnv={infraEnv}
+            onReset={() => setDialogType('iso-config')}
+            hasDHCP={hasDHCP}
+          />
+        )}
+      </EnvironmentErrors>
     </Modal>
+  );
+};
+
+const GeneratingIsoDownload = ({
+  infraEnv,
+  onClose,
+  onReset,
+  hasDHCP,
+}: {
+  infraEnv: InfraEnvK8sResource;
+  onClose: VoidFunction;
+  onReset: VoidFunction;
+  hasDHCP: boolean;
+}) => {
+  const { t } = useTranslation();
+  return infraEnv.status?.isoDownloadURL ? (
+    <DownloadIso
+      onClose={onClose}
+      downloadUrl={infraEnv.status.isoDownloadURL}
+      onReset={onReset}
+      hasDHCP={hasDHCP}
+    />
+  ) : (
+    <EmptyState>
+      <EmptyStateIcon icon={Spinner} />
+      <EmptyStateBody>{t('ai:Generating discovery ISO')}</EmptyStateBody>
+    </EmptyState>
   );
 };
 
