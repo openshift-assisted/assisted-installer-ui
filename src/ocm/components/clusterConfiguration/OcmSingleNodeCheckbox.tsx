b056703f1681eb3df99bbb9e173ce5e094bfee38
Montse Ortega <ammont82@users.noreply.github.com>
2023-04-05 10:44:18 +0200

    MGMT-14214: Disable and uncheck SNO checkbox when choose IBM Power or IBM Z archs - 2.18 (#2008)
    * Disable SNO checkbox when choose IBM Power or IBM Z archs
    
    * Update package.json
    
    * Update package.json
    
    * Disable SNO checkbox when choose IBM Power or IBM Z archs

--- src/ocm/components/clusterConfiguration/OcmSingleNodeCheckbox.tsx
+++ src/ocm/components/clusterConfiguration/OcmSingleNodeCheckbox.tsx
@@ -34,6 +34,7 @@ const OcmSingleNodeCheckbox: React.FC<OcmCheckboxProps> = ({
   const [field, meta, helpers] = useField<'None' | 'Full'>({ name: props.name, validate });
   const featureSupportLevelContext = useNewFeatureSupportLevel();
   const prevVersionRef = React.useRef(openshiftVersion);
+  const prevIsDisabled = React.useRef(props.isDisabled);
   const fieldId = getFieldId(props.name, 'input', idPostfix);
   const { t } = useTranslation();
   const { value } = meta;
@@ -54,6 +55,13 @@ const OcmSingleNodeCheckbox: React.FC<OcmCheckboxProps> = ({
     }
     prevVersionRef.current = openshiftVersion;
   }, [openshiftVersion, onChanged, featureSupportLevelContext]);
+
+  React.useEffect(() => {
+    if (prevIsDisabled.current !== props.isDisabled) {
+      onChanged(false);
+    }
+    prevIsDisabled.current = props.isDisabled;
+  }, [props.isDisabled, setValue, onChanged]);
 
   if (isSingleNodeOpenshiftEnabled && isSupportedVersionAvailable) {
     return (
