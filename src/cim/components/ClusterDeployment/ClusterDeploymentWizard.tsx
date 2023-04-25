f6ff922db62609efe127b1012c69f7f4cb5e6e32
Rastislav Wagner <rawagner@redhat.com>
2023-04-25 10:06:36 +0200

    CIM: Allow setting doc version for user-managed doc link (#2013)

--- src/cim/components/ClusterDeployment/ClusterDeploymentWizard.tsx
+++ src/cim/components/ClusterDeployment/ClusterDeploymentWizard.tsx
@@ -115,6 +115,7 @@ const ClusterDeploymentWizard: React.FC<ClusterDeploymentWizardProps> = ({
             fetchInfraEnv={fetchInfraEnv}
             isPreviewOpen={isPreviewOpen}
             onSetInstallationDiskId={hostActions.onSetInstallationDiskId}
+            docVersion={rest.docVersion}
           />
         );
       case 'review':
