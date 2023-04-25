a9fedd08f05d0d0a397134995df4a21bb56c47a3
Guillaume Vincent <118891193+rh-gvincent@users.noreply.github.com>
2023-04-25 13:24:40 +0200

    MGMT-13837: "Hosts not showing up?" modal redesign (#1986)
    Implementing the "Hosts not showing up?" modal redesign JIRA card.
    Introduce a SimpleAI Prism theme in order to match the desired code block in the modal.

--- src/common/components/ui/PrismCode.tsx
+++ src/common/components/ui/PrismCode.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import Highlight, { defaultProps, Language, PrismTheme } from 'prism-react-renderer';
 import { Text, TextVariants, ClipboardCopy, clipboardCopyFunc } from '@patternfly/react-core';
+import {
+  global_Color_100,
+  global_palette_purple_400,
+  global_palette_blue_300,
+} from '@patternfly/react-tokens';
 import defaultTheme from 'prism-react-renderer/themes/github';
 import './PrismCode.css';
+export const SimpleAIPrismTheme = {
+  plain: {
+    color: global_Color_100.value,
+    backgroundColor: defaultTheme.plain.backgroundColor,
+    fontSize: '.93em',
+  },
+  styles: [
+    {
+      types: ['variable'],
+      style: {
+        color: global_palette_blue_300.value,
+      },
+    },
+    {
+      types: ['class-name', 'function', 'tag', 'attr-name'],
+      style: {
+        color: global_palette_purple_400.value,
+      },
+    },
+  ],
+};
 type PrismCodeProps = {
   code: string;
   language?: Language;
