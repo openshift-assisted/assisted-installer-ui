0f70b9352304da7b978468848c581847100c18a8
Jonathan Kilzi <jkilzi@redhat.com>
2023-06-01 10:34:05 +0300

    Fixes a test failure

--- libs/ui-lib-tests/cypress/support/variables/storage-step.ts
+++ libs/ui-lib-tests/cypress/support/variables/storage-step.ts
@@ -1,5 +1,5 @@
 // Storage Step
-Cypress.env('skipFormattingWarningTitle', 'There may be issues with the boot order');
+Cypress.env('skipFormattingWarningTitle', 'There might be issues with the boot order');
 Cypress.env(
   'skipFormattingWarningDesc',
   'You have opted out of formatting bootable disks on some hosts. To ensure the hosts reboot into the expected installation disk, manual user intervention may be required during OpenShift installation.',
