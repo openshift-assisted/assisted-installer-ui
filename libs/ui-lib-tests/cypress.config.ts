d921fbe4f0bc7eacb8382a499730829f5ce5d0a9
Jonathan Kilzi <jkilzi@redhat.com>
2023-06-10 22:21:25 +0300

    Removes unused env-vars
    - REMOTE_SSH_ID_FILE
    - REMOTE_USER
    - REMOTE_HOST
    - DEV_FLAG
    - ASSISTED_MINIMAL_ISO

--- libs/ui-lib-tests/cypress.config.ts
+++ libs/ui-lib-tests/cypress.config.ts
@@ -14,17 +14,12 @@ export default defineConfig({
     DNS_DOMAIN_NAME: 'redhat.com',
     CLUSTER_NAME: 'ai-e2e-sno',
     HOST_RENAME: 'e2e-control-host',
-    REMOTE_SSH_ID_FILE: '',
-    REMOTE_USER: '',
-    REMOTE_HOST: '',
     PULL_SECRET: JSON.stringify(dummyPullSecret),
     API_BASE_URL: GLOBAL_BASE_URL,
     API_VERSION: 'v2',
-    DEV_FLAG: true,
     NUM_MASTERS: '1',
     NUM_WORKERS: '0',
     HOST_REGISTRATION_TIMEOUT: GLOBAL_TIMEOUT,
-    ASSISTED_MINIMAL_ISO: false,
   },
   e2e: {
     setupNodeEvents(on, config) {
