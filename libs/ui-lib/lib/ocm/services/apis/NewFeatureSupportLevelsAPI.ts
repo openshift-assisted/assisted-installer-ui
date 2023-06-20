0811db33d2d9984c0f449ed7b8ce21c3df8acd7c
Montse Ortega <mortegag@redhat.com>
2023-06-20 12:52:11 +0200

    Creating service for new feature support levels

--- libs/ui-lib/lib/ocm/services/apis/NewFeatureSupportLevelsAPI.ts
+++ libs/ui-lib/lib/ocm/services/apis/NewFeatureSupportLevelsAPI.ts
@@ -3,18 +3,16 @@ import {
   ArchitecturesSupportsLevel,
   FeaturesSupportsLevel,
 } from '../../components/newFeatureSupportLevels/types';
-import { getCpuArchitecture } from '../CpuArchitectureService';
 
 const NewFeatureSupportLevelsAPI = {
   makeBaseURI() {
     return `/v2/support-levels`;
   },
 
-  featuresSupportLevel(openshiftVersion: string, cpuArchitecture?: string, isOcm?: boolean) {
-    const cpuArch = getCpuArchitecture(cpuArchitecture, isOcm);
+  featuresSupportLevel(openshiftVersion: string, cpuArchitecture?: string) {
     let queryParams = '?';
     queryParams += openshiftVersion ? `openshift_version=${openshiftVersion}&` : '';
-    queryParams += cpuArchitecture ? `cpu_architecture=${cpuArch}` : '';
+    queryParams += cpuArchitecture ? `cpu_architecture=${cpuArchitecture}` : '';
     return clientWithoutConverter.get<FeaturesSupportsLevel>(
       `${NewFeatureSupportLevelsAPI.makeBaseURI()}/features${queryParams}`,
     );
