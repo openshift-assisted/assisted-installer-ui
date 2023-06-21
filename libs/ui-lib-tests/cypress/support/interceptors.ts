46b8b6048cd39fea7235dc419aefa1fd91570f5e
Julie Gyselová <jgyselov@redhat.com>
2023-06-21 12:30:52 +0200

    MGMT-14788 - Closing error from BE in operators page - UI should reflect the real status of the cluster (#2182)
    * Make wizard footer alerts not dismisable
    
    * Tests for error response, alert dismissability, and Next button
    
    ---------
    
    Co-authored-by: Jonathan Kilzi <jkilzi@redhat.com>

--- libs/ui-lib-tests/cypress/support/interceptors.ts
+++ libs/ui-lib-tests/cypress/support/interceptors.ts
@@ -76,6 +76,19 @@ const mockClusterResponse: HttpRequestInterceptor = (req) => {
   }
 };
 
+const mockClusterErrorResponse: HttpRequestInterceptor = (req) => {
+  req.reply({
+    statusCode: 400,
+    body: {
+      code: '400',
+      href: '',
+      id: 400,
+      kind: 'Error',
+      reason: 'This is an error response',
+    },
+  });
+};
+
 const mockSupportedPlatformsResponse: HttpRequestInterceptor = (req) => {
   const platforms = Cypress.env('AI_SUPPORTED_PLATFORMS') || ['none'];
   req.reply(platforms);
@@ -145,7 +158,11 @@ const addClusterPatchAndDetailsIntercepts = () => {
     }
 
     req.alias = 'update-cluster';
-    mockClusterResponse(req);
+    if (Cypress.env('AI_ERROR_CLUSTER_PATCH') === true) {
+      mockClusterErrorResponse(req);
+    } else {
+      mockClusterResponse(req);
+    }
   });
 };
 
