408956d9d968ba1bb16142718a5f6f6567fa05df
Marek Libra <marek.libra@gmail.com>
2023-06-25 21:13:25 +0200

    MGMT-14421: Fix I18N for Hosts not showing up

--- libs/ui-lib/lib/ocm/components/hosts/HostsTableEmptyState.tsx
+++ libs/ui-lib/lib/ocm/components/hosts/HostsTableEmptyState.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ConnectedIcon } from '@patternfly/react-icons';
-import { pluralize } from 'humanize-plus';
 import EmptyState from '../../../common/components/ui/uiState/EmptyState';
+import { useTranslation } from '../../../common/hooks/use-translation-wrapper';
 import HostsDiscoveryTroubleshootingInfoLinkWithModal from './HostsDiscoveryTroubleshootingInfoLinkWithModal';
 
 export interface HostsTableEmptyStateProps {
@@ -9,11 +9,13 @@ export interface HostsTableEmptyStateProps {
 }
 
 const HostsTableEmptyState = ({ isSingleNode = false }: HostsTableEmptyStateProps) => {
+  const { t } = useTranslation();
+
   return (
     <EmptyState
       icon={ConnectedIcon}
-      title={`Waiting for ${pluralize(+isSingleNode, 'host')}...`}
-      content="Hosts may take a few minutes to appear here after booting."
+      title={t('ai:Waiting for host...', { count: isSingleNode ? 1 : 2 })}
+      content={t('ai:Hosts may take a few minutes to appear here after booting.')}
       secondaryActions={[
         <HostsDiscoveryTroubleshootingInfoLinkWithModal key={'hosts-not-showing'} />,
       ]}
