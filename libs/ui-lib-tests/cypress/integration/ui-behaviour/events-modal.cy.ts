be01082155be780c6b6d21586188c8a0bbdf4a17
jgyselov <jgyselov@redhat.com>
2023-06-26 12:34:40 +0200

    Events pagination test flakiness

--- libs/ui-lib-tests/cypress/integration/ui-behaviour/events-modal.cy.ts
+++ libs/ui-lib-tests/cypress/integration/ui-behaviour/events-modal.cy.ts
@@ -31,209 +31,116 @@ describe('Events modal behavior', () => {
 
   it('Can use pagination', () => {
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[0]).eq('1');
-        expect(res[1]).eq('10');
-        expect(res[2]).eq('28');
-      });
       eventsModal.contents.should('have.length', 10);
+
+      eventsModal.pagination.first.should('be.disabled');
+      eventsModal.pagination.previous.should('be.disabled');
     });
 
     eventsModal.pagination.next.click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[0]).eq('11');
-        expect(res[1]).eq('20');
-        expect(res[2]).eq('28');
-      });
       eventsModal.contents.should('have.length', 10);
-    });
 
-    eventsModal.pagination.previous.click();
-    cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[0]).eq('1');
-        expect(res[1]).eq('10');
-        expect(res[2]).eq('28');
-      });
-      eventsModal.contents.should('have.length', 10);
+      eventsModal.pagination.first.should('be.enabled');
+      eventsModal.pagination.previous.should('be.enabled');
+      eventsModal.pagination.next.should('be.enabled');
+      eventsModal.pagination.last.should('be.enabled');
     });
 
     eventsModal.pagination.last.click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[0]).eq('21');
-        expect(res[1]).eq('28');
-        expect(res[2]).eq('28');
-      });
       eventsModal.contents.should('have.length', 8);
+
+      eventsModal.pagination.first.should('be.enabled');
+      eventsModal.pagination.previous.should('be.enabled');
+      eventsModal.pagination.next.should('be.disabled');
+      eventsModal.pagination.last.should('be.disabled');
     });
 
     eventsModal.pagination.first.click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[0]).eq('1');
-        expect(res[1]).eq('10');
-        expect(res[2]).eq('28');
-      });
       eventsModal.contents.should('have.length', 10);
+
+      eventsModal.pagination.first.should('be.disabled');
+      eventsModal.pagination.previous.should('be.disabled');
+      eventsModal.pagination.next.should('be.enabled');
+      eventsModal.pagination.last.should('be.enabled');
     });
 
     eventsModal.pagination.menuText.click();
     eventsModal.pagination.perPageOption(50).click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[0]).eq('1');
-        expect(res[1]).eq('28');
-        expect(res[2]).eq('28');
-      });
+      eventsModal.contents.should('have.length', 28);
       eventsModal.pagination.first.should('be.disabled');
       eventsModal.pagination.previous.should('be.disabled');
       eventsModal.pagination.next.should('be.disabled');
       eventsModal.pagination.last.should('be.disabled');
-      eventsModal.contents.should('have.length', 28);
     });
   });
 
   it('Can use hosts filters', () => {
+    eventsModal.pagination.menuText.click();
+    eventsModal.pagination.perPageOption(50).click();
+
     eventsModal.hostFilter.control.click();
     eventsModal.hostFilter.option(hostIds[0]).click();
 
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('7');
-      });
       eventsModal.contents.should('have.length', 7);
     });
 
     eventsModal.hostFilter.option(hostIds[1]).click();
     eventsModal.hostFilter.option(hostIds[2]).click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('21');
-      });
-      eventsModal.contents.should('have.length', 10);
+      eventsModal.contents.should('have.length', 21);
     });
 
     eventsModal.hostFilter.option('cluster-level-action').click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('28');
-      });
-      eventsModal.contents.should('have.length', 10);
+      eventsModal.contents.should('have.length', 28);
     });
   });
 
   it('Can use severity filters', () => {
+    eventsModal.pagination.menuText.click();
+    eventsModal.pagination.perPageOption(50).click();
     eventsModal.severityFilter.control.click();
 
     eventsModal.severityFilter.option('info').click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('20');
-      });
+      eventsModal.contents.should('have.length', 20);
     });
 
     eventsModal.severityFilter.option('warning').click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('28');
-      });
+      eventsModal.contents.should('have.length', 28);
     });
 
     eventsModal.severityFilter.option('info').click();
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('8');
-      });
+      eventsModal.contents.should('have.length', 8);
+    });
+
+    eventsModal.severityFilter.option('warning').click();
+    cy.wait('@events').then(() => {
+      eventsModal.contents.should('have.length', 28);
     });
   });
 
   it('Can use message filter', () => {
+    eventsModal.pagination.menuText.click();
+    eventsModal.pagination.perPageOption(50).click();
+
     eventsModal.messageFilter.type('registered');
     cy.wait(500); // UI debounce time
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('4');
-      });
+      eventsModal.contents.should('have.length', 4);
     });
 
     eventsModal.messageFilter.clear();
     cy.wait(500); // UI debounce time
     cy.wait('@events').then(() => {
-      eventsModal.spinner.should('not.exist');
-      eventsModal.pagination.menuText.then(($elem) => {
-        const res = $elem
-          .text()
-          .match(/(\d+)\s\-\s(\d+)\sof\s(\d+)/)
-          .slice(1, 4);
-        expect(res[2]).eq('28');
-      });
+      eventsModal.contents.should('have.length', 28);
     });
   });
 });
