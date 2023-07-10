62d3edd4935d5ec87c0a3b271de8a0585cd2481d
Celia Amador <camadorg@redhat.com>
2023-07-10 16:48:06 +0200

    Really fix merging of validations

--- libs/ui-lib-tests/cypress/fixtures/cluster/validations-info.ts
+++ libs/ui-lib-tests/cypress/fixtures/cluster/validations-info.ts
@@ -1,5 +1,5 @@
 // Imports from `lodash/module` are not working in the monorepo
-import { isArray, merge, cloneDeep } from 'lodash';
+import { isArray, mergeWith, cloneDeep } from 'lodash';
 
 const isNewValidationId = (prevGroup, newGroup) =>
   prevGroup.find((prev) => {
@@ -10,11 +10,11 @@ const addOrUpdateValidationInfo = (prevValidationGroup, newValidationGroup) => {
   if (isArray(prevValidationGroup) && isNewValidationId(prevValidationGroup, newValidationGroup)) {
     return prevValidationGroup.concat(newValidationGroup);
   }
-  return prevValidationGroup;
+  return newValidationGroup;
 };
 
 const upgradeValidationsInfo = (prevValidationInfo, newValidationInfo) => {
-  return merge(
+  return mergeWith(
     // Use clone to avoid changes propagating to other fixtures by reference
     cloneDeep(prevValidationInfo),
     cloneDeep(newValidationInfo),
