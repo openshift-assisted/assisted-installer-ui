22f1f98b20205dac9def38d7b131fd790edb4cfd
Montse Ortega <ammont82@users.noreply.github.com>
2023-07-18 10:07:22 +0200

    MGMT-15256: Solving problems uploading yaml in staticIp page (#2262)
    * Solving problems uploading yaml files
    
    * Removing unnecessary tests

--- libs/ui-lib/lib/common/components/ui/formik/CodeField.tsx
+++ libs/ui-lib/lib/common/components/ui/formik/CodeField.tsx
@@ -6,7 +6,6 @@ import { getFieldId } from './utils';
 import HelperText from './HelperText';
 import { CodeEditor } from '@patternfly/react-code-editor';
 import useFieldErrorMsg from '../../../hooks/useFieldErrorMsg';
-import { monaco } from 'react-monaco-editor';
 
 const CodeField = ({
   label,
@@ -24,33 +23,7 @@ const CodeField = ({
 }: CodeFieldProps) => {
   const [field, , { setValue, setTouched }] = useField({ name, validate });
   const fieldId = getFieldId(name, 'input', idPostfix);
-  const [monacoSubscription, setMonacoSubscription] = React.useState<monaco.IDisposable>();
-  const [monacoEditor, setMonacoEditor] = React.useState<monaco.editor.IStandaloneCodeEditor>();
   const errorMessage = useFieldErrorMsg({ name, validate });
-
-  React.useEffect(() => {
-    //Work around for Patternfly CodeEditor not calling onChange after upload or drag/drop files
-    //The temporary solution is to handle the changes by registering to the change events of the internal editor in CodeEditor
-    //Once issue https://github.com/patternfly/patternfly-react/issues/7341 will be resolved this code can be replaced with a simple onChange
-    if (!monacoEditor) {
-      return;
-    }
-    if (monacoSubscription) {
-      monacoSubscription.dispose();
-    }
-    setMonacoSubscription(
-      monacoEditor.onDidChangeModelContent(() => {
-        setTouched(true);
-        setValue(monacoEditor.getModel()?.getValue(), true);
-      }),
-    );
-    return () => {
-      if (monacoSubscription) {
-        monacoSubscription.dispose();
-      }
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [monacoEditor]);
 
   const isValid = !errorMessage;
   const fieldHelperText = <HelperText fieldId={fieldId}>{helperText}</HelperText>;
@@ -81,8 +54,13 @@ const CodeField = ({
             isLanguageLabelVisible
             height="400px"
             language={language}
-            onEditorDidMount={(editor) => setMonacoEditor(editor)}
             downloadFileName={downloadFileName}
+            onCodeChange={(value) => {
+              if (value !== '') {
+                setTouched(true);
+                setValue(value, true);
+              }
+            }}
           />
         </FormGroup>
       </StackItem>
