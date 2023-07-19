2e7097fad1feb8934e2f126fc43fd1389ca3a860
Montse Ortega <mortegag@redhat.com>
2023-07-19 14:12:42 +0200

    Adapting tests for last changes

--- libs/ui-lib-tests/cypress/integration/use-cases/create-cluster/with-external-partner-integrations.cy.ts
+++ libs/ui-lib-tests/cypress/integration/use-cases/create-cluster/with-external-partner-integrations.cy.ts
@@ -41,7 +41,10 @@ describe('Create a new cluster with external partner integrations', () => {
     it('Selecting external partner integrations checkbox enables custom manifests as well', () => {
       clusterDetailsPage.inputOpenshiftVersion('4.14');
       ClusterDetailsForm.externalPartnerIntegrationsControl.findLabel().click();
-      ClusterDetailsForm.customManifestsControl.findCheckbox().should('be.checked');
+      ClusterDetailsForm.customManifestsControl
+        .findCheckbox()
+        .should('be.checked')
+        .and('be.disabled');
     });
 
     it('External partner integrations checkbox is unselected after OCP < v4.14 is selected', () => {
