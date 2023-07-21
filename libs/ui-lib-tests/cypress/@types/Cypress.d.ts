e8b195be402198cbb9cd0a2fbfaa273963aa75c9
Jonathan Kilzi <jkilzi@redhat.com>
2023-07-21 12:01:16 +0300

    Fixes a flaky test (#2270)
    Fixes test-case: "Selecting external partner integrations checkbox enables custom manifests as well"
    Reporting: "AssertionError: Timed out retrying after 4000ms: expected '<input#form-input-addCustomManifest-field.pf-c-check__input>' to be 'checked'"

--- libs/ui-lib-tests/cypress/@types/Cypress.d.ts
+++ libs/ui-lib-tests/cypress/@types/Cypress.d.ts
@@ -1,5 +1,11 @@
 declare namespace Cypress {
   interface Chainable {
+    /**
+     * If `ancestorAlias` is defined, executes `cy.get(ancestorAlias).find(childSelector)`. Otherwise, executes `cy.get(childSelector)`.
+     * @param childSelector The CSS selector
+     * @param ancestorAlias The alias of the child's ancestor
+     */
+    findWithinOrGet(childSelector: string, ancestorAlias?: string): Chainable<JQuery<HTMLElement>>;
     pasteText(selector: string, text: string): Chainable<Element>;
     newByDataTestId(selector: string, timeout?: number): Chainable<JQuery<HTMLElement>>;
     hostDetailSelector(i: number, label: string, timeout?: number): Chainable<Element>;
---
+++ libs/ui-lib-tests/cypress/views/forms/ClusterDetails/Fields/OpenShiftVersionField.ts
@@ -0,0 +1,27 @@
+export class OpenShiftVersionField {
+  static readonly alias = `@${OpenShiftVersionField.name}`;
+  static readonly selector = '#form-control__form-input-openshiftVersion-field';
+
+  static init(ancestorAlias?: string) {
+    cy.findWithinOrGet(OpenShiftVersionField.selector, ancestorAlias).as(
+      OpenShiftVersionField.name,
+    );
+
+    return OpenShiftVersionField;
+  }
+
+  static findLabel() {
+    return cy.get(OpenShiftVersionField.alias).findByText(/openshift version/i);
+  }
+
+  static findDropdown() {
+    return cy.get(OpenShiftVersionField.alias).find('#form-input-openshiftVersion-field');
+  }
+
+  static selectVersion(version: string) {
+    OpenShiftVersionField.findDropdown().click();
+    OpenShiftVersionField.findDropdown().within(() => {
+      cy.findByRole('menuitem', { name: new RegExp(`openshift ${version}`, 'i') }).click();
+    });
+  }
+}
