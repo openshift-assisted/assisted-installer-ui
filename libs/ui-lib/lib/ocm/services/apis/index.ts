0a8f95660776f0096d41759de729affa39a32f82
Jonathan Kilzi <jkilzi@redhat.com>
2023-07-28 19:44:38 +0300

    Redirects ComponentVersionsAPI refs to common

--- libs/ui-lib/lib/ocm/services/apis/index.ts
+++ libs/ui-lib/lib/ocm/services/apis/index.ts
@@ -1,6 +1,6 @@
 export { default as ManagedDomainsAPI } from './ManagedDomainsAPI';
 export { default as SupportedOpenshiftVersionsAPI } from './SupportedOpenshiftVersionsAPI';
-export { default as ComponentVersionsAPI } from './ComponentVersionsAPI';
+export { default as ComponentVersionsAPI } from '../../../common/apis/assisted-service/ComponentVersionsAPI';
 export { default as ClustersAPI } from '../../../common/apis/assisted-service/ClustersAPI';
 export { default as HostsAPI } from './HostsAPI';
 export { default as InfraEnvsAPI } from './InfraEnvsAPI';
