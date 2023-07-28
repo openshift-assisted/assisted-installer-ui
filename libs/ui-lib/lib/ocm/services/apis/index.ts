984f45013d876acfda8c3c9cc47c90bdd841e536
Jonathan Kilzi <jkilzi@redhat.com>
2023-07-28 19:48:08 +0300

    Redirects InfraEnvsAPI refs to common

--- libs/ui-lib/lib/ocm/services/apis/index.ts
+++ libs/ui-lib/lib/ocm/services/apis/index.ts
@@ -3,6 +3,6 @@ export { default as SupportedOpenshiftVersionsAPI } from './SupportedOpenshiftVe
 export { default as ComponentVersionsAPI } from '../../../common/apis/assisted-service/ComponentVersionsAPI';
 export { default as ClustersAPI } from '../../../common/apis/assisted-service/ClustersAPI';
 export { default as HostsAPI } from '../../../common/apis/assisted-service/HostsAPI';
-export { default as InfraEnvsAPI } from './InfraEnvsAPI';
+export { default as InfraEnvsAPI } from '../../../common/apis/assisted-service/InfraEnvsAPI';
 export { default as EventsAPI } from '../../../common/apis/assisted-service/EventsAPI';
 export { default as NewFeatureSupportLevelsAPI } from './NewFeatureSupportLevelsAPI';
