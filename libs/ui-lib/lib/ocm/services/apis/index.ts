6bccb17c27cc13cd3baac7e675911ac5316f1df0
Jonathan Kilzi <jkilzi@redhat.com>
2023-07-28 19:49:40 +0300

    Redirects SupportedOpenshiftVersionsAPI refs to common

--- libs/ui-lib/lib/ocm/services/apis/index.ts
+++ libs/ui-lib/lib/ocm/services/apis/index.ts
@@ -1,5 +1,5 @@
 export { default as ManagedDomainsAPI } from '../../../common/apis/assisted-service/ManagedDomainsAPI';
-export { default as SupportedOpenshiftVersionsAPI } from './SupportedOpenshiftVersionsAPI';
+export { default as SupportedOpenshiftVersionsAPI } from '../../../common/apis/assisted-service/SupportedOpenshiftVersionsAPI';
 export { default as ComponentVersionsAPI } from '../../../common/apis/assisted-service/ComponentVersionsAPI';
 export { default as ClustersAPI } from '../../../common/apis/assisted-service/ClustersAPI';
 export { default as HostsAPI } from '../../../common/apis/assisted-service/HostsAPI';
