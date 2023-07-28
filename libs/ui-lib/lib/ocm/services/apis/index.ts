771e5b3c1a002486cb1254933d03940a23fbca1a
Jonathan Kilzi <jkilzi@redhat.com>
2023-07-28 19:47:01 +0300

    Redirects EventsAPI refs to common

--- libs/ui-lib/lib/ocm/services/apis/index.ts
+++ libs/ui-lib/lib/ocm/services/apis/index.ts
@@ -4,5 +4,5 @@ export { default as ComponentVersionsAPI } from '../../../common/apis/assisted-s
 export { default as ClustersAPI } from '../../../common/apis/assisted-service/ClustersAPI';
 export { default as HostsAPI } from './HostsAPI';
 export { default as InfraEnvsAPI } from './InfraEnvsAPI';
-export { default as EventsAPI } from './EventsAPI';
+export { default as EventsAPI } from '../../../common/apis/assisted-service/EventsAPI';
 export { default as NewFeatureSupportLevelsAPI } from './NewFeatureSupportLevelsAPI';
