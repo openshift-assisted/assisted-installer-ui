b2d7e97e36a0207ebf6f5414764dc271cf5cb058
Jonathan Kilzi <jkilzi@redhat.com>
2023-07-28 19:47:42 +0300

    Redirects HostsAPI refs to common

--- libs/ui-lib/lib/ocm/services/apis/index.ts
+++ libs/ui-lib/lib/ocm/services/apis/index.ts
@@ -2,7 +2,7 @@ export { default as ManagedDomainsAPI } from './ManagedDomainsAPI';
 export { default as SupportedOpenshiftVersionsAPI } from './SupportedOpenshiftVersionsAPI';
 export { default as ComponentVersionsAPI } from '../../../common/apis/assisted-service/ComponentVersionsAPI';
 export { default as ClustersAPI } from '../../../common/apis/assisted-service/ClustersAPI';
-export { default as HostsAPI } from './HostsAPI';
+export { default as HostsAPI } from '../../../common/apis/assisted-service/HostsAPI';
 export { default as InfraEnvsAPI } from './InfraEnvsAPI';
 export { default as EventsAPI } from '../../../common/apis/assisted-service/EventsAPI';
 export { default as NewFeatureSupportLevelsAPI } from './NewFeatureSupportLevelsAPI';
