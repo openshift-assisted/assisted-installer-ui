c48fd2b72e1326f35dab6d34f0fa93be51490299
Jonathan Kilzi <jkilzi@redhat.com>
2023-08-12 19:29:14 +0300

    Simplifies the AMS (mini) client

--- libs/ui-lib/lib/common/api/accounts-management-service/access-token-api.ts
+++ libs/ui-lib/lib/common/api/accounts-management-service/access-token-api.ts
@@ -1,19 +1,13 @@
-import { getOcmClient } from '../axiosClient';
+import { ocmClient as client } from '../axiosClient';
 import { AccessTokenCfg } from '@openshift-assisted/types/accounts-management-service';
 
-const client = getOcmClient();
 export const AccessTokenAPI = {
   makeBaseURI() {
-    return `/api/accounts_mgmt/v2/access_token`;
+    return `/api/accounts_mgmt/v1/access_token`;
   },
 
   async fetchPullSecret() {
-    let value: AccessTokenCfg | null = null;
-    if (client) {
-      const { data } = await client.post<AccessTokenCfg>(AccessTokenAPI.makeBaseURI());
-      value = data;
-    }
-
-    return value;
+    const response = await client?.post<AccessTokenCfg>(AccessTokenAPI.makeBaseURI());
+    return response?.data;
   },
 };
