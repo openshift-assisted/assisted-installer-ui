049e2deca324ddb7e455ba773bbae3ecabdf56c2
Marek Libra <marek.libra@gmail.com>
2023-08-12 20:31:25 +0200

    Split tests to multiple smaller tests and suites

--- libs/ui-lib/lib/ocm/components/clusterWizard/wizardTransition.test.ts
+++ libs/ui-lib/lib/ocm/components/clusterWizard/wizardTransition.test.ts
@@ -194,65 +194,121 @@ const flatValidationHostsStorage: HostValidation[] = [
   },
 ];
 
-// TODO: Add e2e happy flow test
-
-describe('OCM wizard transitions', () => {
-  test('canNextClusterDetails', () => {
-    // based on clusterDetailsStepValidationsMap
+// based on clusterDetailsStepValidationsMap
+describe('OCM wizard transition of Cluster Details', () => {
+  test('is disabled for initial cluster', () => {
     const cluster: Cluster = cloneDeep(clusterBase);
-    const validationInfo = cloneDeep(validationInfoClusterDetails);
-
     expect(canNextClusterDetails({ cluster })).toBe(false);
+  });
 
+  test('is enabled for a Ready cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
     cluster.status = 'ready';
     expect(canNextClusterDetails({ cluster })).toBe(true);
+  });
 
+  test('is disabled for a pending required validation', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoClusterDetails);
     cluster.status = clusterBase.status;
     cluster.validationsInfo = JSON.stringify(validationInfo);
     expect(canNextClusterDetails({ cluster })).toBe(false);
+  });
 
+  test('is disabled for a failing required validation', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoClusterDetails);
     // @ts-expect-error Known object content
     validationInfo.configuration[1].status = 'error';
     cluster.validationsInfo = JSON.stringify(validationInfo);
     expect(canNextClusterDetails({ cluster })).toBe(false);
+  });
 
+  test('is enabled when all required validations are successful', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoClusterDetails);
     // @ts-expect-error Known object content
     validationInfo.configuration[1].status = 'success';
     cluster.validationsInfo = JSON.stringify(validationInfo);
     expect(canNextClusterDetails({ cluster })).toBe(true);
   });
+});
 
-  test('canNextHostDiscovery', () => {
-    // based on hostDiscoveryStepValidationsMap
+// based on hostDiscoveryStepValidationsMap
+describe('OCM wizard transition of Host Discovery', () => {
+  test('is disabled for initial cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    expect(canNextHostDiscovery({ cluster })).toBe(false);
+  });
+
+  test('is enabled for a Ready cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    cluster.status = 'ready';
+    expect(canNextHostDiscovery({ cluster })).toBe(true);
+  });
+
+  test('is enabled for a no-host cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoHostsDisovery);
+    cluster.status = clusterBase.status;
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    expect(canNextHostDiscovery({ cluster })).toBe(true); // no hosts so far
+  });
+
+  test('is disabled when a host is with missing validation', () => {
     const cluster: Cluster = cloneDeep(clusterBase);
     const validationInfo = cloneDeep(validationInfoHostsDisovery);
     const hosts: Host[] = cloneDeep(hostsHostDiscovery);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+    expect(canNextHostDiscovery({ cluster })).toBe(false);
+  });
+
+  test('is disabled when a host is with missing validation but the host is disabled', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoHostsDisovery);
+    const hosts: Host[] = cloneDeep(hostsHostDiscovery);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+    cluster.hosts[0].status = 'disabled';
+    expect(canNextHostDiscovery({ cluster })).toBe(true);
+  });
+
+  test('is enabled when all hosts are Known', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoHostsDisovery);
+    const hosts: Host[] = cloneDeep(hostsHostDiscovery);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+    cluster.hosts[0].status = 'known';
+    expect(canNextHostDiscovery({ cluster })).toBe(true);
+  });
+
+  test('is disabled when a host is with a failing required validation', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoHostsDisovery);
+    const hosts: Host[] = cloneDeep(hostsHostDiscovery);
+    const host0ValidationInfo: HostValidationsInfo = cloneDeep(hostValidationInfoHostsDiscovery);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+
+    cluster.hosts[0].status = 'insufficient';
+    cluster.hosts[0].validationsInfo = JSON.stringify(host0ValidationInfo);
+    expect(canNextHostDiscovery({ cluster })).toBe(false);
+  });
+
+  test('is enabled when all hosts are passing required validations but a host is insufficient', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoHostsDisovery);
+    const hosts: Host[] = cloneDeep(hostsHostDiscovery);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
     const host0ValidationInfo: HostValidationsInfo = cloneDeep(hostValidationInfoHostsDiscovery);
     const host0FlatValidationHostsDiscovery: HostValidation[] = cloneDeep(
       flatValidationHostsDiscovery,
     );
 
-    expect(canNextHostDiscovery({ cluster })).toBe(false);
-
-    cluster.status = 'ready';
-    expect(canNextHostDiscovery({ cluster })).toBe(true);
-
-    cluster.status = clusterBase.status;
-    cluster.validationsInfo = JSON.stringify(validationInfo);
-    expect(canNextHostDiscovery({ cluster })).toBe(true); // no hosts so far
-
-    cluster.hosts = hosts;
-    expect(canNextHostDiscovery({ cluster })).toBe(false);
-
-    cluster.hosts[0].status = 'disabled';
-    expect(canNextHostDiscovery({ cluster })).toBe(true);
-
-    cluster.hosts[0].status = 'known';
-    expect(canNextHostDiscovery({ cluster })).toBe(true);
-
     cluster.hosts[0].status = 'insufficient';
-    cluster.hosts[0].validationsInfo = JSON.stringify(host0ValidationInfo);
-    expect(canNextHostDiscovery({ cluster })).toBe(false);
 
     // @ts-expect-error Known object content
     host0ValidationInfo.hardware[1].status = 'success';
@@ -260,37 +316,82 @@ describe('OCM wizard transitions', () => {
     cluster.hosts[0].validationsInfo = JSON.stringify(host0ValidationInfo);
     expect(canNextHostDiscovery({ cluster })).toBe(true);
   });
+});
 
-  test('canNextStorage', () => {
-    // based on storageStepValidationsMap
+// based on storageStepValidationsMap
+describe('OCM wizard transition of Storage', () => {
+  test('is disabled for initial cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    expect(canNextStorage({ cluster })).toBe(false);
+  });
+
+  test('is enabled for a Ready cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    cluster.status = 'ready';
+    expect(canNextStorage({ cluster })).toBe(true);
+  });
+
+  test('is enabled for a no-host cluster', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoStorage);
+    cluster.status = clusterBase.status;
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    expect(canNextStorage({ cluster })).toBe(true); // no hosts so far
+  });
+
+  test('is disabled when a host is with missing validation', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoStorage);
+    const hosts: Host[] = cloneDeep(hostsStorage);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+    expect(canNextStorage({ cluster })).toBe(false);
+  });
+
+  test('is disabled when a host is with missing validation but the host is disabled', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoStorage);
+    const hosts: Host[] = cloneDeep(hostsStorage);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+
+    cluster.hosts = hosts;
+    cluster.hosts[0].status = 'disabled';
+    expect(canNextStorage({ cluster })).toBe(true);
+  });
+
+  test('is enabled when all hosts are Known', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoStorage);
+    const hosts: Host[] = cloneDeep(hostsStorage);
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+    cluster.hosts[0].status = 'known';
+    expect(canNextStorage({ cluster })).toBe(true);
+  });
+
+  test('is disabled when a host is with a failing required validation', () => {
+    const cluster: Cluster = cloneDeep(clusterBase);
+    const validationInfo = cloneDeep(validationInfoStorage);
+    const hosts: Host[] = cloneDeep(hostsStorage);
+    const host0ValidationInfo: HostValidationsInfo = cloneDeep(hostValidationInfoStorage);
+
+    cluster.validationsInfo = JSON.stringify(validationInfo);
+    cluster.hosts = hosts;
+    cluster.hosts[0].status = 'insufficient';
+    cluster.hosts[0].validationsInfo = JSON.stringify(host0ValidationInfo);
+    expect(canNextStorage({ cluster })).toBe(false);
+  });
+
+  test('is enabled when all hosts are passing required validations but a host is insufficient', () => {
     const cluster: Cluster = cloneDeep(clusterBase);
     const validationInfo = cloneDeep(validationInfoStorage);
     const hosts: Host[] = cloneDeep(hostsStorage);
     const host0ValidationInfo: HostValidationsInfo = cloneDeep(hostValidationInfoStorage);
     const host0FlatValidation: HostValidation[] = cloneDeep(flatValidationHostsStorage);
 
-    expect(canNextStorage({ cluster })).toBe(false);
-
-    cluster.status = 'ready';
-    expect(canNextStorage({ cluster })).toBe(true);
-
-    cluster.status = clusterBase.status;
     cluster.validationsInfo = JSON.stringify(validationInfo);
-    expect(canNextStorage({ cluster })).toBe(true); // no hosts so far
-
     cluster.hosts = hosts;
-    expect(canNextStorage({ cluster })).toBe(false);
-
-    cluster.hosts[0].status = 'disabled';
-    expect(canNextStorage({ cluster })).toBe(true);
-
-    cluster.hosts[0].status = 'known';
-    expect(canNextStorage({ cluster })).toBe(true);
-
     cluster.hosts[0].status = 'insufficient';
-    cluster.hosts[0].validationsInfo = JSON.stringify(host0ValidationInfo);
-    expect(canNextStorage({ cluster })).toBe(false);
-
     // @ts-expect-error Known object content
     host0ValidationInfo.hardware[1].status = 'success';
     host0ValidationInfo.operators = host0FlatValidation;
