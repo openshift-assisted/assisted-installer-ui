55695ca6b8d898ddf375c7f4bf008355eb4c9d7d
Jonathan Kilzi <jkilzi@redhat.com>
2023-08-29 15:40:23 +0300

    Simplifies the app routes

--- apps/assisted-ui/src/components/App/App.tsx
+++ apps/assisted-ui/src/components/App/App.tsx
@@ -1,4 +1,3 @@
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Page } from '@patternfly/react-core';
 import * as OCM from '@openshift-assisted/ui-lib/ocm';
@@ -6,33 +5,24 @@ import Header from './components/Header';
 import BackgroundImage from './components/BackgroundImage';
 import '../../i18n';
 
-const {
-  HostsClusterDetailTabMock: Day2AddHostsMock,
-  Router: LibRouter,
-  Features,
-  Store,
-  Config,
-} = OCM;
+const { HostsClusterDetailTabMock, Routes, Features, Config } = OCM;
 window.__app__ = { OCM };
 const { routeBasePath } = Config;
-const { store } = Store;
 
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <BackgroundImage />
-      <Page
-        header={<Header />}
-        style={{ height: '100vh', background: 'transparent' }}
-        isManagedSidebar
-        defaultManagedSidebarIsOpen={false}
-      >
-        <LibRouter features={Features.STANDALONE_DEPLOYMENT_ENABLED_FEATURES}>
-          <Route path={`${routeBasePath}/day2-flow-mock`} component={Day2AddHostsMock} />
-        </LibRouter>
-      </Page>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <BackgroundImage />
+    <Page
+      header={<Header />}
+      style={{ height: '100vh', background: 'transparent' }}
+      isManagedSidebar
+      defaultManagedSidebarIsOpen={false}
+    >
+      <Routes allEnabledFeatures={Features.STANDALONE_DEPLOYMENT_ENABLED_FEATURES}>
+        <Route path={`${routeBasePath}/day2-flow-mock`} component={HostsClusterDetailTabMock} />
+      </Routes>
+    </Page>
+  </BrowserRouter>
 );
 
 export default App;
