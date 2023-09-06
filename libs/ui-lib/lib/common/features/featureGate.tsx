openshift-assisted/assisted-installer-ui

commit 6d9581ddc19935685e323994c35d359c85f7ebf5

MGMT-7525: Removes obsolete feature-toggles (#2326)
* Removes traces of ASSISTED_INSTALLER_PLATFORM_OCI

Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

* Removes traces of ASSISTED_INSTALLER_SNO_FEATURE

Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

* Removes traces of ASSISTED_INSTALLER_OCS_FEATURE

Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

* Removes traces of ASSISTED_INSTALLER_ODF_FEATURE

Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

* Removes traces of ASSISTED_INSTALLER_CNV_FEATURE

Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

---------

Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

libs/ui-lib/lib/common/features/featureGate.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 // Must conform Unleash constants
 export type AssistedInstallerFeatureType =
-  | 'ASSISTED_INSTALLER_SNO_FEATURE'
-  | 'ASSISTED_INSTALLER_OCS_FEATURE'
-  | 'ASSISTED_INSTALLER_ODF_FEATURE'
-  | 'ASSISTED_INSTALLER_CNV_FEATURE'
   | 'ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE'
-  | 'ASSISTED_INSTALLER_MULTIARCH_SUPPORTED'
-  | 'ASSISTED_INSTALLER_PLATFORM_OCI';
+  | 'ASSISTED_INSTALLER_MULTIARCH_SUPPORTED';
 
 export type FeatureListType = {
   [key in AssistedInstallerFeatureType]?: boolean;
@@ -25,23 +20,14 @@ export type AssistedInstallerPermissionTypesListType = {
 
 // Hardcoded for ACM
 export const ACM_ENABLED_FEATURES: FeatureListType = {
-  ASSISTED_INSTALLER_SNO_FEATURE: true,
-  ASSISTED_INSTALLER_OCS_FEATURE: false,
-  ASSISTED_INSTALLER_ODF_FEATURE: false,
-  ASSISTED_INSTALLER_CNV_FEATURE: false,
   ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE: false,
   ASSISTED_INSTALLER_MULTIARCH_SUPPORTED: true,
 };
 
 // Hardcoded outside OCM
 export const STANDALONE_DEPLOYMENT_ENABLED_FEATURES: FeatureListType = {
-  ASSISTED_INSTALLER_SNO_FEATURE: true,
-  ASSISTED_INSTALLER_OCS_FEATURE: true,
-  ASSISTED_INSTALLER_ODF_FEATURE: true,
-  ASSISTED_INSTALLER_CNV_FEATURE: true,
   ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE: false,
   ASSISTED_INSTALLER_MULTIARCH_SUPPORTED: true,
-  ASSISTED_INSTALLER_PLATFORM_OCI: true,
 };
 
 export type FeatureGateContextType = {
@@ -58,9 +44,6 @@ export const FeatureGateContextProvider: React.FC<{
   // hardcoded defaults
   // TODO (mortegag): Remove all multiarch capacity related code in the UI.
   const featuresWithDefaults: FeatureListType = {
-    ASSISTED_INSTALLER_OCS_FEATURE: false,
-    ASSISTED_INSTALLER_ODF_FEATURE: false,
-    ASSISTED_INSTALLER_CNV_FEATURE: false,
     ASSISTED_INSTALLER_MULTIARCH_SUPPORTED: true,
     ...features,
   };

libs/ui-lib/lib/ocm/components/clusterConfiguration/OcmClusterDetailsFormFields.tsx
@@ -66,7 +66,6 @@ export const OcmClusterDetailsFormFields = ({
   const { t } = useTranslation();
   const isSingleClusterFeatureEnabled = useFeature('ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE');
   const isMultiArchSupported = useFeature('ASSISTED_INSTALLER_MULTIARCH_SUPPORTED');
-  const isOracleCloudPlatformIntegrationEnabled = useFeature('ASSISTED_INSTALLER_PLATFORM_OCI');
   const { openshiftVersion, platform } = values;
   const { getCpuArchitectures } = useOpenshiftVersions();
   const cpuArchitecturesByVersionImage = getCpuArchitectures(openshiftVersion);
@@ -172,7 +171,6 @@ export const OcmClusterDetailsFormFields = ({
         <ExternalPlatformDropdown
           onChange={handleExternalPartnerIntegrationsChange}
           cpuArchitecture={values.cpuArchitecture}
-          showOciOption={isOracleCloudPlatformIntegrationEnabled}
           featureSupportLevelData={featureSupportLevelData}
           isSNO={isSNO({ highAvailabilityMode })}
         />

libs/ui-lib/lib/ocm/components/clusterConfiguration/platformIntegration/ExternalPlatformDropdown.tsx
@@ -30,7 +30,6 @@ const INPUT_NAME = 'platform';
 const fieldId = getFieldId(INPUT_NAME, 'input');
 
 type ExternalPlatformDropdownProps = {
-  showOciOption: boolean;
   onChange: (selectedPlatform: PlatformType) => void;
   cpuArchitecture?: string;
   featureSupportLevelData: NewFeatureSupportLevelMap | null;
@@ -63,13 +62,12 @@ const getDisabledReasonForExternalPlatform = (
 };
 
 const getExternalPlatformTypes = (
-  showOciOption: boolean,
   isSNO: boolean,
   newFeatureSupportLevelContext: NewFeatureSupportLevelData,
   featureSupportLevelData?: NewFeatureSupportLevelMap | null,
   cpuArchitecture?: string,
 ): Partial<{ [key in PlatformType]: ExternalPlatformInfo }> => {
-  const platforms = ['none', 'nutanix', showOciOption && 'oci', 'vsphere'] as PlatformType[];
+  const platforms = ['none', 'nutanix', 'oci', 'vsphere'] as PlatformType[];
 
   return platforms.filter(Boolean).reduce(
     (a, platform) => ({
@@ -100,7 +98,6 @@ export const areAllExternalPlatformIntegrationDisabled = (
 };
 
 export const ExternalPlatformDropdown = ({
-  showOciOption,
   onChange,
   cpuArchitecture,
   featureSupportLevelData,
@@ -124,7 +121,6 @@ export const ExternalPlatformDropdown = ({
   React.useEffect(() => {
     // Calculate updated externalPlatformTypes based on the dependencies
     const updatedExternalPlatformTypes = getExternalPlatformTypes(
-      showOciOption,
       isSNO,
       newFeatureSupportLevelContext,
       featureSupportLevelData,

libs/ui-lib/lib/common/components/ui/formik/SingleNodeCheckbox.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { useField, useFormikContext } from 'formik';
 import { Checkbox, FormGroup, Tooltip } from '@patternfly/react-core';
-import { useFeature } from '../../../features';
 import { OpenshiftVersionOptionType } from '../../../types';
 import { getFieldId } from './utils';
 import HelperText from './HelperText';
@@ -21,7 +20,6 @@ const SingleNodeCheckbox: React.FC<SingleNodeCheckboxProps> = ({
   const {
     values: { openshiftVersion },
   } = useFormikContext<ClusterCreateParams>();
-  const isSingleNodeOpenshiftEnabled = useFeature('ASSISTED_INSTALLER_SNO_FEATURE');
   const [field, meta, helpers] = useField<'None' | 'Full'>({ name: props.name, validate });
   const featureSupportLevelContext = useFeatureSupportLevel();
   const prevVersionRef = React.useRef(openshiftVersion);
@@ -54,7 +52,7 @@ const SingleNodeCheckbox: React.FC<SingleNodeCheckboxProps> = ({
     prevVersionRef.current = openshiftVersion;
   }, [openshiftVersion, onChanged, featureSupportLevelContext]);
 
-  if (isSingleNodeOpenshiftEnabled && isSupportedVersionAvailable) {
+  if (isSupportedVersionAvailable) {
     return (
       <FormGroup isInline fieldId={fieldId}>
         <Tooltip hidden={!disabledReason} content={disabledReason}>

libs/ui-lib/lib/ocm/components/clusterConfiguration/OcmSingleNodeCheckbox.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useField, useFormikContext } from 'formik';
 import { Checkbox, FormGroup, Tooltip } from '@patternfly/react-core';
-import { getFieldId, HelperText, useFeature } from '../../../common';
+import { getFieldId, HelperText } from '../../../common';
 import { useNewFeatureSupportLevel } from '../../../common/components/newFeatureSupportLevels';
 import { CheckboxFieldProps } from '../../../common/components/ui/formik/types';
 import NewFeatureSupportLevelBadge from '../../../common/components/newFeatureSupportLevels/NewFeatureSupportLevelBadge';
@@ -28,7 +28,6 @@ const OcmSingleNodeCheckbox: React.FC<OcmCheckboxProps> = ({
   const {
     values: { openshiftVersion },
   } = useFormikContext<ClusterCreateParams>();
-  const isSingleNodeOpenshiftEnabled = useFeature('ASSISTED_INSTALLER_SNO_FEATURE');
   const [field, meta, helpers] = useField<'None' | 'Full'>({ name: props.name, validate });
   const featureSupportLevelContext = useNewFeatureSupportLevel();
   const prevVersionRef = React.useRef(openshiftVersion);
@@ -61,7 +60,7 @@ const OcmSingleNodeCheckbox: React.FC<OcmCheckboxProps> = ({
     prevIsDisabled.current = props.isDisabled;
   }, [props.isDisabled, setValue, onChanged]);
 
-  if (isSingleNodeOpenshiftEnabled && isSupportedVersionAvailable) {
+  if (isSupportedVersionAvailable) {
     return (
       <FormGroup id={`form-control__${fieldId}`} isInline fieldId={fieldId}>
         <Tooltip hidden={!disabledReason} content={disabledReason}>

libs/ui-lib/lib/ocm/components/clusterWizard/OperatorsStep.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Stack, StackItem } from '@patternfly/react-core';
-import { ClusterOperatorProps, ClusterWizardStepHeader, useFeature } from '../../../common';
+import { ClusterOperatorProps, ClusterWizardStepHeader } from '../../../common';
 import CnvCheckbox from '../clusterConfiguration/operators/CnvCheckbox';
 import OdfCheckbox from '../clusterConfiguration/operators/OdfCheckbox';
 import LvmCheckbox from '../clusterConfiguration/operators/LvmCheckbox';
@@ -10,32 +10,19 @@ import { selectIsCurrentClusterSNO } from '../../store/slices/current-cluster/se
 
 export const OperatorsStep = (props: ClusterOperatorProps) => {
   const isSNO = useSelector(selectIsCurrentClusterSNO);
-  const isOpenshiftDataFoundationEnabled = useFeature('ASSISTED_INSTALLER_OCS_FEATURE') && !isSNO;
-  const isContainerNativeVirtualizationEnabled = useFeature('ASSISTED_INSTALLER_CNV_FEATURE');
 
   return (
     <Stack hasGutter data-testid={'operators-form'}>
       <StackItem>
         <ClusterWizardStepHeader>Operators</ClusterWizardStepHeader>
       </StackItem>
-      {isContainerNativeVirtualizationEnabled && (
-        <StackItem>
-          <CnvCheckbox {...props} />
-        </StackItem>
-      )}
+      <StackItem>
+        <CnvCheckbox {...props} />
+      </StackItem>
       <StackItem>
         <MceCheckbox />
       </StackItem>
-      {isOpenshiftDataFoundationEnabled && (
-        <StackItem>
-          <OdfCheckbox />
-        </StackItem>
-      )}
-      {isSNO && (
-        <StackItem>
-          <LvmCheckbox {...props} />
-        </StackItem>
-      )}
+      <StackItem>{isSNO ? <LvmCheckbox {...props} /> : <OdfCheckbox />}</StackItem>
     </Stack>
   );
 };

commit 5e2f0b1d927c7b1cef4a40c15f8e98fc12595953

MGMT-15709: OCI option must be exposed only to selected users
The issue was introduced in #2326

libs/ui-lib/lib/common/features/featureGate.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 // Must conform Unleash constants
 export type AssistedInstallerFeatureType =
   | 'ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE'
-  | 'ASSISTED_INSTALLER_MULTIARCH_SUPPORTED';
+  | 'ASSISTED_INSTALLER_MULTIARCH_SUPPORTED'
+  | 'ASSISTED_INSTALLER_PLATFORM_OCI';
 
 export type FeatureListType = {
   [key in AssistedInstallerFeatureType]?: boolean;
@@ -28,6 +29,7 @@ export const ACM_ENABLED_FEATURES: FeatureListType = {
 export const STANDALONE_DEPLOYMENT_ENABLED_FEATURES: FeatureListType = {
   ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE: false,
   ASSISTED_INSTALLER_MULTIARCH_SUPPORTED: true,
+  ASSISTED_INSTALLER_PLATFORM_OCI: true,
 };
 
 export type FeatureGateContextType = {

libs/ui-lib/lib/ocm/components/clusterConfiguration/OcmClusterDetailsFormFields.tsx
@@ -66,6 +66,7 @@ export const OcmClusterDetailsFormFields = ({
   const { t } = useTranslation();
   const isSingleClusterFeatureEnabled = useFeature('ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE');
   const isMultiArchSupported = useFeature('ASSISTED_INSTALLER_MULTIARCH_SUPPORTED');
+  const isOracleCloudPlatformIntegrationEnabled = useFeature('ASSISTED_INSTALLER_PLATFORM_OCI');
   const { openshiftVersion, platform } = values;
   const { getCpuArchitectures } = useOpenshiftVersions();
   const cpuArchitecturesByVersionImage = getCpuArchitectures(openshiftVersion);
@@ -171,6 +172,7 @@ export const OcmClusterDetailsFormFields = ({
         <ExternalPlatformDropdown
           onChange={handleExternalPartnerIntegrationsChange}
           cpuArchitecture={values.cpuArchitecture}
+          showOciOption={isOracleCloudPlatformIntegrationEnabled}
           featureSupportLevelData={featureSupportLevelData}
           isSNO={isSNO({ highAvailabilityMode })}
         />

libs/ui-lib/lib/ocm/components/clusterConfiguration/platformIntegration/ExternalPlatformDropdown.tsx
@@ -30,6 +30,7 @@ const INPUT_NAME = 'platform';
 const fieldId = getFieldId(INPUT_NAME, 'input');
 
 type ExternalPlatformDropdownProps = {
+  showOciOption: boolean;
   onChange: (selectedPlatform: PlatformType) => void;
   cpuArchitecture?: string;
   featureSupportLevelData: NewFeatureSupportLevelMap | null;
@@ -62,12 +63,13 @@ const getDisabledReasonForExternalPlatform = (
 };
 
 const getExternalPlatformTypes = (
+  showOciOption: boolean,
   isSNO: boolean,
   newFeatureSupportLevelContext: NewFeatureSupportLevelData,
   featureSupportLevelData?: NewFeatureSupportLevelMap | null,
   cpuArchitecture?: string,
 ): Partial<{ [key in PlatformType]: ExternalPlatformInfo }> => {
-  const platforms = ['none', 'nutanix', 'oci', 'vsphere'] as PlatformType[];
+  const platforms = ['none', 'nutanix', showOciOption && 'oci', 'vsphere'] as PlatformType[];
 
   return platforms.filter(Boolean).reduce(
     (a, platform) => ({
@@ -98,6 +100,7 @@ export const areAllExternalPlatformIntegrationDisabled = (
 };
 
 export const ExternalPlatformDropdown = ({
+  showOciOption,
   onChange,
   cpuArchitecture,
   featureSupportLevelData,
@@ -121,6 +124,7 @@ export const ExternalPlatformDropdown = ({
   React.useEffect(() => {
     // Calculate updated externalPlatformTypes based on the dependencies
     const updatedExternalPlatformTypes = getExternalPlatformTypes(
+      showOciOption,
       isSNO,
       newFeatureSupportLevelContext,
       featureSupportLevelData,