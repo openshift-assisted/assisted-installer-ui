f111423e2e29721f3cc90ee1c5ed2e4a68c5c18a
jgyselov <jgyselov@redhat.com>
2023-09-14 16:07:50 +0200

    UI Settings API + Service

---
+++ libs/ui-lib/lib/common/api/assisted-service/UISettingsAPI.ts
@@ -0,0 +1,24 @@
+import { clientWithoutCaseConverter as client } from '../../api/axiosClient';
+import { Cluster } from '@openshift-assisted/types/./assisted-installer-service';
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const UISettingsAPI = {
+  makeBaseURI(clusterId?: Cluster['id']) {
+    return `/v2/clusters/${clusterId ? clusterId : ''}/ui-settings`;
+  },
+
+  get(clusterId?: Cluster['id']) {
+    return client.get(UISettingsAPI.makeBaseURI(clusterId));
+  },
+
+  put(clusterId: Cluster['id'], data: string) {
+    return client.put(UISettingsAPI.makeBaseURI(clusterId), data, config);
+  },
+};
+
+export default UISettingsAPI;
