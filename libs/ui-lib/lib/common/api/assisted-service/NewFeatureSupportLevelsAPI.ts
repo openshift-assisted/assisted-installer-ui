23390ea426e3b1f45fc5f31486760d99b9bba973
Montse Ortega <mortegag@redhat.com>
2023-09-19 08:21:55 +0200

    Change method featureSupportLevel in NewFeatureSupportLevelsAPI

--- libs/ui-lib/lib/common/api/assisted-service/NewFeatureSupportLevelsAPI.ts
+++ libs/ui-lib/lib/common/api/assisted-service/NewFeatureSupportLevelsAPI.ts
@@ -10,12 +10,15 @@ const NewFeatureSupportLevelsAPI = {
   },
 
   featuresSupportLevel(openshiftVersion: string, cpuArchitecture?: string, platformType?: string) {
-    let queryParams = '?';
-    queryParams += openshiftVersion ? `openshift_version=${openshiftVersion}&` : '';
-    queryParams += cpuArchitecture ? `cpu_architecture=${cpuArchitecture}&` : '';
-    queryParams += platformType ? `platform_type=${platformType}` : '';
     return clientWithoutCaseConverter.get<FeaturesSupportsLevel>(
-      `${NewFeatureSupportLevelsAPI.makeBaseURI()}/features${queryParams}`,
+      `${NewFeatureSupportLevelsAPI.makeBaseURI()}/features`,
+      {
+        params: {
+          openshift_version: openshiftVersion,
+          cpu_architecture: cpuArchitecture,
+          platform_type: platformType,
+        },
+      },
     );
   },
 
