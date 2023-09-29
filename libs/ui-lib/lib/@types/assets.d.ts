ad817a67e8ce756b16d0556d4e18a8eaf6fd0b39
Jonathan Kilzi <jkilzi@redhat.com>
2023-09-29 22:09:42 +0300

    Tidies up the @types folder

--- libs/ui-lib/lib/@types/assets.d.ts
+++ libs/ui-lib/lib/@types/assets.d.ts
@@ -2,4 +2,4 @@
  * We use this in order to get decent type definitions
  * for asset files like .svg, .css, .jpg, etc...
  */
-/// <reference types="vite/client" />
+import 'vite/client';
--- libs/ui-lib/lib/@types/insights.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface Insights {
-  chrome: import('@redhat-cloud-services/types').ChromeAPI;
-}
-
-interface Window {
-  insights?: Insights;
-}
