e8e28b1c2af1d2d396242df0d672f257cdfeefb7
Jonathan Kilzi <jkilzi@redhat.com>
2023-10-11 11:35:35 +0300

    Removes ASSISTED_INSTALLER_MULTIARCH_SUPPORTED from the list of external features

--- libs/ui-lib/lib/ocm/config/external-features.ts
+++ libs/ui-lib/lib/ocm/config/external-features.ts
@@ -9,8 +9,4 @@ export const externalFeaturesMappings: Array<FeatureMapping> = [
     featureId: 'ASSISTED_INSTALLER_PLATFORM_OCI',
     capabilityId: 'capability.organization.bare_metal_installer_platform_oci',
   },
-  {
-    featureId: 'ASSISTED_INSTALLER_MULTIARCH_SUPPORTED',
-    capabilityId: 'capability.organization.bare_metal_installer_multiarch',
-  },
 ];
