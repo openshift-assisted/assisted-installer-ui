99ba17d459ac5bf3f82e0659181ede1dd84590c0
rawagner <rawagner@redhat.com>
2023-10-13 13:07:41 +0200

    Add warning icon and i18n to delete host modal

--- libs/ui-lib/lib/ocm/components/hosts/DeleteHostModal.tsx
+++ libs/ui-lib/lib/ocm/components/hosts/DeleteHostModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal, ModalVariant, Button, ButtonVariant } from '@patternfly/react-core';
+import { useTranslation } from '../../../common/hooks/use-translation-wrapper';
 
 type DeleteHostModalProps = {
   hostname?: string;
@@ -8,28 +9,32 @@ type DeleteHostModalProps = {
   onDelete: () => void;
 };
 
-const DeleteHostModal = ({ isOpen, hostname, onClose, onDelete }: DeleteHostModalProps) => (
-  <Modal
-    title="Remove host?"
-    isOpen={isOpen}
-    onClose={onClose}
-    variant={ModalVariant.small}
-    actions={[
-      <Button
-        data-testid="delete-host-submit"
-        key="confirm"
-        variant={ButtonVariant.danger}
-        onClick={onDelete}
-      >
-        Remove host
-      </Button>,
-      <Button key="cancel" variant={ButtonVariant.link} onClick={onClose}>
-        Cancel
-      </Button>,
-    ]}
-  >
-    Host {hostname || ''} will be removed.
-  </Modal>
-);
+const DeleteHostModal = ({ isOpen, hostname, onClose, onDelete }: DeleteHostModalProps) => {
+  const { t } = useTranslation();
+  return (
+    <Modal
+      title={t('ai:Remove host?')}
+      isOpen={isOpen}
+      onClose={onClose}
+      variant={ModalVariant.small}
+      titleIconVariant="warning"
+      actions={[
+        <Button
+          data-testid="delete-host-submit"
+          key="confirm"
+          variant={ButtonVariant.danger}
+          onClick={onDelete}
+        >
+          {t('ai:Remove host')}
+        </Button>,
+        <Button key="cancel" variant={ButtonVariant.link} onClick={onClose}>
+          {t('ai:Cancel')}
+        </Button>,
+      ]}
+    >
+      {t('ai:Host {{hostname}} will be removed', { hostname: hostname || '' })}
+    </Modal>
+  );
+};
 
 export default DeleteHostModal;
