869c475382500a92aab10315194e486d84656d77
Montse Ortega <mortegag@redhat.com>
2023-11-02 07:28:54 +0100

    Adjusting tests

--- libs/ui-lib-tests/cypress/integration/day-2/2-adding-hosts.cy.ts
+++ libs/ui-lib-tests/cypress/integration/day-2/2-adding-hosts.cy.ts
@@ -17,7 +17,7 @@ describe(`Assisted Installer Day2 flow`, () => {
     setTestStartSignal('CREATED_DAY2_CLUSTER');
     cy.visit('/day2-flow-mock');
     cy.window().then((win) => {
-      win.__app__.OCM.setAuthInterceptor((client) => client);
+      win.__app__.OCM.Api.setAuthInterceptor((client) => client);
     });
     cy.findByRole('button', { name: 'Add hosts (With metrics)' }).click();
     cy.wait('@day2-cluster-details');
