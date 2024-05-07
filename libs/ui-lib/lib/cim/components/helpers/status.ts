fb0219412ef0fc0573a8693e6830715dbac4cb5b
rawagner <rawagner@redhat.com>
2024-05-07 09:23:12 +0200

    MGMT-17706: Handle missing message/reason fields in resource conditions

--- libs/ui-lib/lib/cim/components/helpers/status.ts
+++ libs/ui-lib/lib/cim/components/helpers/status.ts
@@ -1,14 +1,6 @@
-import {
-  AgentClusterInstallK8sResource,
-  AgentClusterInstallStatusCondition,
-  AgentClusterInstallStatusConditionType,
-} from '../../types/k8s/agent-cluster-install';
+import { AgentClusterInstallK8sResource } from '../../types/k8s/agent-cluster-install';
 import { Cluster, Host } from '@openshift-assisted/types/assisted-installer-service';
-import {
-  AgentK8sResource,
-  AgentStatusCondition,
-  AgentStatusConditionType,
-} from '../../types/k8s/agent';
+import { AgentK8sResource } from '../../types/k8s/agent';
 import { BareMetalHostK8sResource } from '../../types/k8s/bare-metal-host';
 import { StatusCondition } from '../../types/k8s/shared';
 import { ValidationsInfo } from '../../../common/types/hosts';
@@ -35,50 +27,6 @@ const conditionsByTypeReducer = <K>(
   [condition.type]: condition,
 });
 
-export const getClusterStatusFromConditions = (
-  agentClusterInstall: AgentClusterInstallK8sResource,
-  t: TFunction,
-): [Cluster['status'], string] => {
-  const conditions = agentClusterInstall?.status?.conditions || [];
-
-  const conditionsByType: {
-    [key in AgentClusterInstallStatusConditionType]?: AgentClusterInstallStatusCondition;
-  } = conditions.reduce(conditionsByTypeReducer, {});
-
-  const { Validated, RequirementsMet, Completed, Stopped } = conditionsByType;
-
-  if (!Validated || !RequirementsMet || !Completed || !Stopped) {
-    return ['insufficient', t('ai:AgentClusterInstall conditions are missing.')];
-  }
-
-  if (Stopped.status === 'True' && Stopped.reason === 'InstallationCancelled')
-    return ['cancelled', Stopped.message];
-  if (Stopped.status === 'True' && Stopped.reason === 'InstallationFailed')
-    return ['error', Stopped.message];
-  if (Completed.status === 'True' && Completed.reason === 'InstallationCompleted')
-    return ['installed', Completed.message];
-  if (Completed.status === 'False' && Completed.reason === 'InstallationInProgress')
-    return ['installing', Completed.message];
-  if (Validated.status === 'False' && Validated.reason === 'ValidationsFailing')
-    return ['insufficient', Validated.message];
-  if (Validated.status === 'False' && Validated.reason === 'ValidationsUserPending')
-    return ['pending-for-input', Validated.message];
-  if (Validated.status === 'False' && Validated.reason === 'ValidationsUnknown')
-    return ['insufficient', Validated.message];
-
-  if (RequirementsMet.status === 'False' && RequirementsMet.reason === 'ClusterNotReady')
-    return ['insufficient', RequirementsMet.message];
-  if (RequirementsMet.status === 'False' && RequirementsMet.reason === 'InsufficientAgents')
-    return ['insufficient', RequirementsMet.message];
-  if (RequirementsMet.status === 'False' && RequirementsMet.reason === 'UnapprovedAgents')
-    return ['insufficient', RequirementsMet.message];
-  if (Completed.status === 'False' && Completed.reason === 'UnapprovedAgents')
-    return ['insufficient', Completed.message];
-
-  // console.error('Unhandled conditions to cluster status mapping: ', conditionsByType);
-  return ['insufficient', t('ai:Unexpected AgentClusterInstall conditions.')];
-};
-
 export const getClusterStatus = (
   agentClusterInstall?: AgentClusterInstallK8sResource,
 ): [Cluster['status'], Cluster['statusInfo']] => {
@@ -89,37 +37,6 @@ export const getClusterStatus = (
 export const isDraft = (agentClusterInstall?: AgentClusterInstallK8sResource): boolean =>
   !!agentClusterInstall &&
   ['pending-for-input', 'insufficient', 'ready'].includes(getClusterStatus(agentClusterInstall)[0]);
-
-export const getAgentStatusFromConditions = (
-  agent: AgentK8sResource,
-  t: TFunction,
-): [Host['status'], string] => {
-  const conditions = agent.status?.conditions;
-
-  const conditionsByType: {
-    [key in AgentStatusConditionType]?: AgentStatusCondition;
-  } = (conditions || []).reduce(conditionsByTypeReducer, {});
-
-  const { Installed, Connected, ReadyForInstallation } = conditionsByType;
-
-  if (Installed?.status === 'True') return ['installed', Installed.message];
-  if (Installed?.status === 'False' && Installed.reason === 'InstallationFailed')
-    return ['error', Installed.message];
-  if (Installed?.status === 'False' && Installed?.reason === 'InstallationInProgress')
-    return ['installing', Installed?.message];
-  if (Connected?.status === 'False') return ['disconnected', Connected.message];
-  if (ReadyForInstallation?.status === 'True') return ['known', ReadyForInstallation.message];
-  if (
-    ReadyForInstallation?.status === 'False' &&
-    ReadyForInstallation?.reason === 'AgentIsNotApproved'
-  )
-    return ['pending-for-input', ReadyForInstallation.message];
-  if (ReadyForInstallation?.status === 'False' && ReadyForInstallation?.reason === 'AgentNotReady')
-    return ['insufficient', ReadyForInstallation.message];
-
-  // console.error('Unhandled conditions to agent status mapping: ', conditionsByType);
-  return ['insufficient', t('ai:Unexpected Agent conditions.')];
-};
 
 export type AgentStatus = Host['status'] | 'discovered';
 
