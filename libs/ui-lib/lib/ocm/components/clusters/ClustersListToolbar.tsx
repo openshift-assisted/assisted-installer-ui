0cca6013474b294dbdc67d9cddc66c1492ad37a2
Montse Ortega <mortegag@redhat.com>
2024-08-14 10:14:04 +0200

    Create CustomToolbarFilter to avoid problems with cluster's list

--- libs/ui-lib/lib/ocm/components/clusters/ClustersListToolbar.tsx
+++ libs/ui-lib/lib/ocm/components/clusters/ClustersListToolbar.tsx
@@ -4,7 +4,6 @@ import {
   Toolbar,
   ToolbarItem,
   ToolbarContent,
-  ToolbarFilter,
   InputGroup,
   TextInput,
   ToolbarProps,
@@ -28,6 +27,7 @@ import { useTranslation } from '../../../common/hooks/use-translation-wrapper';
 import { Cluster } from '@openshift-assisted/types/assisted-installer-service';
 import { useDispatchDay1, useSelectorDay1 } from '../../store';
 import { selectClustersUIState } from '../../store/slices/clusters/selectors';
+import { CustomToolbarFilter } from './CustomToolbarFilter';
 
 export type ClusterFiltersType = {
   [key: string]: string[]; // value from clusterStatusLabels
@@ -102,6 +102,7 @@ const ClustersListToolbar: React.FC<ClustersListToolbarProps> = ({
     </>
   );
   const { t } = useTranslation();
+
   return (
     <Toolbar
       id="clusters-list-toolbar"
@@ -126,7 +127,7 @@ const ClustersListToolbar: React.FC<ClustersListToolbarProps> = ({
             </InputGroupItem>
           </InputGroup>
         </ToolbarItem>
-        <ToolbarFilter
+        <CustomToolbarFilter
           chips={filters.status}
           deleteChip={onDeleteChip}
           deleteChipGroup={onDeleteChipGroup}
@@ -150,7 +151,7 @@ const ClustersListToolbar: React.FC<ClustersListToolbarProps> = ({
               />
             ))}
           </Select>
-        </ToolbarFilter>
+        </CustomToolbarFilter>
         <ToolbarButton
           variant={ButtonVariant.primary}
           onClick={() => navigate(`~new`)}
