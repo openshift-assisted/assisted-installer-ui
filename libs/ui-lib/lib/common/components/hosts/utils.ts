859221951bc34ac4dd5803985bc3717d64fdf3d8
Julie Gyselová <jgyselov@redhat.com>
2024-10-07 12:42:15 +0200

    Fix agent table search (#2670)

--- libs/ui-lib/lib/common/components/hosts/utils.ts
+++ libs/ui-lib/lib/common/components/hosts/utils.ts
@@ -210,8 +210,10 @@ export const filterByHostname = (hosts: Host[], hostnameFilter: string | undefin
 
   const fuse = new Fuse(hostsWithHostname, {
     ignoreLocation: true,
+    threshold: 0.3,
     keys: ['hostname'],
   });
+
   return fuse.search(hostnameFilter).map(({ item }) => item.host);
 };
 
