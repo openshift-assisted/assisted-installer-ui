c3cd20d0b8acffe34301db50fd8736e60e56ac55
Julie Gyselová <jgyselov@redhat.com>
2024-11-21 12:08:28 +0100

    Filter OpenShift versions based on availability in AgentServiceConfig (#2714)

--- libs/ui-lib/lib/cim/components/common/CpuArchitectureDropdown.tsx
+++ libs/ui-lib/lib/cim/components/common/CpuArchitectureDropdown.tsx
@@ -12,9 +12,35 @@ import {
 import { useField } from 'formik';
 import { useFormikHelpers } from '../../../common/hooks/useFormikHelpers';
 
-const CpuArchitectureDropdown = ({ isDisabled = false }: { isDisabled?: boolean }) => {
+const allDropdownItems = {
+  [CpuArchitecture.x86]: (
+    <DropdownItem key={'x86_64'} id="x86_64" description={architectureData['x86_64'].description}>
+      {architectureData['x86_64'].label}
+    </DropdownItem>
+  ),
+  [CpuArchitecture.ARM]: (
+    <DropdownItem key={'arm64'} id="arm64" description={architectureData['arm64'].description}>
+      {architectureData['arm64'].label}
+    </DropdownItem>
+  ),
+  [CpuArchitecture.s390x]: (
+    <DropdownItem key={'s390x'} id="s390x" description={architectureData['s390x'].description}>
+      {architectureData['s390x'].label}
+    </DropdownItem>
+  ),
+};
+
+const CpuArchitectureDropdown = ({
+  isDisabled = false,
+  cpuArchitectures,
+}: {
+  isDisabled?: boolean;
+  cpuArchitectures?: string[];
+}) => {
   const { t } = useTranslation();
-  const [{ name, value }, , { setValue }] = useField<SupportedCpuArchitecture>('cpuArchitecture');
+  const [{ name, value }, , { setValue }] = useField<SupportedCpuArchitecture | ''>(
+    'cpuArchitecture',
+  );
   const [cpuArchOpen, setCpuArchOpen] = React.useState(false);
   const fieldId = getFieldId(name, 'input');
   const { setValue: setUserManagedNetworking } = useFormikHelpers<boolean>('userManagedNetworking');
@@ -27,8 +53,24 @@ const CpuArchitectureDropdown = ({ isDisabled = false }: { isDisabled?: boolean 
     setCpuArchOpen(false);
   };
 
+  const dropdownItems = React.useMemo(() => {
+    if (!cpuArchitectures) {
+      return Object.values(allDropdownItems);
+    } else {
+      return Object.entries(allDropdownItems)
+        .filter(([key, _]) => cpuArchitectures.includes(key))
+        .map(([_, val]) => val);
+    }
+  }, [cpuArchitectures]);
+
+  React.useEffect(() => {
+    if (!isDisabled && value !== '' && cpuArchitectures && !cpuArchitectures?.includes(value)) {
+      setValue('');
+    }
+  }, [cpuArchitectures, isDisabled, setValue, value]);
+
   return !isDisabled ? (
-    <FormGroup isInline fieldId={fieldId} label={t('ai:CPU architecture')} required>
+    <FormGroup isInline fieldId={fieldId} label={t('ai:CPU architecture')} isRequired>
       <Dropdown
         toggle={
           <DropdownToggle onToggle={() => setCpuArchOpen(!cpuArchOpen)} className="pf-u-w-100">
@@ -38,35 +80,13 @@ const CpuArchitectureDropdown = ({ isDisabled = false }: { isDisabled?: boolean 
         name="cpuArchitecture"
         isOpen={cpuArchOpen}
         onSelect={onCpuArchSelect}
-        dropdownItems={[
-          <DropdownItem
-            key={'x86_64'}
-            id="x86_64"
-            description={architectureData['x86_64'].description}
-          >
-            {architectureData['x86_64'].label}
-          </DropdownItem>,
-          <DropdownItem
-            key={'arm64'}
-            id="arm64"
-            description={architectureData['arm64'].description}
-          >
-            {architectureData['arm64'].label}
-          </DropdownItem>,
-          <DropdownItem
-            key={'s390x'}
-            id="s390x"
-            description={architectureData['s390x'].description}
-          >
-            {architectureData['s390x'].label}
-          </DropdownItem>,
-        ]}
+        dropdownItems={dropdownItems}
         className="pf-u-w-100"
       />
     </FormGroup>
   ) : (
     <StaticField name={'cpuArchitecture'} label={t('ai:CPU architecture')} isRequired>
-      {architectureData[value].label}
+      {architectureData[value as SupportedCpuArchitecture].label}
     </StaticField>
   );
 };
