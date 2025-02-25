2d74757e69003df45ec0281fbe7fcdd6ccb1e27b
Julie Gyselová <jgyselov@redhat.com>
2025-02-25 13:23:17 +0100

    CIM Wizard component upgrade (#2787)

--- libs/ui-lib/lib/cim/components/ClusterDeployment/ClusterDeploymentReviewStep.tsx
+++ libs/ui-lib/lib/cim/components/ClusterDeployment/ClusterDeploymentReviewStep.tsx
@@ -1,4 +1,12 @@
-import { Grid, GridItem } from '@patternfly/react-core';
+import {
+  Alert,
+  AlertVariant,
+  Grid,
+  GridItem,
+  useWizardContext,
+  useWizardFooter,
+  WizardFooter,
+} from '@patternfly/react-core';
 import * as React from 'react';
 import { canNextFromReviewStep } from './wizardTransition';
 import {
@@ -17,9 +25,6 @@ import {
   HostsValidations,
   useAlerts,
 } from '../../../common';
-import ClusterDeploymentWizardContext from './ClusterDeploymentWizardContext';
-import ClusterDeploymentWizardFooter from './ClusterDeploymentWizardFooter';
-import ClusterDeploymentWizardStep from './ClusterDeploymentWizardStep';
 import { getSelectedVersion, getAICluster, getClusterDeploymentCpuArchitecture } from '../helpers';
 import { isAgentOfCluster } from './helpers';
 import { wizardStepNames } from './constants';
@@ -29,31 +34,31 @@ import {
   allClusterWizardSoftValidationIds,
 } from './wizardTransition';
 import { useTranslation } from '../../../common/hooks/use-translation-wrapper';
+import { ClusterDeploymentWizardContext } from './ClusterDeploymentWizardContext';
+import { ValidationSection } from './components/ValidationSection';
 
 type ClusterDeploymentReviewStepProps = {
   clusterDeployment: ClusterDeploymentK8sResource;
   agentClusterInstall: AgentClusterInstallK8sResource;
   agents: AgentK8sResource[];
-  onClose: VoidFunction;
   // eslint-disable-next-line
   onFinish: () => Promise<any>;
   clusterImages: ClusterImageSetK8sResource[];
   infraEnv?: InfraEnvK8sResource;
 };
 
-const ClusterDeploymentReviewStep: React.FC<ClusterDeploymentReviewStepProps> = ({
+const ClusterDeploymentReviewStep = ({
   agentClusterInstall,
   agents,
-  onClose,
   onFinish,
   clusterDeployment,
   clusterImages,
   infraEnv,
-}) => {
+}: ClusterDeploymentReviewStepProps) => {
   const { addAlert, clearAlerts } = useAlerts();
-  const { setCurrentStepId } = React.useContext(ClusterDeploymentWizardContext);
+  const { activeStep, goToPrevStep, goToStepByName, close } = useWizardContext();
+  const { syncError } = React.useContext(ClusterDeploymentWizardContext);
   const [isSubmitting, setSubmitting] = React.useState(false);
-  const onBack = () => setCurrentStepId('networking');
   const cdName = clusterDeployment.metadata?.name;
   const cdNamespace = clusterDeployment.metadata?.namespace;
   const cpuArchitecture = getClusterDeploymentCpuArchitecture(clusterDeployment, infraEnv);
@@ -79,18 +84,17 @@ const ClusterDeploymentReviewStep: React.FC<ClusterDeploymentReviewStepProps> = 
   };
 
   const footer = (
-    <ClusterDeploymentWizardFooter
-      agentClusterInstall={agentClusterInstall}
-      agents={clusterAgents}
-      isSubmitting={isSubmitting}
-      isNextDisabled={isSubmitting || !canContinue}
-      onBack={onBack}
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <WizardFooter
+      activeStep={activeStep}
       onNext={onNext}
-      onCancel={onClose}
+      isNextDisabled={isSubmitting || !canContinue || !!syncError}
       nextButtonText={t('ai:Install cluster')}
+      nextButtonProps={{ isLoading: isSubmitting }}
+      onBack={goToPrevStep}
+      onClose={close}
     />
   );
+  useWizardFooter(footer);
 
   const openShiftVersion = getSelectedVersion(clusterImages, agentClusterInstall);
 
@@ -100,75 +104,82 @@ const ClusterDeploymentReviewStep: React.FC<ClusterDeploymentReviewStepProps> = 
   );
 
   return (
-    <ClusterDeploymentWizardStep footer={footer}>
-      <Grid hasGutter>
-        <GridItem>
-          <ClusterWizardStepHeader>Review and create</ClusterWizardStepHeader>
-        </GridItem>
-        <GridItem>
-          <DetailList>
-            <DetailItem
-              title={t('ai:Cluster address')}
-              value={`${clusterDeployment.metadata?.name || ''}.${
-                clusterDeployment.spec?.baseDomain || ''
-              }`}
-              testId="cluster-address"
-            />
-            <DetailItem
-              title={t('ai:OpenShift version')}
-              value={openShiftVersion}
-              testId="openshift-version"
-            />
-            <DetailItem
-              title={t('ai:CPU architecture')}
-              value={cpuArchitecture}
-              testId="cpu-architecture"
-            />
-            <DetailItem
-              title={t('ai:API IP')}
-              value={agentClusterInstall.spec?.apiVIP}
-              testId="api-vip"
-            />
-            <DetailItem
-              title={t('ai:Ingress IP')}
-              value={agentClusterInstall.spec?.ingressVIP}
-              testId="ingress-vip"
-            />
-            <DetailItem
-              title={t('ai:Cluster summary')}
-              testId="cluster-summary"
-              value={<ReviewHostsInventory hosts={cluster.hosts} />}
-            />
+    <Grid hasGutter>
+      <GridItem>
+        <ClusterWizardStepHeader>Review and create</ClusterWizardStepHeader>
+      </GridItem>
+      <GridItem>
+        <DetailList>
+          <DetailItem
+            title={t('ai:Cluster address')}
+            value={`${clusterDeployment.metadata?.name || ''}.${
+              clusterDeployment.spec?.baseDomain || ''
+            }`}
+            testId="cluster-address"
+          />
+          <DetailItem
+            title={t('ai:OpenShift version')}
+            value={openShiftVersion}
+            testId="openshift-version"
+          />
+          <DetailItem
+            title={t('ai:CPU architecture')}
+            value={cpuArchitecture}
+            testId="cpu-architecture"
+          />
+          <DetailItem
+            title={t('ai:API IP')}
+            value={agentClusterInstall?.status?.apiVIP || agentClusterInstall?.spec?.apiVIP}
+            testId="api-vip"
+          />
+          <DetailItem
+            title={t('ai:Ingress IP')}
+            value={agentClusterInstall?.status?.ingressVIP || agentClusterInstall?.spec?.ingressVIP}
+            testId="ingress-vip"
+          />
+          <DetailItem
+            title={t('ai:Cluster summary')}
+            testId="cluster-summary"
+            value={<ReviewHostsInventory hosts={cluster.hosts} />}
+          />
 
-            <DetailItem
-              title={t('ai:Cluster validations')}
-              value={
-                <ClusterValidations<ClusterWizardStepsType>
-                  validationsInfo={cluster.validationsInfo}
-                  setCurrentStepId={setCurrentStepId}
-                  wizardStepNames={wizardStepNames(t)}
-                  wizardStepsValidationsMap={wizardStepsValidationsMap}
-                />
-              }
-              testId="cluster-validations"
-            />
-            <DetailItem
-              title={t('ai:Host validations')}
-              value={
-                <HostsValidations<ClusterWizardStepsType, typeof allClusterWizardSoftValidationIds>
-                  hosts={cluster.hosts}
-                  setCurrentStepId={setCurrentStepId}
-                  wizardStepNames={wizardStepNames(t)}
-                  allClusterWizardSoftValidationIds={allClusterWizardSoftValidationIds}
-                  wizardStepsValidationsMap={wizardStepsValidationsMap}
-                />
-              }
-              testId="host-validations"
-            />
-          </DetailList>
+          <DetailItem
+            title={t('ai:Cluster validations')}
+            value={
+              <ClusterValidations<ClusterWizardStepsType>
+                validationsInfo={cluster.validationsInfo}
+                setCurrentStepId={goToStepByName}
+                wizardStepNames={wizardStepNames(t)}
+                wizardStepsValidationsMap={wizardStepsValidationsMap}
+              />
+            }
+            testId="cluster-validations"
+          />
+          <DetailItem
+            title={t('ai:Host validations')}
+            value={
+              <HostsValidations<ClusterWizardStepsType, typeof allClusterWizardSoftValidationIds>
+                hosts={cluster.hosts}
+                setCurrentStepId={goToStepByName}
+                wizardStepNames={wizardStepNames(t)}
+                allClusterWizardSoftValidationIds={allClusterWizardSoftValidationIds}
+                wizardStepsValidationsMap={wizardStepsValidationsMap}
+              />
+            }
+            testId="host-validations"
+          />
+        </DetailList>
+      </GridItem>
+      {syncError && (
+        <GridItem>
+          <ValidationSection currentStepId={'review'} hosts={[]}>
+            <Alert variant={AlertVariant.danger} title={t('ai:An error occured')} isInline>
+              {syncError}
+            </Alert>
+          </ValidationSection>
         </GridItem>
-      </Grid>
-    </ClusterDeploymentWizardStep>
+      )}
+    </Grid>
   );
 };
 
