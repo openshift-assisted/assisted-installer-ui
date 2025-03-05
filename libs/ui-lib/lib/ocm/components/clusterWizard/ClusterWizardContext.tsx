openshift-assisted/facet-lib

remove cancel button from wizard when on dissconnected env
Signed-off-by: Elay Aharoni <elayaha@gmail.com>

libs/ui-lib/lib/ocm/components/clusterWizard/ClusterWizardContext.tsx
@@ -18,6 +18,7 @@ export type ClusterWizardContextType = {
   setWizardPerPage: (perPage: number) => void;
   updateUISettings: (data: UISettingsValues) => Promise<void>;
   uiSettings?: UISettingsValues;
+  isDissconnectedMode: boolean;
 };
 
 export const ClusterWizardContext = React.createContext<ClusterWizardContextType | null>(null);

libs/ui-lib/lib/ocm/components/clusterWizard/ClusterWizardFooter.tsx
@@ -82,6 +82,7 @@ const ClusterWizardFooter = ({
 }: ClusterWizardFooterProps) => {
   const { alerts } = useAlerts();
   const navigate = useNavigate();
+  const { isDissconnectedMode } = useClusterWizardContext();
 
   const handleCancel = React.useCallback(() => navigate('/cluster-list'), [navigate]);
 
@@ -100,7 +101,7 @@ const ClusterWizardFooter = ({
     <WizardFooter
       alerts={alertsSection}
       errors={errorsSection}
-      onCancel={onCancel || handleCancel}
+      onCancel={isDissconnectedMode ? undefined : onCancel || handleCancel}
       leftExtraActions={additionalActions}
       cluster={cluster}
       onFetchEvents={onFetchEvents}

openshift-assisted/assisted-installer-ui

libs/ui-lib/lib/ocm/components/clusterWizard/ClusterWizardContext.tsx
@@ -18,6 +18,7 @@ export type ClusterWizardContextType = {
   setWizardPerPage: (perPage: number) => void;
   updateUISettings: (data: UISettingsValues) => Promise<void>;
   uiSettings?: UISettingsValues;
+  isDisconnectedMode: boolean;
 };
 
 export const ClusterWizardContext = React.createContext<ClusterWizardContextType | null>(null);

libs/ui-lib/lib/ocm/components/clusterWizard/ClusterWizardFooter.tsx
@@ -82,6 +82,7 @@ const ClusterWizardFooter = ({
 }: ClusterWizardFooterProps) => {
   const { alerts } = useAlerts();
   const navigate = useNavigate();
+  const { isDisconnectedMode } = useClusterWizardContext();
 
   const handleCancel = React.useCallback(() => navigate('/cluster-list'), [navigate]);
 
@@ -100,7 +101,7 @@ const ClusterWizardFooter = ({
     <WizardFooter
       alerts={alertsSection}
       errors={errorsSection}
-      onCancel={onCancel || handleCancel}
+      onCancel={isDisconnectedMode ? undefined : onCancel || handleCancel}
       leftExtraActions={additionalActions}
       cluster={cluster}
       onFetchEvents={onFetchEvents}