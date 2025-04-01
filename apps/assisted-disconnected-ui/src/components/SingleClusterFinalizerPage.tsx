80af9949071b2fd837af35b2d98289131d7e84cb
rawagner <rawagner@redhat.com>
2025-04-01 14:46:37 +0200

    AGENT-1160: ABI - add finalizing page

---
+++ apps/assisted-disconnected-ui/src/components/SingleClusterFinalizerPage.tsx
@@ -0,0 +1,60 @@
+import {
+  Bullseye,
+  Button,
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateFooter,
+  EmptyStateHeader,
+  EmptyStateVariant,
+  Spinner,
+} from '@patternfly/react-core';
+import { Cluster } from '@openshift-assisted/types/assisted-installer-service';
+import {
+  TroubleshootingOpenshiftConsoleButton,
+  useTranslation,
+} from '@openshift-assisted/ui-lib/common';
+
+const SingleClusterFinalizerPage = ({
+  cluster,
+  consoleUrl,
+}: {
+  cluster: Cluster;
+  consoleUrl?: string;
+}) => {
+  const { t } = useTranslation();
+  return (
+    <Bullseye>
+      <EmptyState variant={EmptyStateVariant.xl}>
+        <EmptyStateHeader
+          titleText={t('ai:Finalizing')}
+          headingLevel="h4"
+          icon={<Spinner size="xl" />}
+        />
+        <EmptyStateBody>
+          {t('ai:Cluster installation is still in-progress.')}
+          <br />
+          {t('ai:Click the webconsole URL below to check if it is up and running.')}
+          <br />
+          {t(
+            'ai:* If you close this browser window, the webconsole URL can be found also inside the credentials file you have downloaded. Cluster installation is still in-progress.',
+          )}
+          <br />
+        </EmptyStateBody>
+        <EmptyStateFooter>
+          {consoleUrl && (
+            <Button
+              variant="link"
+              onClick={() => window.open(consoleUrl, '_blank', 'noopener')}
+              isInline
+            >
+              {consoleUrl}
+            </Button>
+          )}
+          <TroubleshootingOpenshiftConsoleButton cluster={cluster} consoleUrl={consoleUrl} />
+        </EmptyStateFooter>
+      </EmptyState>
+    </Bullseye>
+  );
+};
+
+export default SingleClusterFinalizerPage;
