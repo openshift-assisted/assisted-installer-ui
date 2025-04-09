09b21542a2ecbad66269de9c2ae7feb51d6ce92c
Elay Aharoni <elayaha@gmail.com>
2025-04-09 15:33:49 +0300

    selecting CNV - not able to deselect it using UI
    Signed-off-by: Elay Aharoni <elayaha@gmail.com>

--- libs/ui-lib-tests/cypress/views/operatorsPage.ts
+++ libs/ui-lib-tests/cypress/views/operatorsPage.ts
@@ -2,6 +2,9 @@ export const operatorsPage = {
   openshiftVirtualization: () => {
     return cy.get('#form-checkbox-useContainerNativeVirtualization-field');
   },
+  migrationToolkitForVirtualization: () => {
+    return cy.get('#form-checkbox-useMigrationToolkitforVirtualization-field');
+  },
   singleOperatorsToggle: () => {
     return cy.contains('Single Operators ');
   },
