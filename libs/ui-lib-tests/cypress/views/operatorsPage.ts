7d8d3e2be70377f540036a59a92e6f78e5947448
Montse Ortega <mortegag@redhat.com>
2025-04-22 12:16:55 +0200

    Solving errors with tests

--- libs/ui-lib-tests/cypress/views/operatorsPage.ts
+++ libs/ui-lib-tests/cypress/views/operatorsPage.ts
@@ -2,9 +2,6 @@ export const operatorsPage = {
   openshiftVirtualization: () => {
     return cy.get('#form-input-cnv-field');
   },
-  migrationToolkitForVirtualization: () => {
-    return cy.get('#form-checkbox-useMigrationToolkitforVirtualization-field');
-  },
   singleOperatorsToggle: () => {
     return cy.contains('Single Operators ');
   },
