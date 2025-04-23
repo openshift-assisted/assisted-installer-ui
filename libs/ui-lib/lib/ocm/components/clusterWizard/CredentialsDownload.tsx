fcb91555b8b4be75d581788d707963abcc5205e5
Elay Aharoni <elayaha@gmail.com>
2025-04-23 15:43:52 +0300

    add console url to credentials download file
    Signed-off-by: Elay Aharoni <elayaha@gmail.com>

--- libs/ui-lib/lib/ocm/components/clusterWizard/CredentialsDownload.tsx
+++ libs/ui-lib/lib/ocm/components/clusterWizard/CredentialsDownload.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  ClusterCredentials,
   ClustersAPI,
   ClusterWizardStep,
   ClusterWizardStepHeader,
@@ -14,38 +13,17 @@ import { useClusterWizardContext } from './ClusterWizardContext';
 import ClusterWizardFooter from './ClusterWizardFooter';
 import ClusterWizardNavigation from './ClusterWizardNavigation';
 import { WithErrorBoundary } from '../../../common/components/ErrorHandling/WithErrorBoundary';
-import { Cluster, Credentials } from '@openshift-assisted/types/assisted-installer-service';
-import { Alert, AlertVariant, Checkbox, Grid, Stack } from '@patternfly/react-core';
-import { downloadFile, getErrorMessage } from '../../../common/utils';
+import { Cluster } from '@openshift-assisted/types/assisted-installer-service';
+import { Alert, AlertVariant, Checkbox, Stack } from '@patternfly/react-core';
+import { downloadFile } from '../../../common/utils';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons/dist/js/icons/external-link-alt-icon';
 
 const CredentialsDownload: React.FC<{ cluster: Cluster }> = ({ cluster }) => {
   const clusterWizardContext = useClusterWizardContext();
   const [isChecked, setIsChecked] = React.useState<boolean>(false);
   const [isDownloading, setIsDownloading] = React.useState(false);
-  const [credentials, setCredentials] = React.useState<Credentials>();
-  const [credentialsError, setCredentialsError] = React.useState<string>();
   const { addAlert } = useAlerts();
   const { t } = useTranslation();
-
-  React.useEffect(() => {
-    const fetch = async () => {
-      setCredentialsError(undefined);
-      if (!cluster.id) {
-        return;
-      }
-      try {
-        const response = await ClustersAPI.getCredentials(cluster.id);
-        const data = { username: response.data.username, password: response.data.password };
-        setCredentials(data);
-      } catch (err) {
-        handleApiError(err, (err) => {
-          setCredentialsError(`Failed to fetch credentials, ${getErrorMessage(err)}`);
-        });
-      }
-    };
-    void fetch();
-  }, [cluster.id, setCredentials]);
 
   const downloadSingleFile = async (fileName: string) => {
     try {
@@ -118,14 +96,6 @@ const CredentialsDownload: React.FC<{ cluster: Cluster }> = ({ cluster }) => {
               'ai:I understand that I need to download credentials files prior of proceeding with the cluster installation.',
             )}
           />
-          <Grid hasGutter style={{ justifyItems: 'start' }}>
-            <ClusterCredentials
-              cluster={cluster}
-              credentials={credentials}
-              credentialsError={credentialsError}
-              error={!!credentialsError}
-            />
-          </Grid>
         </Stack>
       </WithErrorBoundary>
     </ClusterWizardStep>
