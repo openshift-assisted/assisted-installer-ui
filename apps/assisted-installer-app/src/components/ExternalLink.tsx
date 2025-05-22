7d9b0bebb682f5d2f02e0e811deae985daf358fd
rawagner <rawagner@redhat.com>
2025-05-22 14:13:53 +0200

    Move installer-app to monorepo

---
+++ apps/assisted-installer-app/src/components/ExternalLink.tsx
@@ -0,0 +1,3 @@
+import { ExternalLink } from '@openshift-assisted/ui-lib/ocm';
+import '../i18n';
+export default ExternalLink;
---
+++ apps/assisted-installer-app/src/components/FeatureSupportsLevel.tsx
@@ -0,0 +1,3 @@
+import { FeaturesSupportsLevel } from '@openshift-assisted/ui-lib/ocm';
+import '../i18n';
+export type { FeaturesSupportsLevel as default };
