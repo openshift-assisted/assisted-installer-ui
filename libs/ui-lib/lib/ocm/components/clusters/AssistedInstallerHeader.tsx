8929ef677a55dc448ac0ca5cf0c18df25d383a2f
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-06-19 15:30:14 +0200

    ABI updates: - clear download alerts - add AI header + tech preview badge (#3003)
    Co-authored-by: rawagner <rawagner@redhat.com>

--- libs/ui-lib/lib/ocm/components/clusters/AssistedInstallerHeader.tsx
+++ libs/ui-lib/lib/ocm/components/clusters/AssistedInstallerHeader.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { TextContent, Text, Split, SplitItem } from '@patternfly/react-core';
-import { ASSISTED_INSTALLER_DOCUMENTATION_LINK, ExternalLink, isInOcm } from '../../../common';
+import {
+  ASSISTED_INSTALLER_DOCUMENTATION_LINK,
+  DeveloperPreview,
+  ExternalLink,
+  isInOcm,
+} from '../../../common';
+import { useFeature } from '../../hooks/use-feature';
 
 export const AssistedInstallerHeader = () => {
+  const isSingleClusterFeatureEnabled = useFeature('ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE');
   return (
     <TextContent>
       <Text component="h1" className="pf-v5-u-display-inline">
         Install OpenShift with the Assisted Installer
       </Text>
+      {isSingleClusterFeatureEnabled && <DeveloperPreview />}
       <Split hasGutter>
         <SplitItem>
           <ExternalLink href={ASSISTED_INSTALLER_DOCUMENTATION_LINK}>
