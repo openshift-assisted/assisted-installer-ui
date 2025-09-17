761cceb1ea7ef08c7db561456b81406a1cceab42
jgyselov <jgyselov@redhat.com>
2025-09-17 10:43:14 +0200

    Clean up ClusterDetailsFormFields

--- libs/ui-lib/lib/cim/components/ClusterDeployment/clusterDetails/ClusterDetailsFormFields.tsx
+++ libs/ui-lib/lib/cim/components/ClusterDeployment/clusterDetails/ClusterDetailsFormFields.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Alert, AlertVariant, FlexItem, Form } from '@patternfly/react-core';
+import { Form } from '@patternfly/react-core';
 import { useFormikContext } from 'formik';
 
 import {
@@ -99,9 +99,6 @@ export const ClusterDetailsFormFields: React.FC<ClusterDetailsFormFieldsProps> =
     return undefined;
   }, [allVersions, values.customOpenshiftSelect, versions]);
 
-  const isDiskEncryptionEnabled =
-    values.enableDiskEncryptionOnMasters || values.enableDiskEncryptionOnWorkers;
-
   return (
     <Form id="wizard-cluster-details__form">
       {isEditFlow ? (
@@ -170,39 +167,10 @@ export const ClusterDetailsFormFields: React.FC<ClusterDetailsFormFieldsProps> =
       <ExternalPlatformsDropdown isDisabled={isEditFlow} />
 
       {extensionAfter?.['openshiftVersion'] && extensionAfter['openshiftVersion']}
+
       {!isEditFlow && <PullSecret />}
+
       {extensionAfter?.['pullSecret'] && extensionAfter['pullSecret']}
-      {/* <DiskEncryptionControlGroup
-        values={values}
-        isDisabled={isPullSecretSet}
-        isSNO={isSNO({ controlPlaneCount })}
-      /> */}
-      {isDiskEncryptionEnabled && values.diskEncryptionMode === 'tpmv2' && (
-        <Alert
-          variant={AlertVariant.warning}
-          isInline
-          title={
-            <FlexItem>
-              {t(
-                'ai:To use this encryption method, enable TPMv2 encryption in the BIOS of each selected host.',
-              )}
-            </FlexItem>
-          }
-        />
-      )}
-      {isDiskEncryptionEnabled && values.diskEncryptionMode === 'tang' && (
-        <Alert
-          variant={AlertVariant.warning}
-          isInline
-          title={
-            <FlexItem>
-              {t(
-                'ai:The use of Tang encryption mode to encrypt your disks is only supported for bare metal or vSphere installations on user-provisioned infrastructure.',
-              )}
-            </FlexItem>
-          }
-        />
-      )}
     </Form>
   );
 };
