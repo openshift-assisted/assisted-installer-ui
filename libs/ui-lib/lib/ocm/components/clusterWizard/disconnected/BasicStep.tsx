2ef6060714243c55e68344e0f6832f1bb14748bf
Montse Ortega <mortegag@redhat.com>
2025-09-17 10:46:58 +0200

    Migration of TextContent component

--- libs/ui-lib/lib/ocm/components/clusterWizard/disconnected/BasicStep.tsx
+++ libs/ui-lib/lib/ocm/components/clusterWizard/disconnected/BasicStep.tsx
@@ -7,17 +7,7 @@ import {
   StaticTextField,
   useTranslation,
 } from '../../../../common';
-import {
-  Split,
-  SplitItem,
-  TextContent,
-  Text,
-  TextVariants,
-  Grid,
-  GridItem,
-  Form,
-} from '@patternfly/react-core';
-import { Formik } from 'formik';
+import { Split, SplitItem, Grid, GridItem, Form, Content } from '@patternfly/react-core';
 import OcmOpenShiftVersion from '../../clusterConfiguration/OcmOpenShiftVersion';
 import { useClusterWizardContext } from '../ClusterWizardContext';
 import ClusterWizardFooter from '../ClusterWizardFooter';
@@ -45,9 +35,9 @@ const BasicStep = () => {
             <GridItem>
               <Split>
                 <SplitItem>
-                  <TextContent>
-                    <Text component={TextVariants.h2}>Basic information</Text>
-                  </TextContent>
+                  <Content>
+                    <Content component="h2">Basic information</Content>
+                  </Content>
                 </SplitItem>
                 <SplitItem>
                   <DeveloperPreview />
