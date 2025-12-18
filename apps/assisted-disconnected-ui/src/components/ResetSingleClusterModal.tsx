c09f10eb4a1b91e3875795bb89a0e4f1e023eca8
rawagner <rawagner@redhat.com>
2025-12-18 12:25:53 +0100

    Reapply "Handle cluster reset on ABI" (#3281)
    This reverts commit 84b8ef8b71d93b8c2635d6213801670e6d18ee43.

--- apps/assisted-disconnected-ui/src/components/ResetSingleClusterModal.tsx
+++ apps/assisted-disconnected-ui/src/components/ResetSingleClusterModal.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Button, ButtonVariant, Content, Stack, StackItem, Alert } from '@patternfly/react-core';
 import { Modal, ModalVariant } from '@patternfly/react-core/deprecated';
-import { useModalDialogsContext, ClustersService } from '@openshift-assisted/ui-lib/ocm';
+import {
+  useModalDialogsContext,
+  ClustersService,
+  HostsService,
+} from '@openshift-assisted/ui-lib/ocm';
 import {
   getApiErrorMessage,
   handleApiError,
@@ -31,6 +35,14 @@ const ResetSingleClusterModal: React.FC = () => {
     try {
       setError(undefined);
       setIsLoading(true);
+      const hosts = cluster?.hosts;
+      if (hosts) {
+        for (const host of hosts) {
+          await HostsService.unbind(host);
+        }
+      }
+
+      // Remove the regular cluster
       await ClustersService.remove(cluster.id);
       navigate(`/`);
     } catch (e) {
