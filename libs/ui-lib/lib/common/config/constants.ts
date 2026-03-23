b29b9a21f5669f2ee4a6b38a93a396696df058a8
Elay Aharoni <elayaha@gmail.com>
2026-03-23 18:42:46 +0200

    Revert "Include additional operators in OVE UI ISO config (#3413)"
    This reverts commit dc463bbf368874147dd0efe9048927221aa21704.

--- libs/ui-lib/lib/common/config/constants.ts
+++ libs/ui-lib/lib/common/config/constants.ts
@@ -379,7 +379,6 @@ export const OPERATOR_NAME_METALLB = 'metallb';
 
 export const singleClusterOperators = [
   OPERATOR_NAME_CNV,
-  OPERATOR_NAME_LSO,
   OPERATOR_NAME_MTV,
   OPERATOR_NAME_NMSTATE,
   OPERATOR_NAME_NODE_HEALTHCHECK,
@@ -389,9 +388,6 @@ export const singleClusterOperators = [
   OPERATOR_NAME_CLUSTER_OBSERVABILITY,
   OPERATOR_NAME_LOKI,
   OPERATOR_NAME_OPENSHIFT_LOGGING,
-  OPERATOR_NAME_NUMA_RESOURCES,
-  OPERATOR_NAME_OADP,
-  OPERATOR_NAME_METALLB,
 ];
 
 export const singleClusterBundles = ['virtualization'];
