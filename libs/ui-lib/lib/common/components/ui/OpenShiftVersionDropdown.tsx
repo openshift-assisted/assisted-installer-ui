fdf1bcddb0d0e1f9ea9b81a6174aaedba4334248
jgyselov <jgyselov@redhat.com>
2026-04-23 11:03:18 +0200

    Fix Show all versions double button

--- libs/ui-lib/lib/common/components/ui/OpenShiftVersionDropdown.tsx
+++ libs/ui-lib/lib/common/components/ui/OpenShiftVersionDropdown.tsx
@@ -20,6 +20,7 @@ import { getFieldId } from './formik';
 import ExternalLink from './ExternalLink';
 import { OCP_RELEASES_PAGE } from '../../config';
 import { ClusterDetailsValues, ItemDropdown } from '../clusterWizard';
+import { getVersionLabel } from './utils';
 
 export type HelperTextType = (value: string | null, inModal?: boolean) => JSX.Element | null;
 
@@ -58,7 +59,13 @@ export const OpenShiftVersionDropdown = ({
   const {
     values: { customOpenshiftSelect },
   } = useFormikContext<ClusterDetailsValues>();
-  const [current, setCurrent] = React.useState<string>();
+  const current = React.useMemo(
+    () =>
+      (customItem ? [...versions, customItem] : versions).find(
+        (item) => item.value === field.value,
+      ),
+    [customItem, field.value, versions],
+  );
 
   React.useEffect(() => {
     let defaultVersion = versions.find((item) => item.default);
@@ -68,7 +75,6 @@ export const OpenShiftVersionDropdown = ({
       defaultVersion = versions.find((item) => item.value === customOpenshiftSelect);
     }
 
-    setCurrent(defaultVersion?.label || '');
     setValue(defaultVersion?.value || '');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [customOpenshiftSelect]);
@@ -100,11 +106,12 @@ export const OpenShiftVersionDropdown = ({
         <DropdownItem key={customItem.value} id={customItem.value} value={customItem.value}>
           {customItem.label}
         </DropdownItem>
+        <Divider key="all-versions-divider" />
       </DropdownGroup>
     ),
     <DropdownGroup key="all-available-versions">
       <DropdownItem key="all-versions" id="all-versions" value="all-versions">
-        <div className="pf-v6-u-text-color-link">{t('ai:Show all available versions')}</div>
+        <div className="pf-v5-u-link-color">{t('ai:Show all available versions')}</div>
       </DropdownItem>
     </DropdownGroup>,
   ].filter(Boolean);
@@ -127,7 +134,7 @@ export const OpenShiftVersionDropdown = ({
       onClick={() => setOpen(!isOpen)}
       isExpanded={isOpen}
     >
-      {current || t('ai:OpenShift version')}
+      {current ? getVersionLabel(current, t) : t('ai:OpenShift version')}
     </MenuToggle>
   );
 
