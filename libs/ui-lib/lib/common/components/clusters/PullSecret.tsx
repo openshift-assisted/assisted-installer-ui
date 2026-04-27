e4be38f42701e3995682de3a111e3f02ccbd6696
Lior Soffer <liorsoffer1@gmail.com>
2026-04-27 17:22:30 +0300

    MGMT-24152: UI allows editing pull secret in draft cluster (#3662)
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>

--- libs/ui-lib/lib/common/components/clusters/PullSecret.tsx
+++ libs/ui-lib/lib/common/components/clusters/PullSecret.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, AlertVariant, Checkbox } from '@patternfly/react-core';
+import { Checkbox } from '@patternfly/react-core';
 import { useFormikContext } from 'formik';
 import { ClusterCreateParams } from '@openshift-assisted/types/assisted-installer-service';
 import { PullSecretField, PullSecretInfo } from '../ui';
@@ -8,56 +8,46 @@ import { useTranslation } from '../../hooks/use-translation-wrapper';
 export type PullSecretProps = {
   defaultPullSecret?: string;
   isOcm?: boolean;
-  isPullSecretSet?: boolean;
   isSingleClusterFeatureEnabled?: boolean;
 };
 
 const PullSecret: React.FC<PullSecretProps> = ({
   defaultPullSecret,
   isOcm = false,
-  isPullSecretSet = false,
   isSingleClusterFeatureEnabled = false,
 }) => {
-  // Existing cluster with a pull secret on the server: start collapsed so the secret text is not shown until the user chooses "Edit pull secret".
-  const [isExpanded, setExpanded] = React.useState(() =>
-    isPullSecretSet ? false : !defaultPullSecret,
-  );
+  const [isExpanded, setExpanded] = React.useState(!defaultPullSecret);
   const { setFieldValue } = useFormikContext<ClusterCreateParams>();
   const { t } = useTranslation();
-  const onCheckboxChange = () => {
-    if (isExpanded) {
-      // about to collapse, reset to original value
-      setFieldValue('pullSecret', defaultPullSecret);
-    }
-    setExpanded(!isExpanded);
-  };
+  if (isOcm || isSingleClusterFeatureEnabled) {
+    const onCheckboxChange = () => {
+      if (isExpanded) {
+        // about to collapse, reset to original value
+        setFieldValue('pullSecret', defaultPullSecret);
+      }
+      setExpanded(!isExpanded);
+    };
 
-  if (!(isOcm || isSingleClusterFeatureEnabled)) {
-    return <PullSecretField isOcm={isOcm} />;
+    return (
+      <>
+        <Checkbox
+          id="checkbox-pull-secret"
+          className="pf-v6-u-display-inline-flex"
+          aria-label={t('ai:edit pull secret')}
+          isChecked={isExpanded}
+          onChange={onCheckboxChange}
+          label={
+            <>
+              {t('ai:Edit pull secret')} <PullSecretInfo isOcm={isOcm} />
+            </>
+          }
+        />
+        {isExpanded && <PullSecretField isOcm={isOcm} />}
+      </>
+    );
   }
 
-  return (
-    <>
-      {isPullSecretSet && (
-        <Alert variant={AlertVariant.success} isInline title={t('ai:Pull secret configured')}>
-          {t('ai:A pull secret has already been set for this cluster. You can edit it below.')}
-        </Alert>
-      )}
-      <Checkbox
-        id="checkbox-pull-secret"
-        className="pf-v6-u-display-inline-flex"
-        aria-label={t('ai:edit pull secret')}
-        isChecked={isExpanded}
-        onChange={onCheckboxChange}
-        label={
-          <>
-            {t('ai:Edit pull secret')} <PullSecretInfo isOcm={isOcm} />
-          </>
-        }
-      />
-      {isExpanded && <PullSecretField isOcm={isOcm} />}
-    </>
-  );
+  return <PullSecretField isOcm={isOcm} />;
 };
 
 export default PullSecret;
