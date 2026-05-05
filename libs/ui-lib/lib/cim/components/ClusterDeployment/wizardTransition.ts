4c0ce3bb836d36c8fc177bf5565897fc3d471b94
jgyselov <jgyselov@redhat.com>
2026-05-05 14:17:29 +0200

    Add custom-manifests-requirements-satisfied to soft validations on networking wizard step

--- libs/ui-lib/lib/cim/components/ClusterDeployment/wizardTransition.ts
+++ libs/ui-lib/lib/cim/components/ClusterDeployment/wizardTransition.ts
@@ -69,7 +69,11 @@ const networkingStepValidationsMap: WizardStepValidationMap = {
   // Alternatively we would have to whitelist network validations instead of using group
   // TODO(mlibra): remove that container-images-available from soft validations and let backend drive it via disabling it.
   //   Depends on: https://issues.redhat.com/browse/MGMT-5265
-  softValidationIds: ['ntp-synced', 'container-images-available'],
+  softValidationIds: [
+    'ntp-synced',
+    'container-images-available',
+    'custom-manifests-requirements-satisfied',
+  ],
   getPageURL: (host: Host, validationID: string) => {
     if (validationID === 'ntp-synced') {
       return {
