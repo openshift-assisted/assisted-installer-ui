fe2ed0e44fe880e8e8d1b146f412abf828528fd2
Ashraf Masarwa <asmasarw@redhat.com>
2026-05-06 12:29:36 +0300

    OCPBUGS-84038 | [Below the sea UI] Lack of visual feedback (spinner) on disabled Next button during background validation

--- libs/ui-lib/lib/ocm/components/hosts/HostsTableEmptyState.tsx
+++ libs/ui-lib/lib/ocm/components/hosts/HostsTableEmptyState.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Spinner } from '@patternfly/react-core';
 import { ConnectedIcon } from '@patternfly/react-icons/dist/js/icons/connected-icon';
 import EmptyState from '../../../common/components/ui/uiState/EmptyState';
 import { useTranslation } from '../../../common/hooks/use-translation-wrapper';
@@ -16,6 +17,7 @@ const HostsTableEmptyState = ({ isSingleNode = false }: HostsTableEmptyStateProp
       icon={ConnectedIcon}
       title={t('ai:Waiting for host...', { count: isSingleNode ? 1 : 2 })}
       content={t('ai:Hosts may take a few minutes to appear here after booting.')}
+      primaryAction={<Spinner size="xl" />}
       secondaryActions={[
         <HostsDiscoveryTroubleshootingInfoLinkWithModal key={'hosts-not-showing'} />,
       ]}
