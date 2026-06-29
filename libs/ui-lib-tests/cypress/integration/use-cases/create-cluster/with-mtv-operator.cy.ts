d946a72f4a67ea3b53ef7058859f177154f3fbb9
Lior Soffer <liorsoffer1@gmail.com>
2026-06-29 13:00:02 +0300

    Reflect selected operators correctly  (#3805)
    * MGMT-24633:When selecting Openshift AI bundle, dependencies aren't select
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>
    
    * fixes
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>
    
    * fixes2
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>
    
    ---------
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>

--- libs/ui-lib-tests/cypress/integration/use-cases/create-cluster/with-mtv-operator.cy.ts
+++ libs/ui-lib-tests/cypress/integration/use-cases/create-cluster/with-mtv-operator.cy.ts
@@ -2,6 +2,19 @@ import { commonActions } from '../../../views/common';
 import OperatorsForm from '../../../views/forms/Operators/OperatorsForm';
 import { operatorsPage } from '../../../views/operatorsPage';
 
+const mtvOperatorPreflightRequirements = {
+  operators: [
+    {
+      operatorName: 'mtv',
+      dependencies: ['cnv'],
+    },
+    {
+      operatorName: 'cnv',
+      dependencies: ['lso'],
+    },
+  ],
+};
+
 describe(`Create cluster with MTV operator enabled`, () => {
   const setTestStartSignal = (activeSignal: string) => {
     cy.setTestEnvironment({
@@ -14,25 +27,36 @@ describe(`Create cluster with MTV operator enabled`, () => {
 
   beforeEach(() => {
     setTestStartSignal('CLUSTER_CREATED');
+    cy.intercept(
+      'GET',
+      `/api/assisted-install/v2/clusters/${Cypress.env('clusterId')}/preflight-requirements`,
+      mtvOperatorPreflightRequirements,
+    ).as('cluster-req');
     commonActions.visitClusterDetailsPage();
     commonActions.startAtWizardStep('Operators');
     operatorsPage.singleOperatorsToggle().click();
+    cy.wait('@cluster-req');
+    cy.wait('@supported-operators');
   });
 
   describe('When the feature is enabled:', () => {
     it('There is a popover and helper text next to the checkbox label', () => {
       OperatorsForm.mtvOperatorControl.findHelperText();
     });
-    it('The user can select the MTV checkbox', () => {
+    it('Selecting MTV checks dependency operators and sends only MTV to the API', () => {
+      OperatorsForm.assertOperatorCheckbox('cnv', { checked: false, disabled: false });
+      OperatorsForm.assertOperatorCheckbox('lso', { checked: false, disabled: true });
+
       OperatorsForm.mtvOperatorControl.findLabel().click({ force: true });
+
+      OperatorsForm.assertOperatorCheckbox('mtv', { checked: true, disabled: false });
+      OperatorsForm.assertOperatorCheckbox('cnv', { checked: true, disabled: true });
+      OperatorsForm.assertOperatorCheckbox('lso', { checked: true, disabled: true });
+
       commonActions.toNextStepAfter('Operators');
 
       cy.wait('@update-cluster').then(({ request }) => {
-        expect(request.body.olm_operators).to.deep.equal([
-          { name: 'cnv' },
-          { name: 'lso' },
-          { name: 'mtv' },
-        ]);
+        expect(request.body.olm_operators).to.deep.equal([{ name: 'mtv' }]);
       });
     });
   });
