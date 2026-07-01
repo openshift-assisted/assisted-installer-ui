9a3d079157f681d601313806e3d9ad3581f7ae5e
jgyselov <jgyselov@redhat.com>
2026-07-01 09:41:16 +0200

    OCM folder structure - cluster detail card

--- libs/ui-lib/lib/ocm/components/clusterDetail/index.ts
+++ libs/ui-lib/lib/ocm/components/clusterDetail/index.ts
@@ -1,7 +1,6 @@
 export * from './utils';
 export { default as ClusterDetail } from './ClusterDetail';
 export { default as ClusterInstallationProgressCard } from './ClusterInstallationProgressCard';
-export { default as AssistedInstallerDetailCard } from './AssistedInstallerDetailCard';
 
 export * from './ClusterPollingErrorModal';
 export * from './CancelInstallationModal';
--- libs/ui-lib/lib/ocm/pages/index.tsx
+++ libs/ui-lib/lib/ocm/pages/index.tsx
@@ -1,3 +1,4 @@
 export * from './singleClusterPage';
 export * from './Routes';
 export * from './clusterDetailTab';
+export * from './clusterDetailCard';
