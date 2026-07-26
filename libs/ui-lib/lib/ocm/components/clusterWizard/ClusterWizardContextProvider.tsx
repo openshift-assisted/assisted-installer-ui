532bb47ee1512ae9a6f40b0cf3cf845c312bc143
Ashraf Masarwa <asmasarw@redhat.com>
2026-07-26 13:01:50 +0300

    Fix Formating Issue

--- libs/ui-lib/lib/ocm/components/clusterWizard/ClusterWizardContextProvider.tsx
+++ libs/ui-lib/lib/ocm/components/clusterWizard/ClusterWizardContextProvider.tsx
@@ -70,7 +70,9 @@ const getWizardStepIds = (
     stepsCopy = removeStepFromClusterWizard(stepsCopy, 'custom-manifests', 1);
   }
   if (isSingleClusterFeatureEnabled && !stepsCopy.includes('credentials-download')) {
-    const referenceStep = stepsCopy.includes('custom-manifests') ? 'custom-manifests' : 'networking';
+    const referenceStep = stepsCopy.includes('custom-manifests')
+      ? 'custom-manifests'
+      : 'networking';
     stepsCopy = addStepToClusterWizard(stepsCopy, referenceStep, ['credentials-download']);
   }
 
