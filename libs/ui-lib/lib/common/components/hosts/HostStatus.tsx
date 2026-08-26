e095f017c3138bcebdfe4701f7540321d5cfecbd
Lior Soffer <liorsoffer1@gmail.com>
2026-08-26 11:41:47 +0300

    Split HostStatus popover into subcomponents
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>

--- libs/ui-lib/lib/common/components/hosts/HostStatus.tsx
+++ libs/ui-lib/lib/common/components/hosts/HostStatus.tsx
@@ -1,224 +1,21 @@
 import React from 'react';
-import {
-  Popover,
-  Button,
-  Content,
-  FlexItem,
-  Flex,
-  Stack,
-  StackItem,
-  Icon,
-} from '@patternfly/react-core';
-import { PopoverProps } from '@patternfly/react-core/dist/js/components/Popover/Popover';
+import { Popover, FlexItem, Flex, Icon } from '@patternfly/react-core';
 import { ExclamationTriangleIcon } from '@patternfly/react-icons/dist/js/icons/exclamation-triangle-icon';
 import { UnknownIcon } from '@patternfly/react-icons/dist/js/icons/unknown-icon';
 
-import hdate from 'human-date';
-
 import { Host, HostProgressInfo } from '@openshift-assisted/types/assisted-installer-service';
-import { ValidationsInfo } from '../../types/hosts';
-import { ExternalLink, getHumanizedDateTime } from '../ui';
 
-import HostProgress from './HostProgress';
 import { getHostProgressStageNumber, getHostProgressStages } from './utils';
-import {
-  AdditionNtpSourcePropsType,
-  HostValidationGroups,
-  ValidationInfoActionProps,
-} from './HostValidationGroups';
-import OcpConsoleNodesSectionLink from './OcpConsoleNodesSectionLink';
-import { toSentence } from '../ui/table/utils';
+import { HostStatusPopover } from './HostStatusPopover';
 import { HostStatusProps } from './types';
-import { UpdateDay2ApiVipPropsType } from './HostValidationGroups';
 import { useTranslation } from '../../hooks/use-translation-wrapper';
 import { hostStatus } from './status';
-import { getApproveNodesInClLink } from '../../config';
 
 const getTitleWithProgress = (host: Host, status: HostStatusProps['status']) => {
   const stages = getHostProgressStages(host);
   const stageNumber = getHostProgressStageNumber(host);
   return status.withProgress ? `${status.title} ${stageNumber}/${stages.length}` : status.title;
 };
-
-type HostStatusPopoverContentProps = ValidationInfoActionProps & {
-  details?: string;
-  validationsInfo: ValidationsInfo;
-  autoCSR?: boolean;
-  additionalPopoverContent?: React.ReactNode;
-  additionalBMHInfo?: React.ReactNode;
-  openshiftVersion?: string;
-};
-
-const HostStatusPopoverContent: React.FC<HostStatusPopoverContentProps> = ({
-  details,
-  autoCSR,
-  additionalPopoverContent,
-  additionalBMHInfo,
-  openshiftVersion,
-  ...props
-}) => {
-  const { host } = props;
-  const { status, statusInfo } = host;
-  const { t } = useTranslation();
-  if (['installing-in-progress'].includes(status)) {
-    return (
-      <Content>
-        <HostProgress host={host} />
-      </Content>
-    );
-  }
-
-  if (['error', 'cancelled', 'installing-pending-user-action'].includes(status)) {
-    return (
-      <Content>
-        <Content component="p">
-          {details}
-          <br />
-          {toSentence(statusInfo)}
-        </Content>
-        <HostProgress host={host} />
-      </Content>
-    );
-  }
-
-  if (['installed'].includes(status)) {
-    return (
-      <Content>
-        <Content component="p">{details}</Content>
-        <HostProgress host={host} />
-      </Content>
-    );
-  }
-
-  if (['added-to-existing-cluster'].includes(status)) {
-    return (
-      <Content>
-        <Content component="p">{details}</Content>
-        {!autoCSR && (
-          <>
-            <br />
-            {t(
-              "ai:To finish adding nodes to the cluster, approve the join request inside OpenShift Console's Nodes section.",
-            )}
-            <br />
-            {t('ai:It may take a few minutes for the join request to appear.')}
-            <br />
-            {t('ai:If you prefer using the CLI, follow the instructions in')}&nbsp;
-            <ExternalLink href={getApproveNodesInClLink(openshiftVersion)}>
-              {t('ai:How to approve nodes using the CLI')}
-            </ExternalLink>
-          </>
-        )}
-        <HostProgress host={host} />
-      </Content>
-    );
-  }
-
-  if (
-    [
-      'preparing-for-installation',
-      'preparing-successful',
-      'installing',
-      'unbinding-pending-user-action',
-      'binding',
-      'unbinding',
-      'reclaiming',
-      'reclaiming-rebooting',
-    ].includes(status)
-  ) {
-    // No additional error messages shown
-    return (
-      <Content>
-        <Content component="p">{details}</Content>
-      </Content>
-    );
-  }
-
-  return (
-    <Stack hasGutter>
-      {additionalPopoverContent}
-      {details && (
-        <StackItem>
-          <Content component="p">{details}</Content>
-        </StackItem>
-      )}
-      <StackItem>
-        <HostValidationGroups openshiftVersion={openshiftVersion} {...props} />
-        {additionalBMHInfo}
-      </StackItem>
-    </Stack>
-  );
-};
-
-const HostStatusPopoverFooter: React.FC<{ host: Host }> = ({ host }) => {
-  const { progress, statusUpdatedAt } = host;
-  const { t } = useTranslation();
-
-  if (host.status === 'added-to-existing-cluster') {
-    return (
-      <OcpConsoleNodesSectionLink
-        id={`host-status-detail-link-to-ocp-nodes-${host.requestedHostname || host.id}`}
-      />
-    );
-  }
-  let footerText;
-  if (host.status === 'installing-in-progress') {
-    if (progress?.stageUpdatedAt && progress.stageUpdatedAt !== progress.stageStartedAt) {
-      footerText = t('ai:Step started at {{startedAt}}, updated {{updatedAt}}', {
-        startedAt: getHumanizedDateTime(progress.stageStartedAt),
-        updatedAt: hdate.relativeTime(progress.stageUpdatedAt),
-      });
-    } else {
-      footerText = t('ai:Step started at {{startedAt}}', {
-        startedAt: getHumanizedDateTime(progress?.stageStartedAt || statusUpdatedAt),
-      });
-    }
-  } else if (statusUpdatedAt) {
-    footerText = t('ai:Status updated at {{humanizedDataTime}}', {
-      humanizedDataTime: getHumanizedDateTime(statusUpdatedAt),
-    });
-  }
-
-  return <>{!!footerText && <small>{footerText}</small>}</>;
-};
-
-type WithHostStatusPopoverProps = AdditionNtpSourcePropsType &
-  UpdateDay2ApiVipPropsType & {
-    hideOnOutsideClick: PopoverProps['hideOnOutsideClick'];
-    host: Host;
-    onEditHostname: HostStatusPopoverContentProps['onEditHostname'];
-    title: string;
-    validationsInfo: ValidationsInfo;
-    details?: string;
-    zIndex?: number;
-    autoCSR?: boolean;
-    additionalPopoverContent?: React.ReactNode;
-    additionalBMHInfo?: React.ReactNode;
-    openshiftVersion?: string;
-  };
-
-const WithHostStatusPopover: React.FC<React.PropsWithChildren<WithHostStatusPopoverProps>> = (
-  props,
-) => (
-  <Popover
-    headerContent={<div>{props.title}</div>}
-    bodyContent={
-      <div style={{ maxHeight: '33vh', overflow: 'auto' }}>
-        <HostStatusPopoverContent {...props} />
-      </div>
-    }
-    footerContent={<HostStatusPopoverFooter host={props.host} />}
-    minWidth="30rem"
-    maxWidth="50rem"
-    hideOnOutsideClick={props.hideOnOutsideClick}
-    zIndex={props.zIndex || 300}
-    closeBtnAriaLabel={`close-popover-${props.host.requestedHostname ?? ''}`}
-  >
-    <Button variant={'link'} isInline size="sm">
-      {props.children}
-    </Button>
-  </Popover>
-);
 
 const getHostStatusIcon = (
   icon: React.ReactNode,
@@ -241,7 +38,8 @@ const getHostStatusIcon = (
         </Icon>
       </Popover>
     );
-  } else return icon;
+  }
+  return icon;
 };
 
 const HostStatus: React.FC<React.PropsWithChildren<HostStatusProps>> = ({
@@ -270,41 +68,44 @@ const HostStatus: React.FC<React.PropsWithChildren<HostStatusProps>> = ({
 
   const { title, icon, sublabel, details, noPopover } = status;
   const titleWithProgress = getTitleWithProgress(host, status);
-  const popoverProps: WithHostStatusPopoverProps = {
-    hideOnOutsideClick: !keepOnOutsideClick,
-    host,
-    onEditHostname: toggleHostname,
-    AdditionalNTPSourcesDialogToggleComponent,
-    NtpSyncFailureMessageComponent,
-    title,
-    validationsInfo,
-    details,
-    UpdateDay2ApiVipDialogToggleComponent,
-    zIndex,
-    autoCSR,
-    additionalPopoverContent,
-    additionalBMHInfo,
-    openshiftVersion,
-  };
-
   const hostIcon = getHostStatusIcon(icon, host.progress, status);
+
+  const renderStatusPopover = (label: React.ReactNode) => (
+    <HostStatusPopover
+      hideOnOutsideClick={!keepOnOutsideClick}
+      host={host}
+      onEditHostname={toggleHostname}
+      title={title}
+      validationsInfo={validationsInfo}
+      details={details}
+      zIndex={zIndex}
+      autoCSR={autoCSR}
+      additionalPopoverContent={additionalPopoverContent}
+      additionalBMHInfo={additionalBMHInfo}
+      openshiftVersion={openshiftVersion}
+      AdditionalNTPSourcesDialogToggleComponent={AdditionalNTPSourcesDialogToggleComponent}
+      NtpSyncFailureMessageComponent={NtpSyncFailureMessageComponent}
+      UpdateDay2ApiVipDialogToggleComponent={UpdateDay2ApiVipDialogToggleComponent}
+    >
+      {label}
+    </HostStatusPopover>
+  );
+
   return (
     <Flex
       alignItems={{ default: 'alignItemsCenter' }}
       spaceItems={{ default: 'spaceItemsXs' }}
       flexWrap={{ default: 'nowrap' }}
     >
-      {
-        <FlexItem>
-          {(autoCSR && status.key === 'added-to-existing-cluster'
-            ? hostStatus(t).installed.icon
-            : hostIcon) || <UnknownIcon />}
-        </FlexItem>
-      }
+      <FlexItem>
+        {(autoCSR && status.key === 'added-to-existing-cluster'
+          ? hostStatus(t).installed.icon
+          : hostIcon) || <UnknownIcon />}
+      </FlexItem>
 
       <Flex direction={{ default: 'column' }} spaceItems={{ default: 'spaceItemsXs' }}>
         {!children && !sublabel && !noPopover ? (
-          <WithHostStatusPopover {...popoverProps}>{titleWithProgress}</WithHostStatusPopover>
+          renderStatusPopover(titleWithProgress)
         ) : (
           <FlexItem>{titleWithProgress}</FlexItem>
         )}
@@ -314,7 +115,7 @@ const HostStatus: React.FC<React.PropsWithChildren<HostStatusProps>> = ({
             className="pf-v6-u-font-size-xs"
             style={{ marginTop: 'calc(-1 * var(--pf-v6-l-flex--spacer--xs))' }}
           >
-            <WithHostStatusPopover {...popoverProps}>{sublabel}</WithHostStatusPopover>
+            {renderStatusPopover(sublabel)}
           </FlexItem>
         )}
       </Flex>
