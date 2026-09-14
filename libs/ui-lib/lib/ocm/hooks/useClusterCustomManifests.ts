a3b65d1cbdbe9340b398ac394c8a8360e268f1f2
Lior Soffer <liorsoffer1@gmail.com>
2026-09-14 16:48:58 +0300

    OCPBUGS-121378:  Do not show IRI custom manifests in the disconnected UI (#4048)
    * OCPBUGS-121378:  Do not show IRI custom manifests in the disconnected UI (below the sea)
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>
    
    * Use partial match patterns for manifest filenames
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>
    
    ---------
    
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>

--- libs/ui-lib/lib/ocm/hooks/useClusterCustomManifests.ts
+++ libs/ui-lib/lib/ocm/hooks/useClusterCustomManifests.ts
@@ -9,6 +9,7 @@ import { ClustersAPI } from '../services/apis';
 import { getApiErrorMessage, handleApiError } from '../../common/api';
 import { getErrorMessage } from '../../common/utils';
 import { ListManifestsExtended } from '../components/wizard/steps/customManifests/components/types';
+import { useFeature } from './use-feature';
 
 const { addAlert } = alertsSlice.actions;
 
@@ -51,22 +52,39 @@ const getManifestsInfo = async (customManifests: ListManifests, clusterId: strin
   return manifestsExtended;
 };
 
+// Temporary fix for OCPBUGS-121378: Do not show IRI custom manifests in the disconnected UI
+const iriManifestPatterns = [
+  /^idms-oc-mirror-.*\.yaml$/,
+  /^itms-oc-mirror-.*\.yaml$/,
+  /^cs-redhat-operator-index-.*\.yaml$/,
+  /^internalreleaseimage\.yaml$/,
+  /^operatorhub-cluster\.yaml$/,
+];
+
+const isIriManifest = (fileName: string): boolean =>
+  iriManifestPatterns.some((pattern) => pattern.test(fileName));
+
+const filterIriManifests = (manifests: ListManifests): ListManifests =>
+  manifests.filter((m) => !isIriManifest(m.fileName || ''));
+
 export const useClusterCustomManifests = (clusterId: Cluster['id'], extendedVersion: boolean) => {
   const [customManifests, setCustomManifests] = React.useState<ListManifestsExtended>();
   const [error, setError] = React.useState('');
   const [extendedManifestsLoaded, setExtendedManifestsLoaded] = React.useState<boolean>(false);
+  const isSingleClusterFeatureEnabled = useFeature('ASSISTED_INSTALLER_SINGLE_CLUSTER_FEATURE');
 
   const fetchCustomManifests = React.useCallback(async () => {
     try {
       if (clusterId !== '') {
         const { data } = await ClustersAPI.getManifests(clusterId);
+        const filteredData = isSingleClusterFeatureEnabled ? filterIriManifests(data) : data;
         if (extendedVersion) {
-          await getManifestsInfo(data, clusterId).then((manifests) => {
+          await getManifestsInfo(filteredData, clusterId).then((manifests) => {
             setCustomManifests(manifests);
             setExtendedManifestsLoaded(true);
           });
         } else {
-          const manifestsExtended = data.map((manifest: Manifest) => ({
+          const manifestsExtended = filteredData.map((manifest: Manifest) => ({
             ...manifest,
             folder: manifest.folder || 'manifests',
             fileName: manifest.fileName || '',
@@ -85,7 +103,7 @@ export const useClusterCustomManifests = (clusterId: Cluster['id'], extendedVers
         }),
       );
     }
-  }, [clusterId, extendedVersion]);
+  }, [clusterId, extendedVersion, isSingleClusterFeatureEnabled]);
 
   React.useEffect(() => {
     void fetchCustomManifests();
